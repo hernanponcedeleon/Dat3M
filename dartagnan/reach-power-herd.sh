7053487ec59e089b42b4d7dd2f31c7e6eef03494
Hernan <ponce@fortiss.org>
2017-07-12 14:28:30 +0300

    removed output files

--- dartagnan/reach-power-herd.sh
+++
@@ -1,5 +0,0 @@
-for file in ../../Projects/Memory-Models/LitmusSuite/safe/*.litmus;
-do
-    echo $file;
-    herd7 -model ../../Projects/Memory-Models/herd/power.cat $file | grep -e 'Never' | wc -l;
-done
