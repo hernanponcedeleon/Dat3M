97aa6c2cc56d8efae54be83d5ca27081d87aa6aa
Zeta <zeta96@gmail.com>
2017-10-20 17:24:41 +0200

    Delete run_herd_litmus.sh

--- dartagnan/scripts/run_herd_litmus.sh
+++
@@ -1,7 +0,0 @@
-for file in ~/Documents/Projects/Memory-Models/LitmusSuite/reach/*.litmus;
-do
-    echo $(basename $file);
-    herd7 -model ~/Documents/Projects/Memory-Models/herd/power.cat $file > ./rob.out;
-    grep -e 'Never' rob.out | wc -l
-    rm rob.out
-done
