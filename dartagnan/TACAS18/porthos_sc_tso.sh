90b6f84d9da62f00e42937d60cdb8af4ea6ff3a6
Zeta <zeta96@gmail.com>
2017-10-24 23:07:35 +0200

    Scripts for TACAS experiments

--- dartagnan/TACAS18/porthos_sc_tso.sh
+++ dartagnan/TACAS18/porthos_sc_tso.sh
@@ -2,7 +2,7 @@ for file in ../benchmarks/all_rx/*.pts;
 do
    echo $file
    START1=$(python -c 'import time; print time.time()')
-   timeout 600 java porthos/Porthos -s sc -t tso -i $file -state
+   /opt/local/bin/timeout 600 java porthos/Porthos -s sc -t tso -i $file -state
    END1=$(python -c 'import time; print time.time()')
    DIFF1=$(echo "$END1 - $START1" | bc)
    echo $DIFF1
