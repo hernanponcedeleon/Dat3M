a12ae092aefb53579dae70b57e0669163357528b
Zeta <zeta96@gmail.com>
2017-10-24 22:42:29 +0200

    Scripts for TACAS experiments

---
+++ dartagnan/TACAS18/porthos_herd_sc_tso.sh
@@ -0,0 +1,22 @@
+for file in ../benchmarks/all_rx/*.pts;
+do
+   echo $file
+   START1=$(python -c 'import time; print time.time()')
+   timeout 600 java porthos/Porthos -s sc -t tso -i $file -state
+   END1=$(python -c 'import time; print time.time()')
+   DIFF1=$(echo "$END1 - $START1" | bc)
+   echo $DIFF1
+done
+
+#for file in ../benchmarks/all_rx/*.litmus;
+#do
+#   echo $file
+#   START2=$(python -c 'import time; print time.time()')
+#   timeout 600 herd7 -model ~/Documents/Projects/Memory-Models/herd/tso.cat $file
+#   timeout 600 herd7 -model ~/Documents/Projects/Memory-Models/herd/sc.cat $file
+#   END2=$(python -c 'import time; print time.time()')
+#   DIFF2=$(echo "$END2 - $START2" | bc)
+#   echo $DIFF2
+#done
+
+
