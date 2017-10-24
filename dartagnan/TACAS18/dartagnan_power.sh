54016139ea0caadb0575fc3a3fbcf4a49f9f7388
Zeta <zeta96@gmail.com>
2017-10-24 23:06:36 +0200

    Scripts for TACAS experiments

--- dartagnan/TACAS18/dartagnan_power.sh
+++ dartagnan/TACAS18/dartagnan_power.sh
@@ -2,7 +2,7 @@ for file in ../benchmarks/all_rx/*.pts;
 do
    echo $file
    START1=$(python -c 'import time; print time.time()')
-   timeout 600 java dartagnan/Dartagnan -t power -i $file
+   /opt/local/bin/timeout 600 java dartagnan/Dartagnan -t power -i $file
    END1=$(python -c 'import time; print time.time()')
    DIFF1=$(echo "$END1 - $START1" | bc)
    echo $DIFF1
