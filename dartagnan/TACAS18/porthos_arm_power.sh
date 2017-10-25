f5c171f675dae1d3c9e5ab413f2690fe1ccb1fc4
Zeta <zeta96@gmail.com>
2017-10-25 11:07:24 +0200

    Scripts for TACAS experiments

--- dartagnan/TACAS18/porthos_arm_power.sh
+++ dartagnan/TACAS18/porthos_arm_power.sh
@@ -2,7 +2,7 @@ for file in ../benchmarks/all_rx/*.pts;
 do
    echo $file
    START1=$(python -c 'import time; print time.time()')
-   /opt/local/bin/timeout 600 java porthos/Porthos -s arm -t power -i $file -state
+   timeout 600 java porthos/Porthos -s arm -t power -i $file -state
    END1=$(python -c 'import time; print time.time()')
    DIFF1=$(echo "$END1 - $START1" | bc)
    echo $DIFF1
