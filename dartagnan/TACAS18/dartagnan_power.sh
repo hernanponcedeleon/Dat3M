4946cd613324a282526b72da74cf37def44ea6b1
Zeta <zeta96@gmail.com>
2018-03-02 22:28:07 +0100

    Delete dartagnan_power.sh

--- dartagnan/TACAS18/dartagnan_power.sh
+++
@@ -1,9 +0,0 @@
-for file in ../benchmarks/all_rx/*.pts;
-do
-   echo $file
-   START1=$(python -c 'import time; print time.time()')
-   timeout 600 java dartagnan/Dartagnan -t power -i $file
-   END1=$(python -c 'import time; print time.time()')
-   DIFF1=$(echo "$END1 - $START1" | bc)
-   echo $DIFF1
-done
