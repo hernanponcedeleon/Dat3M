884dd05c12e5b851de77d6c38ea7669d5bc89021
Natalia Gavrilenko <natalia.gavrilenko@aalto.fi>
2018-07-11 14:02:51 +0300

    Scripts updated to match new output

--- dartagnan/scripts/dartagnan_litmus_arm.sh
+++ dartagnan/scripts/dartagnan_litmus_arm.sh
@@ -1,9 +1,9 @@
 for file in litmus/PPC/*.litmus;
 do
    java dartagnan/Dartagnan -t arm -i $file > ./dart_arm.out
-   dart=$(grep -e 'not' dart_arm.out | wc -l)
+   dart=$(grep -e 'No' dart_arm.out | wc -l)
    herd7 -model cat/arm.cat $file > ./herd_arm.out
-   herd=$(grep -e 'Never' herd_arm.out | wc -l)
+   herd=$(grep -e 'No' herd_arm.out | wc -l)
    rm dart_arm.out
    rm herd_arm.out
    if [ $dart != $herd ]
--- dartagnan/scripts/dartagnan_litmus_tso.sh
+++ dartagnan/scripts/dartagnan_litmus_tso.sh
@@ -1,9 +1,9 @@
 for file in litmus/X86/*.litmus;
 do
    java dartagnan/Dartagnan -t tso -i $file > ./dart_tso.out
-   dart=$(grep -e 'not' dart_tso.out | wc -l)
+   dart=$(grep -e 'No' dart_tso.out | wc -l)
    herd7 -model cat/tso.cat $file > ./herd_tso.out
-   herd=$(grep -e 'Never' herd_tso.out | wc -l)
+   herd=$(grep -e 'No' herd_tso.out | wc -l)
    rm dart_tso.out
    rm herd_tso.out
    if [ $dart != $herd ]
