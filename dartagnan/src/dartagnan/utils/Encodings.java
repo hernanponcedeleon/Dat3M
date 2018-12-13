a2a64e2fde21194d6d25fd7a72a9ff4df675419a
Natalia Gavrilenko <natalia.gavrilenko@aalto.fi>
2018-12-13 14:28:13 +0200

    Minor refactoring of encodeCommonExecution

--- dartagnan/src/dartagnan/utils/Encodings.java
+++ dartagnan/src/dartagnan/utils/Encodings.java
@@ -6,84 +6,72 @@ import com.microsoft.z3.Model;
 import dartagnan.program.Program;
 import dartagnan.program.Register;
 import dartagnan.program.event.Event;
+import dartagnan.program.event.Load;
 import dartagnan.program.event.MemEvent;
+import dartagnan.program.event.Store;
 import dartagnan.program.event.utils.RegWriter;
 import dartagnan.program.memory.Location;
 import dartagnan.program.utils.EventRepository;
 import dartagnan.wmm.utils.Tuple;
 import dartagnan.wmm.utils.TupleSet;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Encodings {
 
 	public static BoolExpr encodeCommonExecutions(Program p1, Program p2, Context ctx) {
-		BoolExpr enc = ctx.mkTrue();
+		List<Event> p1Events = sortedByHlId(p1);
+        List<Event> p2Events = sortedByHlId(p2);
 
-		Set<Event> lEventsP1 = p1.getEventRepository().getEvents(EventRepository.MEMORY | EventRepository.LOCAL);
-		Set<Event> lEventsP2 = p2.getEventRepository().getEvents(EventRepository.MEMORY | EventRepository.LOCAL);
-		Set<Event> rEventsP1 = p1.getEventRepository().getEvents(EventRepository.LOAD);
-		Set<Event> wEventsP1 = p1.getEventRepository().getEvents(EventRepository.STORE | EventRepository.INIT);
-		Set<Event> rEventsP2 = p2.getEventRepository().getEvents(EventRepository.LOAD);
-		Set<Event> wEventsP2 = p2.getEventRepository().getEvents(EventRepository.STORE | EventRepository.INIT);
+        Iterator it1 = p1Events.iterator();
+        Iterator it2 = p2Events.iterator();
 
+        Set<Tuple> rTuples = new TupleSet();
+        Set<Tuple> wTuples = new TupleSet();
 
-		for(Event e1 : lEventsP1) {
-			for(Event e2 : lEventsP2) {
-				if(e1.getHLId() == e2.getHLId()) {
-					enc = ctx.mkAnd(enc, ctx.mkEq(e1.executes(ctx), e2.executes(ctx)));
-				}
-			}
-		}
+        BoolExpr enc = ctx.mkTrue();
 
-		Set<Tuple> rTuples = new TupleSet();
-		Set<Tuple> wTuples = new TupleSet();
+        while(it1.hasNext() && it2.hasNext()) {
+            Event e1 = (Event) it1.next();
+            Event e2 = (Event) it2.next();
 
-		for(Event r1 : rEventsP1) {
-			for(Event r2 : rEventsP2) {
-				if(r1.getHLId() == r2.getHLId()) {
-					enc = ctx.mkAnd(enc, ctx.mkEq(((MemEvent)r1).getAddressExpr(), ((MemEvent)r2).getAddressExpr()));
-					rTuples.add(new Tuple(r1, r2));
-					break;
-				}
-			}
-		}
+            if(e1.getHLId() != e2.getHLId()){
+                throw new RuntimeException("Invalid HLId");
+            }
+            enc = ctx.mkAnd(enc, ctx.mkEq(e1.executes(ctx), e2.executes(ctx)));
 
-		for(Event w1 : wEventsP1) {
-			for(Event w2 : wEventsP2) {
-				if(w1.getHLId() == w2.getHLId()) {
-					enc = ctx.mkAnd(enc, ctx.mkEq(((MemEvent)w1).getAddressExpr(), ((MemEvent)w2).getAddressExpr()));
-					wTuples.add(new Tuple(w1, w2));
-					break;
-				}
-			}
-		}
+            if(e1 instanceof Load && e2 instanceof Load){
+                rTuples.add(new Tuple(e1, e2));
 
-		for(Tuple rTuple : rTuples){
-			Event r1 = rTuple.getFirst();
-			Event r2 = rTuple.getSecond();
-			for(Tuple wTuple : wTuples){
-				enc = ctx.mkAnd(enc, ctx.mkEq(
-						Utils.edge("rf", wTuple.getFirst(), r1, ctx),
-						Utils.edge("rf", wTuple.getSecond(), r2, ctx)
-				));
-			}
-		}
+            } else if(e1 instanceof Store && e2 instanceof Store){
+                wTuples.add(new Tuple(e1, e2));
+            }
+        }
 
-		for(Tuple wTupleFrom : wTuples){
-			Event w1From = wTupleFrom.getFirst();
-			Event w2From = wTupleFrom.getSecond();
-			for(Tuple wTupleTo : wTuples){
-				if(w1From.getEId() != wTupleTo.getFirst().getEId()){
-					enc = ctx.mkAnd(enc, ctx.mkEq(
-							Utils.edge("co", w1From, wTupleTo.getFirst(), ctx),
-							Utils.edge("co", w2From, wTupleTo.getSecond(), ctx)
-					));
-				}
-			}
-		}
+        for(Tuple rTuple : rTuples){
+            Event r1 = rTuple.getFirst();
+            Event r2 = rTuple.getSecond();
+            for(Tuple wTuple : wTuples){
+                enc = ctx.mkAnd(enc, ctx.mkEq(
+                        Utils.edge("rf", wTuple.getFirst(), r1, ctx),
+                        Utils.edge("rf", wTuple.getSecond(), r2, ctx)
+                ));
+            }
+        }
+
+        for(Tuple wTupleFrom : wTuples){
+            Event w1From = wTupleFrom.getFirst();
+            Event w2From = wTupleFrom.getSecond();
+            for(Tuple wTupleTo : wTuples){
+                if(w1From.getEId() != wTupleTo.getFirst().getEId()){
+                    enc = ctx.mkAnd(enc, ctx.mkEq(
+                            Utils.edge("co", w1From, wTupleTo.getFirst(), ctx),
+                            Utils.edge("co", w2From, wTupleTo.getSecond(), ctx)
+                    ));
+                }
+            }
+        }
 		return enc;
 	}
 	
@@ -106,4 +94,12 @@ public class Encodings {
 		}
 		return reachedState;
 	}
+
+	private static List<Event> sortedByHlId(Program program){
+        return program.getEventRepository()
+                .getEvents(EventRepository.MEMORY | EventRepository.LOCAL)
+                .stream()
+                .sorted(Comparator.comparing(Event::getHLId))
+                .collect(Collectors.toList());
+    }
 }
