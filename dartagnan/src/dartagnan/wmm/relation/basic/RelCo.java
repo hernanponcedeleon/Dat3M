a0043354e794c73ba4fffdb8ff69f716283823c4
Natalia Gavrilenko <natalia.gavrilenko@aalto.fi>
2019-01-11 11:38:52 +0200

    Minor refactoring of co

--- dartagnan/src/dartagnan/wmm/relation/basic/RelCo.java
+++ dartagnan/src/dartagnan/wmm/relation/basic/RelCo.java
@@ -2,6 +2,7 @@ package dartagnan.wmm.relation.basic;
 
 import com.google.common.collect.ImmutableSetMultimap;
 import com.microsoft.z3.BoolExpr;
+import com.microsoft.z3.IntExpr;
 import dartagnan.program.event.Event;
 import dartagnan.program.event.MemEvent;
 import dartagnan.program.memory.Address;
@@ -12,7 +13,9 @@ import dartagnan.wmm.relation.Relation;
 import dartagnan.wmm.utils.Tuple;
 import dartagnan.wmm.utils.TupleSet;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 import static dartagnan.utils.Utils.edge;
@@ -62,11 +65,15 @@ public class RelCo extends Relation {
             enc = ctx.mkAnd(enc, ctx.mkEq(intVar("co", e, ctx), ctx.mkInt(1)));
         }
 
+        List<IntExpr> intVars = new ArrayList<>();
+        for(Event w : eventRepository.getEvents(EventRepository.STORE)) {
+            intVars.add(intVar("co", w, ctx));
+            enc = ctx.mkAnd(enc, ctx.mkGt(Utils.intVar("co", w, ctx), ctx.mkInt(1)));
+        }
+        enc = ctx.mkAnd(enc, ctx.mkDistinct(intVars.toArray(new IntExpr[0])));
+
         for(Event w :  writes){
             MemEvent w1 = (MemEvent)w;
-            enc = ctx.mkAnd(enc, ctx.mkImplies(w1.executes(ctx),
-                    ctx.mkGt(Utils.intVar("co", w1, ctx), ctx.mkInt(0))));
-
             BoolExpr lastCo = w1.executes(ctx);
 
             for(Tuple t : maxTupleSet.getByFirst(w1)){
@@ -78,11 +85,6 @@ public class RelCo extends Relation {
                         ctx.mkAnd(ctx.mkAnd(w1.executes(ctx), w2.executes(ctx)), ctx.mkEq(w1.getMemAddressExpr(), w2.getMemAddressExpr())),
                         ctx.mkLt(Utils.intVar("co", w1, ctx), Utils.intVar("co", w2, ctx))
                 )));
-
-                enc = ctx.mkAnd(enc, ctx.mkImplies(
-                        ctx.mkAnd(ctx.mkAnd(w1.executes(ctx), w2.executes(ctx)), ctx.mkEq(w1.getMemAddressExpr(), w2.getMemAddressExpr())),
-                        ctx.mkNot(ctx.mkEq(Utils.intVar("co", w1, ctx), Utils.intVar("co", w2, ctx)))
-                ));
             }
 
             BoolExpr lastCoExpr = ctx.mkBoolConst("co_last(" + w1.repr() + ")");
