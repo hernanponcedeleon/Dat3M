6deecb6341916b2cdbea5a2d5bc734be4758766b
Hernan Ponce de Leon <ponce@fortiss.org>
2019-08-06 11:22:11 +0200

    Added class for inline assertions

---
+++ dartagnan/src/main/java/com/dat3m/dartagnan/asserts/AssertInline.java
@@ -0,0 +1,24 @@
+package com.dat3m.dartagnan.asserts;
+
+import com.microsoft.z3.BoolExpr;
+import com.microsoft.z3.Context;
+import com.dat3m.dartagnan.program.event.Local;
+
+public class AssertInline extends AbstractAssert {
+
+    private final Local e;
+
+    public AssertInline(Local e){
+        this.e = e;
+    }
+
+    @Override
+    public BoolExpr encode(Context ctx) {
+		return ctx.mkAnd(e.exec(), ctx.mkEq(e.getResultRegisterExpr(), ctx.mkInt(0)));
+    }
+
+    @Override
+    public String toString(){
+        return "!" + e.getResultRegister();
+    }
+}
