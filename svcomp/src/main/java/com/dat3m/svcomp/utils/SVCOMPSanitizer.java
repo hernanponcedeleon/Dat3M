123ca1e3e94817c1c673d07fb0defeeedb90bb8b
Hernan Ponce de Leon <ponce@fortiss.org>
2019-10-02 14:04:46 +0200

    More things in the SVCOMP sanitizer

--- svcomp/src/main/java/com/dat3m/svcomp/utils/SVCOMPSanitizer.java
+++ svcomp/src/main/java/com/dat3m/svcomp/utils/SVCOMPSanitizer.java
@@ -24,13 +24,15 @@ public class SVCOMPSanitizer {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
 			PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(tmp)));
 			for (String line; (line = reader.readLine()) != null;) {
-				line = line.replace("void __VERIFIER_assert(int expression) { if (!expression) { ERROR: __VERIFIER_error(); }; return; }", "");					
+				line = line.replace("void __VERIFIER_assert(int expression) { if (!expression) { ERROR: __VERIFIER_error(); }; return; }", "");
+				line = line.replace("void __VERIFIER_assert(int expression) { if (!expression) { ERROR: __VERIFIER_error();}; return; }", "");
 				// SMACK does not create procedure for inline functions
 				if(!line.contains("__")) {
 					line = line.replace("inline ", "");	
 				}
-				if(line.contains("while(1) { pthread_create(&t, 0, thr1, 0); }")) {
-					line = line.replace("while(1) { pthread_create(&t, 0, thr1, 0); }", "");
+				if(line.contains("while(1) { pthread_create(&t, 0, thr1, 0); }") || line.contains("while(1) pthread_create(&t, 0, thr1, 0);")) {
+					line = line.replace("while(1) { pthread_create(&t, 0, thr1, 0); }", "");					
+					line = line.replace("while(1) pthread_create(&t, 0, thr1, 0);", "");
 					for(int i : IntStream.range(0, bound).toArray()) {
 						writer.println("pthread_create(&t, 0, thr1, 0);");
 					}
