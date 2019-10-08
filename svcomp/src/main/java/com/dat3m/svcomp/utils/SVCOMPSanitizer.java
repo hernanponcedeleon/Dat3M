ab61206fe8ed8b6ad5bb5e9f42e78bd0fc0fa1f9
Hernan Ponce de Leon <ponce@fortiss.org>
2019-10-08 18:53:23 +0200

    Sanitizer update

--- svcomp/src/main/java/com/dat3m/svcomp/utils/SVCOMPSanitizer.java
+++ svcomp/src/main/java/com/dat3m/svcomp/utils/SVCOMPSanitizer.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.stream.IntStream;
 
 import com.dat3m.dartagnan.parsers.program.utils.ParsingException;
 
@@ -26,22 +25,24 @@ public class SVCOMPSanitizer {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
 			PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(tmp)));
 			for (String line; (line = reader.readLine()) != null;) {
-				line = line.replace("void __VERIFIER_assert(int expression) { if (!expression) { ERROR: __VERIFIER_error(); }; return; }", "");
-				line = line.replace("void __VERIFIER_assert(int expression) { if (!expression) { ERROR: __VERIFIER_error();}; return; }", "");
-				line = line.replace("void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }", "");
-				line = line.replace("void __VERIFIER_assert(int cond) { if (!(cond)) { ERROR: __VERIFIER_error(); } return; }", "");
 				// SMACK does not create procedure for inline functions
 				if(!line.contains("__")) {
 					line = line.replace("inline ", "");	
 				}
 				if(line.contains("while(1) { pthread_create(&t, 0, thr1, 0); }") || line.contains("while(1) pthread_create(&t, 0, thr1, 0);")) {
-					line = line.replace("while(1) { pthread_create(&t, 0, thr1, 0); }", "");					
-					line = line.replace("while(1) pthread_create(&t, 0, thr1, 0);", "");
-					for(int i : IntStream.range(0, bound).toArray()) {
+					// While with empty body to force the creation of boundEvents
+					line = line.replace("while(1) { pthread_create(&t, 0, thr1, 0); }", "while(1) {}");					
+					line = line.replace("while(1) pthread_create(&t, 0, thr1, 0);", "while(1) {}");
+					int i = 0;
+					while(i < bound) {
 						writer.println("pthread_create(&t, 0, thr1, 0);");
+						i++;
 					}
 				}
-				if(line.contains("while") && line.contains("pthread_create")) {
+				if((line.contains("while") && line.contains("pthread_create"))
+						// TODO: remove this
+						|| line.contains("pthread_create(&t, 0, producer, 0);") 
+						|| line.contains("__VERIFIER_assume(n >= 20 && n < 40);")) {
 					reader.close();
 					writer.close();
 			        throw new ParsingException(line + " cannot be parsed");
