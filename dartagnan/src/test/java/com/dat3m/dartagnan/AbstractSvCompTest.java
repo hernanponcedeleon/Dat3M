a8068007836e3ae37c9dfd9d6ac1e98e45f6ccd4
ponce <ponce@12515.dynamic.rz.unibw-muenchen.de>
2020-01-31 13:49:45 +0100

    SVComp UnitTests

--- dartagnan/src/test/java/com/dat3m/dartagnan/AbstractSvCompTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/AbstractSvCompTest.java
@@ -1,10 +1,8 @@
 package com.dat3m.dartagnan;
 
-import com.dat3m.dartagnan.parsers.cat.ParserCat;
 import com.dat3m.dartagnan.parsers.program.ProgramParser;
 import com.dat3m.dartagnan.program.Program;
 import com.dat3m.dartagnan.utils.Settings;
-import com.dat3m.dartagnan.utils.ResourceHelper;
 import com.dat3m.dartagnan.utils.Result;
 import com.dat3m.dartagnan.wmm.Wmm;
 import com.dat3m.dartagnan.wmm.utils.Arch;
@@ -20,44 +18,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import static com.dat3m.dartagnan.utils.ResourceHelper.TEST_RESOURCE_PATH;
 import static com.dat3m.dartagnan.utils.Result.FAIL;
 import static com.dat3m.dartagnan.utils.Result.PASS;
 import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
 public abstract class AbstractSvCompTest {
-
-	@Parameterized.Parameters(name = "{index}: {0} bound={2}")
-    public static Iterable<Object[]> data() throws IOException {
-        Wmm wmm = new ParserCat().parse(new File(ResourceHelper.CAT_RESOURCE_PATH + "cat/svcomp.cat"));
-
-        List<Object[]> data = new ArrayList<>();
-
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/fib_bench-1.bpl", wmm, 6});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/fib_bench-2.bpl", wmm, 6});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/fib_bench_longer-1.bpl", wmm, 7});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/fib_bench_longer-2.bpl", wmm, 7});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/lazy01.bpl", wmm, 1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/triangular-1.bpl", wmm, 6});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/triangular-2.bpl", wmm, 6});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/gcd-2.bpl", wmm, 1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/read_write_lock-1.bpl", wmm, 1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/read_write_lock-2.bpl", wmm, 1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/18_read_write_lock.bpl", wmm, 1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/19_time_var_mutex.bpl", wmm, 2});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/safe000_pso.oepc.bpl", wmm, 2});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/safe001_tso.oepc.bpl", wmm, 2});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/thin000_pso.oepc.bpl", wmm, 2});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/race-1_1-join.bpl", wmm, 1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/race-1_2-join.bpl", wmm, 1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/race-1_3-join.bpl", wmm, 1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/concurrency/race-3_2-container_of-global.bpl", wmm, 1});
-        
-        return data;
-    }
 
     private String path;
     private Wmm wmm;
@@ -70,7 +36,7 @@ public abstract class AbstractSvCompTest {
         this.bound = bound;
     }
 
-    @Test(timeout = 120000)
+    @Test(timeout = 900000)
     public void test() {
         try {
         	String property = path.substring(0, path.lastIndexOf(".")) + ".yml";
---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/SvCompTestControlFlow.java
@@ -0,0 +1,44 @@
+package com.dat3m.dartagnan;
+
+import com.dat3m.dartagnan.parsers.cat.ParserCat;
+import com.dat3m.dartagnan.utils.ResourceHelper;
+import com.dat3m.dartagnan.wmm.Wmm;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import static com.dat3m.dartagnan.utils.ResourceHelper.TEST_RESOURCE_PATH;
+
+@RunWith(Parameterized.class)
+public class SvCompTestControlFlow extends AbstractSvCompTest {
+
+	@Parameterized.Parameters(name = "{index}: {0} bound={2}")
+    public static Iterable<Object[]> data() throws IOException {
+        Wmm wmm = new ParserCat().parse(new File(ResourceHelper.CAT_RESOURCE_PATH + "cat/svcomp.cat"));
+
+        List<Object[]> data = new ArrayList<>();
+
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/s3_srvr_1b.cil.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_5.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_6.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_7.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_8.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_9.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_10.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_11.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_12.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_13.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_14-1.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_14-2.bpl", wmm, 2});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "boogie/cf/test_locks_15-1.bpl", wmm, 2});
+        
+        return data;
+    }
+
+	public SvCompTestControlFlow(String path, Wmm wmm, int bound) {
+		super(path, wmm, bound);
+	}
+}
