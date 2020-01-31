e03c332145199fc440880765ecf169f607f1fd81
ponce <ponce@12515.dynamic.rz.unibw-muenchen.de>
2020-01-31 08:39:53 +0100

    Abstract test for svcomp

--- dartagnan/src/test/java/com/dat3m/dartagnan/AbstractSvCompTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/AbstractSvCompTest.java
@@ -1,19 +1,34 @@
 package com.dat3m.dartagnan;
 
 import com.dat3m.dartagnan.parsers.cat.ParserCat;
+import com.dat3m.dartagnan.parsers.program.ProgramParser;
+import com.dat3m.dartagnan.program.Program;
+import com.dat3m.dartagnan.utils.Settings;
 import com.dat3m.dartagnan.utils.ResourceHelper;
+import com.dat3m.dartagnan.utils.Result;
 import com.dat3m.dartagnan.wmm.Wmm;
+import com.dat3m.dartagnan.wmm.utils.Arch;
+import com.dat3m.dartagnan.wmm.utils.Mode;
+import com.dat3m.dartagnan.wmm.utils.alias.Alias;
+import com.microsoft.z3.Context;
+import com.microsoft.z3.Solver;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import static com.dat3m.dartagnan.utils.ResourceHelper.TEST_RESOURCE_PATH;
+import static com.dat3m.dartagnan.utils.Result.FAIL;
+import static com.dat3m.dartagnan.utils.Result.PASS;
+import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
-public class SvCompTestConcurrency extends AbstractSvCompTest {
+public abstract class AbstractSvCompTest {
 
 	@Parameterized.Parameters(name = "{index}: {0} bound={2}")
     public static Iterable<Object[]> data() throws IOException {
@@ -44,7 +59,45 @@ public class SvCompTestConcurrency extends AbstractSvCompTest {
         return data;
     }
 
-	public SvCompTestConcurrency(String path, Wmm wmm, int bound) {
-		super(path, wmm, bound);
+    private String path;
+    private Wmm wmm;
+    private int bound;
+    private Result expected;
+
+    public AbstractSvCompTest(String path, Wmm wmm, int bound) {
+        this.path = path;
+        this.wmm = wmm;
+        this.bound = bound;
+    }
+
+    @Test(timeout = 120000)
+    public void test() {
+        try {
+        	String property = path.substring(0, path.lastIndexOf(".")) + ".yml";
+        	expected = readExptected(property);
+        	
+            Program program = new ProgramParser().parse(new File(path));
+            Context ctx = new Context();
+            Settings settings = new Settings(Mode.KNASTER, Alias.CFIS, bound);
+            Solver solver = ctx.mkSolver(ctx.mkTactic(Settings.TACTIC));
+            assertTrue(Dartagnan.testProgram(solver, ctx, program, wmm, Arch.NONE, settings).equals(expected));
+            ctx.close();
+        } catch (IOException e){
+            fail("Missing resource file");
+        }
+    }
+
+	private Result readExptected(String property) {
+		try (BufferedReader br = new BufferedReader(new FileReader(new File(property)))) {
+		    while (!(br.readLine()).contains("unreach-call.prp")) {
+		       continue;
+		    }
+		    return br.readLine().contains("false") ? FAIL : PASS;
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+            System.exit(0);
+		}
+		return null;
 	}
 }
