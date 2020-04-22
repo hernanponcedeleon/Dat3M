71bb450f33967a7d1172a7d8da27593878c4045a
Hernan Ponce de Leon <zeta96@gmail.com>
2020-04-22 14:47:50 +0200

    Cleaned up compilations (libraries)

--- include/bits/gthr-default.h
+++
@@ -1,7 +0,0 @@
-//
-// This file is distributed under the MIT License. See LICENSE for details.
-//
-
-// Empty header file to prevent various pthread types from being redefined
-// when the standard header is included.
-
--- lib/errno.c
+++
@@ -1,7 +0,0 @@
-//
-// This file is distributed under the MIT License. See LICENSE for details.
-//
-#include <errno.h>
-
-static int errno_global;
-int *__errno_location(void) { return &errno_global; }
