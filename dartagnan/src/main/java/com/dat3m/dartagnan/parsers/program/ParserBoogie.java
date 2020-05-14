0378ac6a37dc16146f25d8d0d73c53df5669e922
Hernan Ponce de Leon <zeta96@gmail.com>
2020-05-14 20:17:33 +0200

    Removed NONE as the default arch for boogie programs

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/ParserBoogie.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/ParserBoogie.java
@@ -24,7 +24,6 @@ class ParserBoogie implements ParserInterface{
         VisitorBoogie visitor = new VisitorBoogie(pb);
 
         Program program = (Program) parserEntryPoint.accept(visitor);
-        program.setArch(Arch.NONE);
         return program;
     }
 }
