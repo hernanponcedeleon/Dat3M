d9f988d7b1b077485366e2edfb852afb76e9390d
Hernan Ponce de Leon <zeta96@gmail.com>
2020-08-04 11:13:57 +0200

    Small refactoring

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/event/Local.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/Local.java
@@ -15,6 +15,7 @@ import static com.dat3m.dartagnan.expression.INonDetTypes.UCHAR;
 
 import com.dat3m.dartagnan.expression.ExprInterface;
 import com.dat3m.dartagnan.expression.INonDet;
+import com.dat3m.dartagnan.expression.INonDetTypes;
 import com.dat3m.dartagnan.program.Register;
 import com.dat3m.dartagnan.program.event.utils.RegReaderData;
 import com.dat3m.dartagnan.program.event.utils.RegWriter;
@@ -77,17 +78,21 @@ public class Local extends Event implements RegWriter, RegReaderData {
 		Expr exprEnc = expr.toZ3Int(this, ctx);
 		if(expr instanceof INonDet) {
 			INonDet iNonDet = (INonDet)expr;
+			long min = iNonDet.getMin();
+			long max = iNonDet.getMax();
 			if(exprEnc.isBV()) {
-				if(iNonDet.getType().equals(UINT) || iNonDet.getType().equals(ULONG) || iNonDet.getType().equals(USHORT) || iNonDet.getType().equals(UCHAR)) {
-		        	enc = ctx.mkAnd(enc, ctx.mkBVUGE((BitVecExpr)exprEnc, ctx.mkBV(iNonDet.getMin(), iNonDet.getPrecision())));
-		        	enc = ctx.mkAnd(enc, ctx.mkBVULE((BitVecExpr)exprEnc, ctx.mkBV(iNonDet.getMax(), iNonDet.getPrecision())));					
+				int precision = iNonDet.getPrecision();
+				INonDetTypes type = iNonDet.getType();
+				if(type.equals(UINT) || type.equals(ULONG) || type.equals(USHORT) || type.equals(UCHAR)) {
+		        	enc = ctx.mkAnd(enc, ctx.mkBVUGE((BitVecExpr)exprEnc, ctx.mkBV(min, precision)));
+		        	enc = ctx.mkAnd(enc, ctx.mkBVULE((BitVecExpr)exprEnc, ctx.mkBV(max, precision)));					
 				} else {
-		        	enc = ctx.mkAnd(enc, ctx.mkBVSGE((BitVecExpr)exprEnc, ctx.mkBV(iNonDet.getMin(), iNonDet.getPrecision())));
-		        	enc = ctx.mkAnd(enc, ctx.mkBVSLE((BitVecExpr)exprEnc, ctx.mkBV(iNonDet.getMax(), iNonDet.getPrecision())));					
+		        	enc = ctx.mkAnd(enc, ctx.mkBVSGE((BitVecExpr)exprEnc, ctx.mkBV(min, precision)));
+		        	enc = ctx.mkAnd(enc, ctx.mkBVSLE((BitVecExpr)exprEnc, ctx.mkBV(max, precision)));					
 				}
 			} else {
-	        	enc = ctx.mkAnd(enc, ctx.mkGe((IntExpr)exprEnc, ctx.mkInt(iNonDet.getMin())));
-	        	enc = ctx.mkAnd(enc, ctx.mkLe((IntExpr)exprEnc, ctx.mkInt(iNonDet.getMax())));
+	        	enc = ctx.mkAnd(enc, ctx.mkGe((IntExpr)exprEnc, ctx.mkInt(min)));
+	        	enc = ctx.mkAnd(enc, ctx.mkLe((IntExpr)exprEnc, ctx.mkInt(max)));
 			}
 		}
 		return ctx.mkAnd(enc, ctx.mkEq(regResultExpr,  exprEnc));
