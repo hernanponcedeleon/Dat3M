f6d41c0449b5092d1f2c517e77cc9965940e8ad5
Hernan Ponce de Leon <zeta96@gmail.com>
2020-10-14 15:53:16 +0200

    Go back with printf

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/boogie/DummyProcedures.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/boogie/DummyProcedures.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class DummyProcedures {
 
 	public static List<String> DUMMYPROCEDURES = Arrays.asList(
-			"boogie_si_record", 
+			"boogie_si_record",
+			"printf",
 			"printk.");
 }	
