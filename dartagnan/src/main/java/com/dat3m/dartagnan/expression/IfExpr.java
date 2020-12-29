f2a4ee708a4e0ca6599ec63776c705695fb09ec9
Hernan Ponce de Leon <zeta96@gmail.com>
2020-12-29 13:46:32 +0100

    Implemented reduce() for IfExpr. It improves allocation

--- dartagnan/src/main/java/com/dat3m/dartagnan/expression/IfExpr.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/expression/IfExpr.java
@@ -59,7 +59,7 @@ public class IfExpr implements ExprInterface {
 
 	@Override
 	public IConst reduce() {
-		throw new UnsupportedOperationException("Reduce not supported for " + this);
+		return (IConst)(guard.reduce().getValue() > 0 ? tbranch.reduce() : fbranch.reduce());
 	}
 	
 	public BExpr getGuard() {
