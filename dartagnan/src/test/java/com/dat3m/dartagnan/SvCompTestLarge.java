8ff4bd8c5ee8a566f3b36450f3bd705691a499c5
Thomas Haas <tomy.haas@t-online.de>
2021-02-05 14:04:51 +0100

    - Added a simple unit test for large benchmarks

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/SvCompTestLarge.java
@@ -0,0 +1,95 @@
+package com.dat3m.dartagnan;
+
+import com.dat3m.dartagnan.parsers.cat.ParserCat;
+import com.dat3m.dartagnan.utils.ResourceHelper;
+import com.dat3m.dartagnan.utils.Settings;
+import com.dat3m.dartagnan.wmm.Wmm;
+import com.dat3m.dartagnan.wmm.utils.Mode;
+import com.dat3m.dartagnan.wmm.utils.alias.Alias;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.dat3m.dartagnan.utils.ResourceHelper.TEST_RESOURCE_PATH;
+
+/*
+NOTE: Currently we fail on pretty much all of the following tasks
+ */
+
+@RunWith(Parameterized.class)
+public class SvCompTestLarge extends AbstractSvCompTest {
+
+	@Parameterized.Parameters(name = "{index}: {0} bound={2}")
+    public static Iterable<Object[]> data() throws IOException {
+        Wmm wmm = new ParserCat().parse(new File(ResourceHelper.CAT_RESOURCE_PATH + "cat/svcomp.cat"));
+
+        Settings s1 = new Settings(Mode.KNASTER, Alias.CFIS, 1, false);
+        Settings s2 = new Settings(Mode.KNASTER, Alias.CFIS, 2, false);
+        Settings s3 = new Settings(Mode.KNASTER, Alias.CFIS, 3, false);
+        Settings s6 = new Settings(Mode.KNASTER, Alias.CFIS, 6, false);
+        Settings s7 = new Settings(Mode.KNASTER, Alias.CFIS, 7, false);
+
+        List<Object[]> data = new ArrayList<>();
+        String base = TEST_RESOURCE_PATH + "large/";
+        // mixXXX examples
+        data.add(new Object[]{base + "mix000_power.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "mix000_pso.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "mix000_rmo.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "mix000_tso.opt-O0.bpl", wmm, s2});
+
+        data.add(new Object[]{base + "mix003_power.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "mix003_pso.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "mix003_rmo.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "mix003_tso.opt-O0.bpl", wmm, s2});
+
+        // podwrXXX examples
+        data.add(new Object[]{base + "podwr000_power.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "podwr000_pso.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "podwr000_rmo.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "podwr000_tso.opt-O0.bpl", wmm, s2});
+
+        data.add(new Object[]{base + "podwr001_power.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "podwr001_pso.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "podwr001_rmo.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "podwr001_tso.opt-O0.bpl", wmm, s2});
+
+
+        // safeXXX examples
+        data.add(new Object[]{base + "safe000_power.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "safe000_pso.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "safe000_rmo.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "safe000_tso.opt-O0.bpl", wmm, s2});
+
+        data.add(new Object[]{base + "safe030_power.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "safe030_pso.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "safe030_rmo.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "safe030_tso.opt-O0.bpl", wmm, s2});
+
+        // rfiXXX examples
+        data.add(new Object[]{base + "rfi000_power.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "rfi000_pso.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "rfi000_rmo.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "rfi000_tso.opt-O0.bpl", wmm, s2});
+
+        data.add(new Object[]{base + "rfi009_power.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "rfi009_pso.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "rfi009_rmo.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "rfi009_tso.opt-O0.bpl", wmm, s2});
+
+        // thinXXX examples
+        data.add(new Object[]{base + "thin000_power.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "thin000_pso.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "thin000_rmo.opt-O0.bpl", wmm, s2});
+        data.add(new Object[]{base + "thin000_tso.opt-O0.bpl", wmm, s2});
+
+        return data;
+    }
+
+	public SvCompTestLarge(String path, Wmm wmm, Settings settings) {
+		super(path, wmm, settings);
+	}
+}
