d84e59b14a6fa1a0378764873db371eb12961a44
Hernan Ponce de Leon <zeta96@gmail.com>
2021-05-05 19:56:23 +0200

    TTAS using 3 threads

--- ttas.c
+++ ttas.c
@@ -44,19 +44,23 @@ int main()
 {
     lock_init(&my_lock);
 
-    pthread_t t1, t2;
+    pthread_t t1, t2, t3;
 
     if (pthread_create(&t1, NULL, thread_1, NULL))
         abort();
     if (pthread_create(&t2, NULL, thread_1, NULL))
+        abort();
+    if (pthread_create(&t3, NULL, thread_1, NULL))
         abort();
 
     if (pthread_join(t1, NULL))
         abort();
     if (pthread_join(t2, NULL))
         abort();
+    if (pthread_join(t3, NULL))
+        abort();
 
-    __VERIFIER_assert(a == 2);
+    __VERIFIER_assert(a == 3);
     
     return 0;
 }
