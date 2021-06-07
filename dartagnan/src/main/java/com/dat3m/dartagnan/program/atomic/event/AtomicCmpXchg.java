4f50a4060646cfaccb50e84c64b55daefbb89c36
Hernan Ponce de Leon <zeta96@gmail.com>
2021-06-07 12:55:09 +0200

    Refactored similar code for compilation of AtomicCmpXchg under
    POWER and ARM8

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/atomic/event/AtomicCmpXchg.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/atomic/event/AtomicCmpXchg.java
@@ -21,6 +21,7 @@ import static com.dat3m.dartagnan.expression.op.COpBin.EQ;
 import static com.dat3m.dartagnan.expression.op.COpBin.NEQ;
 import static com.dat3m.dartagnan.program.arch.aarch64.utils.Mo.*;
 import static com.dat3m.dartagnan.program.atomic.utils.Mo.*;
+import static com.dat3m.dartagnan.wmm.utils.Arch.POWER;
 
 public class AtomicCmpXchg extends AtomicAbstract implements RegWriter, RegReaderData {
 
@@ -58,24 +59,24 @@ public class AtomicCmpXchg extends AtomicAbstract implements RegWriter, RegReade
 
     @Override
     protected RecursiveFunction<Integer> compileRecursive(Arch target, int nextId, Event predecessor, int depth) {
-    	LinkedList<Event> events;
+    	Load load;
+    	Store store;
+    	LinkedList<Event> events = new LinkedList<>();
         switch(target) {
             case NONE: case TSO: {
                 Register dummy = new Register(null, resultRegister.getThreadId(), resultRegister.getPrecision());
-                RMWLoad load = new RMWLoad(dummy, address, mo);
+                load = new RMWLoad(dummy, address, mo);
                 Local casResult = new Local(resultRegister, new Atom(dummy, EQ, expected));
                 Label fail = new Label("CAS_fail");
                 Label endCas = new Label("CAS_end");
                 CondJump branch = new CondJump(new Atom(resultRegister, NEQ, IConst.ONE), fail);
-                RMWStore succStore = new RMWStore(load, address, value, mo);
+                store = new RMWStore((RMWLoad)load, address, value, mo);
                 CondJump jumpToEnd = new CondJump(BConst.TRUE, endCas);
                 Local updateReg = new Local(expected, dummy);
-
-                events = new LinkedList<>(Arrays.asList(load, casResult, branch, succStore, jumpToEnd, fail, updateReg, endCas));
-
-                return compileSequenceRecursive(target, nextId, predecessor, events, depth + 1);
+                events.addAll(Arrays.asList(load, casResult, branch, store, jumpToEnd, fail, updateReg, endCas));
+                break;
             }
-            case ARM8: {
+            case POWER: case ARM8: {
                 String loadMo;
                 String storeMo;
                 switch (mo) {
@@ -101,89 +102,43 @@ public class AtomicCmpXchg extends AtomicAbstract implements RegWriter, RegReade
                 }
 
                 Register dummy = new Register(null, resultRegister.getThreadId(), resultRegister.getPrecision());
-                RMWLoadExclusive load = new RMWLoadExclusive(dummy, address, loadMo);
+                load = new RMWLoadExclusive(dummy, address, loadMo);
                 Local casResult = new Local(resultRegister, new Atom(dummy, EQ, expected));
                 Label fail = new Label("CAS_fail");
                 Label endCas = new Label("CAS_end");
                 CondJump branch = new CondJump(new Atom(resultRegister, NEQ, IConst.ONE), fail);
                 // ---- CAS success ----
-                RMWStoreExclusive succStore = new RMWStoreExclusive(address, value, storeMo);
+                store = new RMWStoreExclusive(address, value, storeMo);
                 Register statusReg = new Register("status(" + getOId() + ")", resultRegister.getThreadId(), resultRegister.getPrecision());
-                RMWStoreExclusiveStatus status = new RMWStoreExclusiveStatus(statusReg, succStore);
+                RMWStoreExclusiveStatus status = new RMWStoreExclusiveStatus(statusReg, (RMWStoreExclusive)store);
                 Event jumpStoreFail = new CondJump(new Atom(statusReg, EQ, IConst.ONE), (Label) getThread().getExit());
                 jumpStoreFail.addFilters(EType.BOUND);
                 CondJump jumpToEndCas = new CondJump(BConst.TRUE, endCas);
                 // ---------------------
                 // ---- CAS Fail ----
                 Local updateReg = new Local(expected, dummy);
-                // ------------------
-
-                events = new LinkedList<>(Arrays.asList(load, casResult, branch, succStore, status, jumpStoreFail, jumpToEndCas, fail, updateReg, endCas));
-                return compileSequenceRecursive(target, nextId, predecessor, events, depth + 1);
-            }
-            case POWER: {
-                String loadMo;
-                String storeMo;
-                switch (mo) {
-                    case SC:
-                    case ACQ_REL:
-                        loadMo = ACQ;
-                        storeMo = REL;
-                        break;
-                    case ACQUIRE:
-                        loadMo = ACQ;
-                        storeMo = RX;
-                        break;
-                    case RELEASE:
-                        loadMo = RX;
-                        storeMo = REL;
-                        break;
-                    case RELAXED:
-                        loadMo = RX;
-                        storeMo = RX;
-                        break;
-                    default:
-                        throw new UnsupportedOperationException("Compilation to " + target + " is not supported for " + this);
-                }
-
-                Register dummy = new Register(null, resultRegister.getThreadId(), resultRegister.getPrecision());
-                RMWLoadExclusive load = new RMWLoadExclusive(dummy, address, loadMo);
-                Local casResult = new Local(resultRegister, new Atom(dummy, EQ, expected));
-                Label fail = new Label("CAS_fail");
-                Label endCas = new Label("CAS_end");
-                CondJump branch = new CondJump(new Atom(resultRegister, NEQ, IConst.ONE), fail);
-                // ---- CAS success ----
-                RMWStoreExclusive succStore = new RMWStoreExclusive(address, value, storeMo);
-                Register statusReg = new Register("status(" + getOId() + ")", resultRegister.getThreadId(), resultRegister.getPrecision());
-                RMWStoreExclusiveStatus status = new RMWStoreExclusiveStatus(statusReg, succStore);
-                Event jumpStoreFail = new CondJump(new Atom(statusReg, EQ, IConst.ONE), (Label) getThread().getExit());
-                jumpStoreFail.addFilters(EType.BOUND);
-                CondJump jumpToEndCas = new CondJump(BConst.TRUE, endCas);
-                // ---------------------
-                // ---- CAS Fail ----
-                Local updateReg = new Local(expected, dummy);
-                // ------------------
-
-                events = new LinkedList<>();
-                // --- Add Fence before ---
-                if (mo.equals(SC)) {
-                    events.addFirst(new Fence("Sync"));
-                } else if (storeMo.equals(REL)) {
-                    events.addFirst(new Fence("Lwsync"));
+               
+                // --- Add Fence before under POWER ---
+                if(target.equals(POWER)) {
+                    if (mo.equals(SC)) {
+                        events.addFirst(new Fence("Sync"));
+                    } else if (storeMo.equals(REL)) {
+                        events.addFirst(new Fence("Lwsync"));
+                    }                	
                 }
                 // --- Add success events ---
-                events.addAll(Arrays.asList(load, casResult, branch, succStore, status, jumpStoreFail));
-                // --- Add Fence after success ---
-                if (loadMo.equals(ACQ)) {
+                events.addAll(Arrays.asList(load, casResult, branch, store, status, jumpStoreFail));
+                // --- Add Fence after success under POWER ---
+                if (target.equals(POWER) && loadMo.equals(ACQ)) {
                     events.addLast(new Fence("Isync"));
                 }
                 // --- Add fail events + exit ---
                 events.addAll(Arrays.asList(jumpToEndCas, fail, updateReg, endCas));
-
-                return compileSequenceRecursive(target, nextId, predecessor, events, depth + 1);
+                break;
             }
             default:
                 throw new UnsupportedOperationException("Compilation to " + target + " is not supported for " + this);
         }
+        return compileSequenceRecursive(target, nextId, predecessor, events, depth + 1);
     }
 }
