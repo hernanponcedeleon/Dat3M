328cc6fb02d9a27d9b5b7380169f5fed155e32f8
Hernan Ponce de Leon <zeta96@gmail.com>
2021-06-10 13:15:24 +0200

    Added unit tests with relaxed accesses
    (expected results have not been tested and might be wrong)

--- dartagnan/src/test/java/com/dat3m/dartagnan/SafeCTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/SafeCTest.java
@@ -4,6 +4,7 @@ import com.dat3m.dartagnan.parsers.cat.ParserCat;
 import com.dat3m.dartagnan.parsers.program.ProgramParser;
 import com.dat3m.dartagnan.program.Program;
 import com.dat3m.dartagnan.utils.ResourceHelper;
+import com.dat3m.dartagnan.utils.Result;
 import com.dat3m.dartagnan.utils.Settings;
 import com.dat3m.dartagnan.verification.VerificationTask;
 import com.dat3m.dartagnan.wmm.Wmm;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.dat3m.dartagnan.utils.ResourceHelper.TEST_RESOURCE_PATH;
+import static com.dat3m.dartagnan.utils.Result.FAIL;
 import static com.dat3m.dartagnan.utils.Result.UNKNOWN;
 import static com.dat3m.dartagnan.wmm.utils.Arch.ARM8;
 import static com.dat3m.dartagnan.wmm.utils.Arch.POWER;
@@ -36,6 +38,7 @@ public class SafeCTest {
     private final Wmm wmm;
     private final Arch target;
     private final Settings settings;
+    private final Result expected;
 
 	@Parameterized.Parameters(name = "{index}: {0} target={2}")
     public static Iterable<Object[]> data() throws IOException {
@@ -47,30 +50,55 @@ public class SafeCTest {
 
         List<Object[]> data = new ArrayList<>();
 
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-O0.bpl", tso, TSO, s1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-O0.bpl", arm, ARM8, s1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-O0.bpl", power, POWER, s1});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-O0.bpl", tso, TSO, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-O0.bpl", arm, ARM8, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-O0.bpl", power, POWER, s1, UNKNOWN});
 
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-O0.bpl", tso, TSO, s1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-O0.bpl", arm, ARM8, s1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-O0.bpl", power, POWER, s1});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-acq2rx-O0.bpl", tso, TSO, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-acq2rx-O0.bpl", arm, ARM8, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-acq2rx-O0.bpl", power, POWER, s1, FAIL});
 
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-O0.bpl", tso, TSO, s1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-O0.bpl", arm, ARM8, s1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-O0.bpl", power, POWER, s1});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-rel2rx-O0.bpl", tso, TSO, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-rel2rx-O0.bpl", arm, ARM8, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ttas-5-rel2rx-O0.bpl", power, POWER, s1, FAIL});
 
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex_musl-4-O0.bpl", tso, TSO, s1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex_musl-4-O0.bpl", arm, ARM8, s1});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex_musl-4-O0.bpl", power, POWER, s1});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-O0.bpl", tso, TSO, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-O0.bpl", arm, ARM8, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-O0.bpl", power, POWER, s1, UNKNOWN});
+
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-acq2rx-O0.bpl", tso, TSO, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-acq2rx-O0.bpl", arm, ARM8, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-acq2rx-O0.bpl", power, POWER, s1, FAIL});
+
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-rel2rx-O0.bpl", tso, TSO, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-rel2rx-O0.bpl", arm, ARM8, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/ticketlock-6-rel2rx-O0.bpl", power, POWER, s1, FAIL});
+
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-O0.bpl", tso, TSO, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-O0.bpl", arm, ARM8, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-O0.bpl", power, POWER, s1, UNKNOWN});
+
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-acq2rx-futex-O0.bpl", tso, TSO, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-acq2rx-futex-O0.bpl", arm, ARM8, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-acq2rx-futex-O0.bpl", power, POWER, s1, FAIL});
+
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-O0-rel2rx-futex-O0.bpl", tso, TSO, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-O0-rel2rx-futex-O0.bpl", arm, ARM8, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex-4-O0-rel2rx-futex-O0.bpl", power, POWER, s1, FAIL});
+
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex_musl-4-O0.bpl", tso, TSO, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex_musl-4-O0.bpl", arm, ARM8, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/safe/mutex_musl-4-O0.bpl", power, POWER, s1, UNKNOWN});
 
         return data;
     }
 
-    public SafeCTest(String path, Wmm wmm, Arch target, Settings settings) {
+    public SafeCTest(String path, Wmm wmm, Arch target, Settings settings, Result expected) {
         this.path = path;
         this.wmm = wmm;
         this.target = target;
         this.settings = settings;
+        this.expected = expected;
     }
     
     @Test(timeout = TIMEOUT)
@@ -79,7 +107,7 @@ public class SafeCTest {
             Program program = new ProgramParser().parse(new File(path));
             Context ctx = new Context();
             VerificationTask task = new VerificationTask(program, wmm, target, settings);
-            assertEquals(UNKNOWN, com.dat3m.dartagnan.analysis.Base.runAnalysisAssumeSolver(ctx.mkSolver(), ctx, task));
+            assertEquals(expected, com.dat3m.dartagnan.analysis.Base.runAnalysisAssumeSolver(ctx.mkSolver(), ctx, task));
             ctx.close();
         } catch (IOException e){
             fail("Missing resource file");
