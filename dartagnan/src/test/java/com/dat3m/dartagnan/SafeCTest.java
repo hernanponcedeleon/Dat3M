3d6b2b05125bfc2d37a191257b8d9eb746eeb25a
Hernan Ponce de Leon <zeta96@gmail.com>
2021-06-10 15:21:43 +0200

    Updated expected results based on refinement

--- dartagnan/src/test/java/com/dat3m/dartagnan/SafeCTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/SafeCTest.java
@@ -91,10 +91,9 @@ public class SafeCTest {
         data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-acq2rx-futex-O0.bpl", tso, TSO, s1, UNKNOWN});
         data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-acq2rx-futex-O0.bpl", arm, ARM8, s1, UNKNOWN});
         data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-acq2rx-futex-O0.bpl", power, POWER, s1, UNKNOWN});
-
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", tso, TSO, s1, FAIL});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", arm, ARM8, s1, FAIL});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", power, POWER, s1, FAIL});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", tso, TSO, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", arm, ARM8, s1, UNKNOWN});
+        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", power, POWER, s1, UNKNOWN});
 
         // Known to be safe
         data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex_musl-4-O0.bpl", tso, TSO, s1, UNKNOWN});
