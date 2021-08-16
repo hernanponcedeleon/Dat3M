fb3f57ac52e3fce61c9fe6ef7a1ae6af19f93e9b
Hernan Ponce de Leon <zeta96@gmail.com>
2021-08-16 15:00:38 +0200

    Use JavaSMT excape mechanism to be able to use "\\" in RelMinus

--- dartagnan/src/main/java/com/dat3m/dartagnan/wmm/utils/Utils.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/wmm/utils/Utils.java
@@ -1,6 +1,7 @@
 package com.dat3m.dartagnan.wmm.utils;
 
 import org.sosy_lab.java_smt.api.BooleanFormula;
+import org.sosy_lab.java_smt.api.FormulaManager;
 import org.sosy_lab.java_smt.api.NumeralFormula.IntegerFormula;
 import org.sosy_lab.java_smt.api.SolverContext;
 
@@ -10,7 +11,8 @@ import com.dat3m.dartagnan.program.event.Event;
 public class Utils {
 
 	public static BooleanFormula edge(String relName, Event e1, Event e2, SolverContext ctx) {
-		return ctx.getFormulaManager().getBooleanFormulaManager().makeVariable(relName + "(" + e1.repr() + "," + e2.repr() + ")");
+		FormulaManager fmgr = ctx.getFormulaManager();
+		return fmgr.getBooleanFormulaManager().makeVariable(fmgr.escape(relName) + "(" + e1.repr() + "," + e2.repr() + ")");
 	}
 
 	public static BooleanFormula bindRegVal(Register register, Event w, Event r, SolverContext ctx){
@@ -18,10 +20,12 @@ public class Utils {
 	}
 
 	public static IntegerFormula intVar(String relName, Event e, SolverContext ctx) {
-		return ctx.getFormulaManager().getIntegerFormulaManager().makeVariable(relName + "(" + e.repr() + ")");
+		FormulaManager fmgr = ctx.getFormulaManager();
+		return fmgr.getIntegerFormulaManager().makeVariable(fmgr.escape(relName) + "(" + e.repr() + ")");
 	}
 	
 	public static IntegerFormula intCount(String relName, Event e1, Event e2, SolverContext ctx) {
-		return ctx.getFormulaManager().getIntegerFormulaManager().makeVariable(relName + "(" + e1.repr() + "," + e2.repr() + ")");
+		FormulaManager fmgr = ctx.getFormulaManager();
+		return fmgr.getIntegerFormulaManager().makeVariable(fmgr.escape(relName) + "(" + e1.repr() + "," + e2.repr() + ")");
 	}
 }
