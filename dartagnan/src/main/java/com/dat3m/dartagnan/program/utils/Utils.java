73a9356253748a4648ba499a9aaaa0cfcfddd8b9
Thomas Haas <tomy.haas@t-online.de>
2021-08-17 17:37:14 +0200

    Removed some unused code.

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/utils/Utils.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/utils/Utils.java
@@ -39,17 +39,6 @@ public class Utils {
 		throw new RuntimeException(String.format("Formulas %s and %s have different types or are of unsupported type for generalEqual", f1, f2));
 	}
 	
-	public static BooleanFormula generalGreaterThan(Formula f1, Formula f2, SolverContext ctx) {
-		FormulaManager fmgr = ctx.getFormulaManager();
-		if(f1 instanceof IntegerFormula && f2 instanceof IntegerFormula) {
-			return fmgr.getIntegerFormulaManager().greaterThan((IntegerFormula)f1, (IntegerFormula)f2);
-		}
-		if(f1 instanceof BitvectorFormula && f2 instanceof BitvectorFormula) {
-			return fmgr.getBitvectorFormulaManager().greaterThan((BitvectorFormula)f1, (BitvectorFormula)f2, false);
-		}
-		throw new RuntimeException(String.format("Formulas %s and %s have different types or are of unsupported type for generalEqual", f1, f2));
-	}
-	
     public static IntegerFormula convertToIntegerFormula(Formula f, SolverContext ctx) {
     	return f instanceof BitvectorFormula ? 
     			ctx.getFormulaManager().getBitvectorFormulaManager().toIntegerFormula((BitvectorFormula) f, false) : 
--- dartagnan/src/main/java/com/dat3m/dartagnan/wmm/utils/Utils.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/wmm/utils/Utils.java
@@ -1,12 +1,10 @@
 package com.dat3m.dartagnan.wmm.utils;
 
+import com.dat3m.dartagnan.program.event.Event;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.FormulaManager;
 import org.sosy_lab.java_smt.api.NumeralFormula.IntegerFormula;
 import org.sosy_lab.java_smt.api.SolverContext;
-
-import com.dat3m.dartagnan.program.Register;
-import com.dat3m.dartagnan.program.event.Event;
 
 public class Utils {
 
@@ -15,17 +13,9 @@ public class Utils {
 		return fmgr.getBooleanFormulaManager().makeVariable(fmgr.escape(relName) + "(" + e1.repr() + "," + e2.repr() + ")");
 	}
 
-	public static BooleanFormula bindRegVal(Register register, Event w, Event r, SolverContext ctx){
-		return ctx.getFormulaManager().getBooleanFormulaManager().makeVariable("BindRegVal(E" + w.getCId() + ",E" + r.getCId() + "," + register.getName() + ")");
-	}
-
 	public static IntegerFormula intVar(String relName, Event e, SolverContext ctx) {
 		FormulaManager fmgr = ctx.getFormulaManager();
 		return fmgr.getIntegerFormulaManager().makeVariable(fmgr.escape(relName) + "(" + e.repr() + ")");
 	}
-	
-	public static IntegerFormula intCount(String relName, Event e1, Event e2, SolverContext ctx) {
-		FormulaManager fmgr = ctx.getFormulaManager();
-		return fmgr.getIntegerFormulaManager().makeVariable(fmgr.escape(relName) + "(" + e1.repr() + "," + e2.repr() + ")");
-	}
+
 }
