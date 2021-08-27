b12879ae256aaac3594035828ea00d4a12ad16bd
Hernan Ponce de Leon <zeta96@gmail.com>
2021-08-27 19:20:21 +0200

    Timeout tests

--- dartagnan/src/test/java/com/dat3m/dartagnan/SafeCTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/SafeCTest.java
@@ -65,35 +65,35 @@ public class SafeCTest {
         data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ttas-5-rel2rx-O0.bpl", arm, ARM8, s1, FAIL});
         data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ttas-5-rel2rx-O0.bpl", power, POWER, s1, FAIL});
 
-        // Known to be safe
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-O0.bpl", tso, TSO, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-O0.bpl", arm, ARM8, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-O0.bpl", power, POWER, s1, UNKNOWN});
-        // We don't yet know what expected should be and currently we timeout
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-acq2rx-O0.bpl", tso, TSO, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-acq2rx-O0.bpl", arm, ARM8, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-acq2rx-O0.bpl", power, POWER, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-rel2rx-O0.bpl", tso, TSO, s1, UNKNOWN});
-        // These expected result were obtained from refinement. Cannot guarantee they are correct
-        // This two I expect to be correct
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-rel2rx-O0.bpl", arm, ARM8, s1, FAIL});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-rel2rx-O0.bpl", power, POWER, s1, FAIL});
-
-        // Known to be safe
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-O0.bpl", tso, TSO, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-O0.bpl", arm, ARM8, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-O0.bpl", power, POWER, s1, UNKNOWN});
-        // These expected result were obtained from refinement. Cannot guarantee they are correct
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-acq2rx-futex-O0.bpl", tso, TSO, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-acq2rx-futex-O0.bpl", arm, ARM8, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-acq2rx-futex-O0.bpl", power, POWER, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", tso, TSO, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", arm, ARM8, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", power, POWER, s1, UNKNOWN});
-
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex_musl-4-O0.bpl", tso, TSO, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex_musl-4-O0.bpl", arm, ARM8, s1, UNKNOWN});
-        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex_musl-4-O0.bpl", power, POWER, s1, UNKNOWN});
+//        // Known to be safe
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-O0.bpl", tso, TSO, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-O0.bpl", arm, ARM8, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-O0.bpl", power, POWER, s1, UNKNOWN});
+//        // We don't yet know what expected should be and currently we timeout
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-acq2rx-O0.bpl", tso, TSO, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-acq2rx-O0.bpl", arm, ARM8, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-acq2rx-O0.bpl", power, POWER, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-rel2rx-O0.bpl", tso, TSO, s1, UNKNOWN});
+//        // These expected result were obtained from refinement. Cannot guarantee they are correct
+//        // This two I expect to be correct
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-rel2rx-O0.bpl", arm, ARM8, s1, FAIL});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/ticketlock-6-rel2rx-O0.bpl", power, POWER, s1, FAIL});
+//
+//        // Known to be safe
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-O0.bpl", tso, TSO, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-O0.bpl", arm, ARM8, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-O0.bpl", power, POWER, s1, UNKNOWN});
+//        // These expected result were obtained from refinement. Cannot guarantee they are correct
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-acq2rx-futex-O0.bpl", tso, TSO, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-acq2rx-futex-O0.bpl", arm, ARM8, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-acq2rx-futex-O0.bpl", power, POWER, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", tso, TSO, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", arm, ARM8, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex-4-rel2rx-futex-O0.bpl", power, POWER, s1, UNKNOWN});
+//
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex_musl-4-O0.bpl", tso, TSO, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex_musl-4-O0.bpl", arm, ARM8, s1, UNKNOWN});
+//        data.add(new Object[]{TEST_RESOURCE_PATH + "locks/mutex_musl-4-O0.bpl", power, POWER, s1, UNKNOWN});
 
         return data;
     }
