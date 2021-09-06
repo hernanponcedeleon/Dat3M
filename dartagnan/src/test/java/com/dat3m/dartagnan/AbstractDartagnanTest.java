e819b79d3367d9c5c8a27fb90392c545c161fe58
Thomas Haas <tomy.haas@t-online.de>
2021-09-06 22:33:53 +0200

    - Reverted wrong commit (turned off the logs) - Added missing timeout

--- dartagnan/src/test/java/com/dat3m/dartagnan/AbstractDartagnanTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/AbstractDartagnanTest.java
@@ -123,7 +123,7 @@ public abstract class AbstractDartagnanTest {
         }
     }
 
-    //@Test
+    //@Test(timeout = TIMEOUT)
     public void testRefinement() {
         try (SolverContext ctx = TestHelper.createContext();
              ProverEnvironment prover = ctx.newProverEnvironment(ProverOptions.GENERATE_MODELS))
