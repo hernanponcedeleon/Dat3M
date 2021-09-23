98c863af5bb970012329621cf1dd0d65453e4feb
Thomas Haas <tomy.haas@t-online.de>
2021-09-23 18:02:00 +0200

    - Added new collections and iterators (mostly for testing right now)

--- dartagnan/src/main/java/com/dat3m/dartagnan/analysis/saturation/graphs/relationGraphs/utils/collections/EdgeSet.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/analysis/saturation/graphs/relationGraphs/utils/collections/EdgeSet.java
@@ -1,4 +1,4 @@
-package com.dat3m.dartagnan.analysis.saturation.graphs.relationGraphs.utils;
+package com.dat3m.dartagnan.analysis.saturation.graphs.relationGraphs.utils.collections;
 
 import com.dat3m.dartagnan.analysis.saturation.graphs.relationGraphs.Edge;
 
@@ -10,24 +10,6 @@ import java.util.stream.Stream;
     It is specialized to hold edges.
  */
 public class EdgeSet implements Set<Edge> {
-
-    // A list of primes to use as capacities
-    // Note: We assume that we don't have any edgeSet with more than 1151 edges
-    // TODO: Extend this list and prune most primes numbers (we don't need primes with minor differences)
-    private static final List<Integer> PRIMES = Arrays.asList(
-            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
-            101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
-            211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,
-            307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
-            401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499,
-            503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,
-            601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,
-            701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
-            809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887,
-            907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997,
-            1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069,
-            1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151
-    );
 
     private static final List<Integer> powersOfTwo;
 
@@ -53,9 +35,6 @@ public class EdgeSet implements Set<Edge> {
 
     private static int getHashIndex(int hashCode, int length) {
         return hashCode & (length-1);
-        //final int result = hashCode % length;
-        //return result < 0 ? result + length : result;
-        //return Math.floorMod(hashCode, length);
     }
 
     private int findSlot(Edge key) {
@@ -76,16 +55,6 @@ public class EdgeSet implements Set<Edge> {
 
     private void resize(int newCapacity) {
         assert numElements < newCapacity;
-
-        // Find next prime number
-        // Little trick to force this number to be even so binary search is guaranteed to give us a negative index
-        /*if (newCapacity < PRIMES.get(PRIMES.size() - 1)) {
-            newCapacity = newCapacity & (~1);
-            final int nextPrimeIndex = (~Collections.binarySearch(PRIMES, newCapacity));
-            newCapacity = PRIMES.get(nextPrimeIndex);
-        } else {
-            newCapacity |= 1;
-        }*/
 
         newCapacity = powersOfTwo.get(~Collections.binarySearch(powersOfTwo, newCapacity | 1));
 
---
+++ dartagnan/src/main/java/com/dat3m/dartagnan/analysis/saturation/graphs/relationGraphs/utils/iteration/ReusableIterator.java
@@ -0,0 +1,24 @@
+package com.dat3m.dartagnan.analysis.saturation.graphs.relationGraphs.utils.iteration;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/*
+    TODO: This iterator should ideally be used via try-finally to guarantee a return to the IteratorPool.
+ */
+public abstract class ReusableIterator<T, V extends Collection<T>> implements Iterator<T>, AutoCloseable {
+    protected final IteratorPool<T, V> pool;
+    protected boolean isInUse;
+
+    public ReusableIterator(IteratorPool<T,V> pool) {
+        this.pool = pool;
+        this.isInUse = false;
+    }
+
+    public abstract void reuseFor(V collection);
+
+    @Override
+    public void close() {
+        pool.returnToPool(this);
+    }
+}
