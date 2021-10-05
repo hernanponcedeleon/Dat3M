09104cdb3853d2c899e77cf0b3d58e6a9e5d60df
Hernan Ponce de Leon <zeta96@gmail.com>
2021-10-05 10:57:24 +0200

    Fixed axis

--- output/plot.py
+++ output/plot.py
@@ -5,30 +5,30 @@ from PIL import Image
 
 path = '/Users/ponce/git/Dat3M/output/csv/'
 
-methods = ["assume", "refinement"]
-arch = ["TSO", "Power", "ARM8"]
-
-genmc = pd.read_csv(path + 'genMCTest-genMC.csv')
-genmc_df = pd.DataFrame(genmc)
-
-df = df_empty = pd.DataFrame({'benchmark' : []})
-df['benchmark'] = genmc.iloc[:, 0].apply(lambda x: x.replace(".c", ""))
-df['genmc'] = genmc.iloc[:, 1]
-
-for a in arch:
-    for m in methods:
-        current_df = pd.DataFrame(pd.read_csv(path + "CLocksTest-" + m + "-" + a + ".csv"))
-        df[m] = current_df.iloc[:, 1]
-
-    df.set_index('benchmark').sort_index().plot.bar(log=True, width=0.8)
-    plt.title(a)
-    plt.xticks(rotation=45, ha="right")
-    plt.xlabel("")
-    plt.ylabel("Time (ms)")
-    plt.tight_layout()
-    plt.legend(loc='upper left', bbox_to_anchor=(0.01, 1.025))
-    plt.savefig(a + ".png")
-    Image.open(a + ".png").show()
+#methods = ["assume", "refinement"]
+#arch = ["TSO", "Power", "ARM8"]
+#
+#genmc = pd.read_csv(path + 'genMCTest-genMC.csv')
+#genmc_df = pd.DataFrame(genmc)
+#
+#df = df_empty = pd.DataFrame({'benchmark' : []})
+#df['benchmark'] = genmc.iloc[:, 0].apply(lambda x: x.replace(".c", ""))
+#df['genmc'] = genmc.iloc[:, 1]
+#
+#for a in arch:
+#    for m in methods:
+#        current_df = pd.DataFrame(pd.read_csv(path + "CLocksTest-" + m + "-" + a + ".csv"))
+#        df[m] = current_df.iloc[:, 1]
+#
+#    df.set_index('benchmark').sort_index().plot.bar(log=True, width=0.8)
+#    plt.title(a)
+#    plt.xticks(rotation=45, ha="right")
+#    plt.xlabel("")
+#    plt.ylabel("Time (ms)")
+#    plt.tight_layout()
+#    plt.legend(loc='upper left', bbox_to_anchor=(0.01, 1.025))
+#    plt.savefig(a + ".png")
+#    Image.open(a + ".png").show()
 
 arch = ["TSO", "Power", "ARM8", "Linux"]
 
@@ -49,6 +49,17 @@ for a in arch:
     ax.plot([0, 1], [0, 1], color='r', transform=ax.transAxes)
     plt.title(a)
     plt.xlabel("Two solvers time (ms)")
+    limit = 1000
+    if a == "TSO":
+        limit = 60
+    if a == "Power":
+        limit = 120
+    if a == "ARM8":
+        limit = 90
+    if a == "Linux":
+        limit = 1000
+    plt.xlim([0, limit])
+    plt.ylim([0, limit])
     plt.ylabel("Refinement time (ms)")
     plt.plot(mapping["two-" + a].iloc[:, 1], mapping["refinement-" + a].iloc[:, 1], 's')
     plt.savefig("litmus-" + a + ".png")
