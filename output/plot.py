3470d7d21aefa9cbfa932e8ef0579cf7e822b4b4
Hernan Ponce de Leon <zeta96@gmail.com>
2021-10-05 20:44:24 +0200

    Improved plot

--- output/plot.py
+++ output/plot.py
@@ -5,16 +5,6 @@ from PIL import Image
 
 path = '/Users/ponce/git/Dat3M/output/csv/'
 
-methods = ["assume", "refinement"]
-arch = ["TSO", "Power", "ARM8"]
-
-genmc = pd.read_csv(path + 'genMCTest-genMC.csv')
-genmc_df = pd.DataFrame(genmc)
-
-df = df_empty = pd.DataFrame({'benchmark' : []})
-df['benchmark'] = genmc.iloc[:, 0].apply(lambda x: x.replace(".c", ""))
-df['GenMC'] = genmc.iloc[:, 1]
-
 mapping_method = dict([
     ('assume', 'Dartagnan'),
     ('refinement', 'Refinement')
@@ -28,21 +18,38 @@ mapping_title = dict([
 ])
 
 mapping_files = dict([
-    ('two-TSO', pd.read_csv(path + 'DartagnanX86Test-two.csv')),
-    ('refinement-TSO', pd.read_csv(path + 'DartagnanX86Test-refinement.csv')),
-    ('two-Power', pd.read_csv(path + 'DartagnanPPCTest-two.csv')),
-    ('refinement-Power', pd.read_csv(path + 'DartagnanPPCTest-refinement.csv')),
-    ('two-ARM8', pd.read_csv(path + 'DartagnanAARCH64Test-two.csv')),
-    ('refinement-ARM8', pd.read_csv(path + 'DartagnanAARCH64Test-refinement.csv')),
-    ('two-Linux', pd.read_csv(path + 'DartagnanLinuxTest-two.csv')),
-    ('refinement-Linux', pd.read_csv(path + 'DartagnanLinuxTest-refinement.csv'))
+    ('two-TSO', pd.read_csv(path + 'X86Test-two.csv')),
+    ('refinement-TSO', pd.read_csv(path + 'X86Test-refinement.csv')),
+    ('two-Power', pd.read_csv(path + 'PPCTest-two.csv')),
+    ('refinement-Power', pd.read_csv(path + 'PPCTest-refinement.csv')),
+    ('two-ARM8', pd.read_csv(path + 'AARCH64Test-two.csv')),
+    ('refinement-ARM8', pd.read_csv(path + 'AARCH64Test-refinement.csv')),
+    ('two-Linux', pd.read_csv(path + 'LinuxTest-two.csv')),
+    ('refinement-Linux', pd.read_csv(path + 'LinuxTest-refinement.csv'))
 ])
+
+##################################################
+### Generates bar char for the lock benchmarks ###
+##################################################
+
+methods = ["assume", "refinement"]
+arch = ["TSO", "Power", "ARM8"]
+
+genmc = pd.read_csv(path + 'genMCTest-genMC.csv')
+genmc_df = pd.DataFrame(genmc)
+
+df = df_empty = pd.DataFrame({'benchmark' : []})
+df['benchmark'] = genmc.iloc[:, 0].apply(lambda x: x.replace(".c", ""))
+## colums are: benchmark, result, time
+df['GenMC'] = genmc.iloc[:, 2]
 
 for a in arch:
     for m in methods:
         current_df = pd.DataFrame(pd.read_csv(path + "CLocksTest-" + m + "-" + a + ".csv"))
-        df[mapping_method[m]] = current_df.iloc[:, 1]
+        ## colums are: benchmark, result, time
+        df[mapping_method[m]] = current_df.iloc[:, 2]
 
+    plt.figure()
     df.set_index('benchmark').sort_index().plot.bar(log=True, width=0.8)
     plt.title(mapping_title[a])
     plt.xticks(rotation=45, ha="right")
@@ -51,6 +58,33 @@ for a in arch:
     plt.tight_layout()
     plt.legend(loc='upper left', bbox_to_anchor=(0.01, 1.025))
     plt.savefig("Figures/" + a + ".png")
+    plt.close()
+
+##################################################
+### Generates table for verification result of the lock benchmarks ###
+##################################################
+
+df = df_empty = pd.DataFrame({'benchmark' : []})
+df['benchmark'] = genmc.iloc[:, 0].apply(lambda x: x.replace(".c", ""))
+## colums are: benchmark, result, time
+df['GenMC'] = genmc.iloc[:, 1]
+
+for a in arch:
+    for m in methods:
+        current_df = pd.DataFrame(pd.read_csv(path + "CLocksTest-" + m + "-" + a + ".csv"))
+        ## colums are: benchmark, result, time
+        df[mapping_method[m]] = current_df.iloc[:, 1]
+
+    plt.figure()
+    plt.table(cellText=df.values,
+        colLabels=df.columns,
+        loc="center")
+    plt.savefig("Figures/table-" + a + ".png")
+    plt.close()
+
+##################################################
+### Generates plots for the litmus tests ###
+##################################################
 
 arch = ["TSO", "Power", "ARM8", "Linux"]
 
@@ -73,8 +107,8 @@ for a in arch:
     plt.ylim([0, limit])
     plt.ylabel("Refinement time (ms)")
     plt.plot(
-        mapping_files["two-" + a].iloc[:, 1],
-        mapping_files["refinement-" + a].iloc[:, 1],
+        mapping_files["two-" + a].iloc[:, 2],
+        mapping_files["refinement-" + a].iloc[:, 2],
         's')
     plt.savefig("Figures/litmus-" + a + ".png")
     plt.close()
