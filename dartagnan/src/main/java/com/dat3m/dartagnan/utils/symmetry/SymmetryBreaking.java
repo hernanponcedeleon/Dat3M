a3b924ccafe3a071611d217c96af0c662184e23a
Thomas Haas <tomy.haas@t-online.de>
2021-12-07 13:20:53 +0100

    - Imrpoved symmetry breaking

--- dartagnan/src/main/java/com/dat3m/dartagnan/utils/symmetry/SymmetryBreaking.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/utils/symmetry/SymmetryBreaking.java
@@ -5,31 +5,38 @@ import com.dat3m.dartagnan.program.event.Event;
 import com.dat3m.dartagnan.program.utils.EType;
 import com.dat3m.dartagnan.utils.equivalence.EquivalenceClass;
 import com.dat3m.dartagnan.verification.VerificationTask;
+import com.dat3m.dartagnan.wmm.axiom.Axiom;
 import com.dat3m.dartagnan.wmm.relation.Relation;
+import com.dat3m.dartagnan.wmm.utils.RelationRepository;
 import com.dat3m.dartagnan.wmm.utils.Tuple;
+import com.dat3m.dartagnan.wmm.utils.TupleSet;
+import com.google.common.base.Preconditions;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.BooleanFormulaManager;
 import org.sosy_lab.java_smt.api.SolverContext;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import static com.dat3m.dartagnan.wmm.relation.RelationNameRepository.RF;
+import static com.dat3m.dartagnan.GlobalSettings.*;
 
 
 // A first rough implementation for symmetry breaking
 public class SymmetryBreaking {
 
-    private static final int LEX_LEADER_SIZE = 1000;
-
+    private final VerificationTask task;
     private final ThreadSymmetry symm;
-    private final Relation rf;
+    private final Relation rel;
 
     public SymmetryBreaking(VerificationTask task) {
+        this.task = task;
         this.symm = task.getThreadSymmetry();
-        this.rf = task.getMemoryModel().getRelationRepository().getRelation(RF);
+
+        RelationRepository repo = task.getMemoryModel().getRelationRepository();
+        Preconditions.checkState(repo.containsRelation(BREAK_SYMMETRY_ON_RELATION),
+                "Unknown relation: " + BREAK_SYMMETRY_ON_RELATION);
+        this.rel = repo.getRelation(BREAK_SYMMETRY_ON_RELATION);
 
     }
 
@@ -59,32 +66,72 @@ public class SymmetryBreaking {
         // These need to get skipped!
         Thread t1 = symmThreads.get(0);
         List<Tuple> r1 = new ArrayList<>();
-        for (Tuple t : rf.getMaxTupleSet()) {
+        for (Tuple t : rel.getMaxTupleSet()) {
             Event a = t.getFirst();
             Event b = t.getSecond();
             if (!a.is(EType.PTHREAD) && !b.is(EType.PTHREAD) && a.getThread() == t1) {
                 r1.add(t);
             }
         }
-        r1.sort(Comparator.naturalOrder());
+        sort(r1);
 
         // Construct symmetric rows
         for (int i = 1; i < symmThreads.size(); i++) {
             Thread t2 = symmThreads.get(i);
             Function<Event, Event> p = symm.createTransposition(t1, t2);
-            List<Tuple> r2 = new ArrayList<>();
-            for (Tuple t : r1) {
-                r2.add(mapTuple(t, p));
-                // assert rf.getMaxTupleSet().contains(r2.get(r2.size() - 1));
-            }
-
-
+            List<Tuple> r2 = r1.stream().map(t -> mapTuple(t, p)).collect(Collectors.toList());
             enc = bmgr.and(enc, encodeLexLeader(rep, i, r1, r2, ctx));
             t1 = t2;
             r1 = r2;
         }
 
         return enc;
+    }
+
+    private void sort(List<Tuple> row) {
+        if (!BREAK_SYMMETRY_BY_SYNC_DEGREE) {
+            row.sort(Comparator.naturalOrder());
+            return;
+        }
+
+        // Setup of data structures
+        Map<Axiom, Map<Event, Integer>> inDegrees = new HashMap<>();
+        Map<Axiom, Map<Event, Integer>> outDegrees = new HashMap<>();
+        Set<Event> inEvents = new HashSet<>();
+        Set<Event> outEvents = new HashSet<>();
+        for (Tuple t : row) {
+            inEvents.add(t.getFirst());
+            outEvents.add(t.getSecond());
+        }
+
+        // Compute sync degrees per axiom
+        for (Axiom ax : task.getAxioms()) {
+            TupleSet mustSet = ax.getRelation().getMinTupleSet();
+            Map<Event, Integer> in = inDegrees.computeIfAbsent(ax, key -> new HashMap<>());
+            Map<Event, Integer> out = outDegrees.computeIfAbsent(ax, key -> new HashMap<>());
+
+            for (Event e : inEvents) {
+                in.put(e, mustSet.getBySecond(e).size());
+            }
+            for (Event e : outEvents) {
+                out.put(e, mustSet.getByFirst(e).size());
+            }
+        }
+
+        // Combine sync degrees of axioms
+        Map<Event, Integer> combInDegree = new HashMap<>(inEvents.size());
+        Map<Event, Integer> combOutDegree = new HashMap<>(outEvents.size());
+        for (Axiom ax : task.getAxioms()) {
+            for (Map.Entry<Event, Integer> entry : inDegrees.get(ax).entrySet()) {
+                combInDegree.compute(entry.getKey(), (k, v) -> Math.max(v == null ? -1 : v, entry.getValue()));
+            }
+            for (Map.Entry<Event, Integer> entry : outDegrees.get(ax).entrySet()) {
+                combOutDegree.compute(entry.getKey(), (k, v) -> Math.max(v == null ? -1 : v, entry.getValue()));
+            }
+        }
+
+        // Sort by sync degrees
+        row.sort(Comparator.<Tuple>comparingInt(t -> combInDegree.get(t.getFirst()) * combOutDegree.get(t.getSecond())).reversed());
     }
 
     /*
@@ -102,8 +149,8 @@ public class SymmetryBreaking {
         // From x1 to x(n-1)
         for (int i = 1; i < size; i++) {
             BooleanFormula y = bmgr.makeVariable("y" + i + suffix);
-            BooleanFormula orig = rf.getSMTVar(r1.get(i-1), ctx);
-            BooleanFormula perm = rf.getSMTVar(r2.get(i-1), ctx);
+            BooleanFormula orig = rel.getSMTVar(r1.get(i-1), ctx);
+            BooleanFormula perm = rel.getSMTVar(r2.get(i-1), ctx);
             enc = bmgr.and(
                     enc,
                     bmgr.or(y, bmgr.not(ylast), bmgr.not(orig)),
@@ -113,8 +160,8 @@ public class SymmetryBreaking {
             ylast = y;
         }
         // Final iteration is handled differently
-        BooleanFormula orig = rf.getSMTVar(r1.get(size-1), ctx);
-        BooleanFormula perm = rf.getSMTVar(r2.get(size-1), ctx);
+        BooleanFormula orig = rel.getSMTVar(r1.get(size-1), ctx);
+        BooleanFormula perm = rel.getSMTVar(r2.get(size-1), ctx);
         enc = bmgr.and(enc, bmgr.or(bmgr.not(ylast), bmgr.not(orig), perm));
 
 
