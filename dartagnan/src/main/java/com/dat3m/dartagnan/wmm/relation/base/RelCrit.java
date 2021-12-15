b3bc6637b62001c243c3b267fb02a86270575dc3
René Pascal Maseli <r.maseli@tu-bs.de>
2021-12-15 23:49:02 +0100

    fix RelCrit#fetchMinTupleSet()

--- dartagnan/src/main/java/com/dat3m/dartagnan/wmm/relation/base/RelCrit.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/wmm/relation/base/RelCrit.java
@@ -3,7 +3,11 @@ package com.dat3m.dartagnan.wmm.relation.base;
 import com.dat3m.dartagnan.program.Thread;
 import com.dat3m.dartagnan.program.arch.linux.utils.EType;
 import com.dat3m.dartagnan.program.event.Event;
+import com.dat3m.dartagnan.program.utils.ThreadCache;
+import com.dat3m.dartagnan.utils.equivalence.BranchEquivalence;
+import com.dat3m.dartagnan.verification.VerificationTask;
 import com.dat3m.dartagnan.wmm.filter.FilterBasic;
+import com.dat3m.dartagnan.wmm.filter.FilterUnion;
 import com.dat3m.dartagnan.wmm.relation.base.stat.StaticRelation;
 import com.dat3m.dartagnan.wmm.utils.Tuple;
 import com.dat3m.dartagnan.wmm.utils.TupleSet;
@@ -11,15 +15,49 @@ import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.BooleanFormulaManager;
 import org.sosy_lab.java_smt.api.SolverContext;
 
+import java.util.List;
+
 import static com.dat3m.dartagnan.wmm.relation.RelationNameRepository.CRIT;
 
 public class RelCrit extends StaticRelation {
     //TODO: We can optimize this a lot by using branching analysis
-	//TODO minTupleSet
+
+	private boolean fetchedMinTupleSet;
 
     public RelCrit(){
         term = CRIT;
     }
+
+	@Override
+	public void initialise(VerificationTask task, SolverContext ctx) {
+		super.initialise(task, ctx);
+		fetchedMinTupleSet = false;
+	}
+
+	@Override
+	public void fetchMinTupleSet() {
+		if(fetchedMinTupleSet) {
+			return;
+		}
+		fetchedMinTupleSet = true;
+		FilterBasic filterLock = FilterBasic.get(EType.RCU_LOCK);
+		FilterBasic filterUnlock = FilterBasic.get(EType.RCU_UNLOCK);
+		FilterUnion filterBoth = FilterUnion.get(filterLock,filterUnlock);
+		BranchEquivalence eq = task.getBranchEquivalence();
+		for(Event lock : task.getProgram().getCache().getEvents(filterLock)) {
+			ThreadCache thread = lock.getThread().getCache();
+			int idLock = lock.getCId();
+			List<Event> intermediates = thread.getEvents(filterBoth);
+			for(Event unlock : thread.getEvents(filterUnlock)) {
+				int idUnlock = unlock.getCId();
+				if(idLock < idUnlock && intermediates.stream().noneMatch(e->idLock<e.getCId() && e.getCId()<idUnlock
+						&& !eq.areMutuallyExclusive(lock,e) && !eq.areMutuallyExclusive(e,unlock))) {
+					minTupleSet.add(new Tuple(lock,unlock));
+				}
+			}
+		}
+		removeMutuallyExclusiveTuples(minTupleSet);
+	}
 
     @Override
     public TupleSet getMaxTupleSet(){
