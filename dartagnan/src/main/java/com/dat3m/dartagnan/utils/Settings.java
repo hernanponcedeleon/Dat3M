d0490f4cc2786fc57349a688573768c597e3d312
Hernan Ponce de Leon <zeta96@gmail.com>
2021-12-21 16:58:53 +0100

    Removed unused encoding options for RF

--- dartagnan/src/main/java/com/dat3m/dartagnan/utils/Settings.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/utils/Settings.java
@@ -1,27 +1,11 @@
 package com.dat3m.dartagnan.utils;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Settings {
 
     public static final String TACTIC = "qfbv";
     
-    // TODO: UI and console options to set these flags
-    public static final int FLAG_USE_SEQ_ENCODING_REL_RF            = 1;
-    public static final int FLAG_CAN_ACCESS_UNINITIALIZED_MEMORY    = 2;
-
     private final int bound;
     private final int solver_timeout;
-
-    private final Map<Integer, Boolean> flags = createFlags();
-
-    private static Map<Integer, Boolean> createFlags() {
-        Map<Integer, Boolean> flags = new HashMap<>(2);
-        flags.put(FLAG_USE_SEQ_ENCODING_REL_RF, true);
-        flags.put(FLAG_CAN_ACCESS_UNINITIALIZED_MEMORY, false);
-        return flags;
-    }
 
     public Settings(int bound, int solver_timeout){
         this.bound = Math.max(1, bound);
@@ -38,20 +22,6 @@ public class Settings {
 
     public boolean hasSolverTimeout(){
         return solver_timeout > 0;
-    }
-
-    public boolean getFlag(int flag){
-        if(!flags.containsKey(flag)){
-            throw new UnsupportedOperationException("Unrecognized settings flag");
-        }
-        return flags.get(flag);
-    }
-
-    public void setFlag(int flag, boolean value){
-        if(!flags.containsKey(flag)){
-            throw new UnsupportedOperationException("Unrecognized settings flag");
-        }
-        flags.put(flag, value);
     }
 
     @Override
