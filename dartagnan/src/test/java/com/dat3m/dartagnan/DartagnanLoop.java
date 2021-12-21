38a4d6c8daaa79d893635a591d5082c4213988ff
Hernan Ponce de Leon <zeta96@gmail.com>
2021-12-21 11:32:10 +0100

    Fixed #151

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/DartagnanLoop.java
@@ -0,0 +1,46 @@
+package com.dat3m.dartagnan;
+
+import com.dat3m.dartagnan.parsers.program.ProgramParser;
+import com.dat3m.dartagnan.program.Program;
+import com.dat3m.dartagnan.utils.ResourceHelper;
+import com.dat3m.dartagnan.utils.printer.Printer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RunWith(Parameterized.class)
+public class DartagnanLoop {
+
+    @Parameterized.Parameters(name = "{index}: {0}")
+    public static Iterable<Object[]> data() throws IOException {
+        try (Stream<Path> fileStream = Files.walk(Paths.get(ResourceHelper.TEST_RESOURCE_PATH + "loops/"))) {
+            return fileStream
+                    .filter(Files::isRegularFile)
+                    .filter(f -> (f.toString().endsWith("litmus")))
+                    .map(f -> new Object[]{f.toString()})
+                    .collect(Collectors.toList());
+        }
+    }
+
+    private final String path;
+
+    public DartagnanLoop(String path) {
+        this.path = path;
+    }
+
+    @Test
+    public void test() throws Exception {
+    	Program p = new ProgramParser().parse(new File(path));
+    	System.out.println(new Printer().print(p));
+    	p.unroll(1, 0);
+    }
+}
