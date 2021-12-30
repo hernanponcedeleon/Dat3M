2bcb76c8d48b5080cf6c6de9fa3521c42c257734
Hernan Ponce de Leon <zeta96@gmail.com>
2021-12-30 13:23:45 +0100

    Use task name for the witness file

--- dartagnan/src/main/java/com/dat3m/dartagnan/witness/WitnessGraph.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/witness/WitnessGraph.java
@@ -6,6 +6,8 @@ import com.dat3m.dartagnan.program.event.Load;
 import com.dat3m.dartagnan.program.event.Store;
 import com.dat3m.dartagnan.wmm.filter.FilterBasic;
 import com.google.common.collect.Lists;
+import com.google.common.io.Files;
+
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.BooleanFormulaManager;
 import org.sosy_lab.java_smt.api.IntegerFormulaManager;
@@ -20,6 +22,7 @@ import java.util.stream.Collectors;
 import static com.dat3m.dartagnan.program.utils.EType.*;
 import static com.dat3m.dartagnan.expression.utils.Utils.convertToIntegerFormula;
 import static com.dat3m.dartagnan.witness.EdgeAttributes.*;
+import static com.dat3m.dartagnan.witness.GraphAttributes.PROGRAMFILE;
 import static com.dat3m.dartagnan.wmm.utils.Utils.edge;
 
 public class WitnessGraph extends ElemWithAttributes {
@@ -55,7 +58,7 @@ public class WitnessGraph extends ElemWithAttributes {
 	}
 	
 	public String getProgram() {
-		return attributes.get("programfile");
+		return attributes.get(PROGRAMFILE.toString());
 	}
 	
 	public String toXML() {
@@ -109,7 +112,8 @@ public class WitnessGraph extends ElemWithAttributes {
 	}
 	
 	public void write() {
-		try (FileWriter fw = new FileWriter(System.getenv("DAT3M_HOME") + "/output/witness.graphml")) {
+		try (FileWriter fw = new FileWriter(String.format("%s/output/%s.graphml", 
+				System.getenv("DAT3M_HOME"), Files.getNameWithoutExtension(getProgram())))) {
 			fw.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
 			fw.write("<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n");
 			for(GraphAttributes attr : GraphAttributes.values()) {fw.write("<key attr.name=\"" + attr.toString() + "\" attr.type=\"string\" for=\"graph\" id=\"" + attr + "\"/>\n");}
