3be1e52010fa3bffa2ce57c0fdee899ccfe30108
Hernan Ponce de Leon <zeta96@gmail.com>
2021-12-31 10:40:06 +0100

    UnsupportedOperationException -> IllegalArgumentException in WrongTargetTest

--- dartagnan/src/test/java/com/dat3m/dartagnan/exceptions/WrongTargetTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/exceptions/WrongTargetTest.java
@@ -11,42 +11,42 @@ import org.junit.Test;
 
 public class WrongTargetTest {
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void X86CompiledToNone() throws Exception {
     	Program p = new ProgramParser().parse(new File(ResourceHelper.LITMUS_RESOURCE_PATH + "litmus/X86/2+2W+mfence-rmws.litmus"));
     	p.unroll(1, 0);
     	p.compile(Arch.NONE, 0);
     }
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void X86CompiledToPower() throws Exception {
     	Program p = new ProgramParser().parse(new File(ResourceHelper.LITMUS_RESOURCE_PATH + "litmus/X86/2+2W+mfence-rmws.litmus"));
     	p.unroll(1, 0);
     	p.compile(Arch.POWER, 0);
     }
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void X86CompiledToARM8() throws Exception {
     	Program p = new ProgramParser().parse(new File(ResourceHelper.LITMUS_RESOURCE_PATH + "litmus/X86/2+2W+mfence-rmws.litmus"));
     	p.unroll(1, 0);
     	p.compile(Arch.ARM8, 0);
     }
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void ARMCompiledToNone() throws Exception {
     	Program p = new ProgramParser().parse(new File(ResourceHelper.LITMUS_RESOURCE_PATH + "litmus/AARCH64/ATOM/2+2W+poxxs.litmus"));
     	p.unroll(1, 0);
     	p.compile(Arch.NONE, 0);
     }
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void ARMCompiledToTSO() throws Exception {
     	Program p = new ProgramParser().parse(new File(ResourceHelper.LITMUS_RESOURCE_PATH + "litmus/AARCH64/ATOM/2+2W+poxxs.litmus"));
     	p.unroll(1, 0);
     	p.compile(Arch.TSO, 0);
     }
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void ARMCompiledToPower() throws Exception {
     	Program p = new ProgramParser().parse(new File(ResourceHelper.LITMUS_RESOURCE_PATH + "litmus/AARCH64/ATOM/2+2W+poxxs.litmus"));
     	p.unroll(1, 0);
