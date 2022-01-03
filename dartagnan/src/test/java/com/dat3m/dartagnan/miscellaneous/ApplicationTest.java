59851a452c07907035a4a7f072f95731abfd86e1
Hernan Ponce de Leon <zeta96@gmail.com>
2022-01-03 16:05:01 +0100

    Application test for validation

--- dartagnan/src/test/java/com/dat3m/dartagnan/miscellaneous/ApplicationTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/miscellaneous/ApplicationTest.java
@@ -15,61 +15,76 @@ public class ApplicationTest {
 
     @Test
     public void Two() throws Exception {
-    	Dartagnan.main(createandFillOptions(REACHABILITY.asStringOption(), 
+    	Dartagnan.main(createAndFillOptions(REACHABILITY.asStringOption(), 
     										TWO.asStringOption(),
     										Solvers.Z3.toString()));
     }
 
     @Test
     public void Assume() throws Exception {
-    	Dartagnan.main(createandFillOptions(REACHABILITY.asStringOption(), 
+    	Dartagnan.main(createAndFillOptions(REACHABILITY.asStringOption(), 
     										ASSUME.asStringOption(),
     										Solvers.Z3.toString()));
     }
 
     @Test
     public void Incremental() throws Exception {
-    	Dartagnan.main(createandFillOptions(REACHABILITY.asStringOption(), 
+    	Dartagnan.main(createAndFillOptions(REACHABILITY.asStringOption(), 
     										INCREMENTAL.asStringOption(),
     										Solvers.Z3.toString()));
     }
 
     @Test
     public void CAAT() throws Exception {
-    	Dartagnan.main(createandFillOptions(REACHABILITY.asStringOption(), 
+    	Dartagnan.main(createAndFillOptions(REACHABILITY.asStringOption(), 
     										CAAT.asStringOption(),
     										Solvers.Z3.toString()));
     }
 
     @Test
     public void Races() throws Exception {
-    	Dartagnan.main(createandFillOptions(RACES.asStringOption(), 
+    	Dartagnan.main(createAndFillOptions(RACES.asStringOption(), 
     										ASSUME.asStringOption(),
     										Solvers.Z3.toString()));
     }
 
+    @Test
+    public void Validation() throws Exception {
+		String[] options = new String[7];
+		
+	    options[0] = ResourceHelper.TEST_RESOURCE_PATH + "witness/lazy01-for-witness.bpl";
+	    options[1] = ResourceHelper.CAT_RESOURCE_PATH + "cat/svcomp.cat";
+	    options[2] = String.format("--%s=%s", BOUND, 2);
+	    options[3] = String.format("--%s=%s", ANALYSIS, REACHABILITY.asStringOption());
+	    options[4] = String.format("--%s=%s", METHOD, INCREMENTAL.asStringOption());
+	    options[5] = String.format("--%s=%s", SOLVER, Solvers.Z3.toString());
+    	options[6] = String.format("--%s=%s", VALIDATE, ResourceHelper.TEST_RESOURCE_PATH + "witness/lazy01.graphml");
+
+    	Dartagnan.main(options);
+    }
+
     @Test(expected = InvalidConfigurationException.class)
     public void UnsupportedAnalysis() throws Exception {
-    	Dartagnan.main(createandFillOptions("unsupported-analysis", 
+    	Dartagnan.main(createAndFillOptions("unsupported-analysis", 
     										ASSUME.asStringOption(),
     										Solvers.Z3.toString()));
     }
 
     @Test(expected = InvalidConfigurationException.class)
     public void UnsupportedMethod() throws Exception {
-    	Dartagnan.main(createandFillOptions(REACHABILITY.asStringOption(), 
+    	Dartagnan.main(createAndFillOptions(REACHABILITY.asStringOption(), 
     										"unsupported-method",
     										Solvers.Z3.toString()));
     }
 
     @Test(expected = InvalidConfigurationException.class)
     public void UnsupportedSolver() throws Exception {
-    	Dartagnan.main(createandFillOptions(REACHABILITY.asStringOption(), 
+    	Dartagnan.main(createAndFillOptions(REACHABILITY.asStringOption(), 
     										ASSUME.asStringOption(),
     										"unsupported-solver"));
     }
 
-	private String[] createandFillOptions(String analysis, String method, String solver) {
+	private String[] createAndFillOptions(String analysis, String method, String solver) {
 		String[] dartagnanOptions = new String[6];
 		
 	    dartagnanOptions[0] = ResourceHelper.TEST_RESOURCE_PATH + "locks/ttas-5.bpl";
