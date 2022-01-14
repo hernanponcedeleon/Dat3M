c8dcd8c34ae6c975a6b293d095d823eda58cf575
Hernan Ponce de Leon <zeta96@gmail.com>
2022-01-14 09:29:45 +0100

    Lock and Unlock are Store

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/event/lang/pthread/Lock.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/lang/pthread/Lock.java
@@ -1,14 +1,15 @@
 package com.dat3m.dartagnan.program.event.lang.pthread;
 
+import com.dat3m.dartagnan.expression.IConst;
 import com.dat3m.dartagnan.expression.IExpr;
 import com.dat3m.dartagnan.program.Register;
 import com.dat3m.dartagnan.program.event.core.Event;
 import com.dat3m.dartagnan.program.event.core.Label;
-import com.dat3m.dartagnan.program.event.core.MemEvent;
+import com.dat3m.dartagnan.program.event.core.Store;
 import com.dat3m.dartagnan.program.event.visitors.EventVisitor;
 import static com.dat3m.dartagnan.program.event.Tag.C11.MO_SC;
 
-public class Lock extends MemEvent {
+public class Lock extends Store {
 	
 	private final String name;
 	private final Register reg;
@@ -16,7 +17,7 @@ public class Lock extends MemEvent {
     private Label label4Copy;
 
 	public Lock(String name, IExpr address, Register reg, Label label){
-		super(address, MO_SC);
+		super(address, IConst.ONE, MO_SC);
 		this.name = name;
         this.reg = reg;
         this.label = label;
