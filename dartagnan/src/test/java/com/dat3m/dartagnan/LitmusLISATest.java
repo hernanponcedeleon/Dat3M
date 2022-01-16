adaa4bd82062d3e7c91650f021a2119bda72a744
Hernan Ponce de Leon <zeta96@gmail.com>
2022-01-16 08:53:22 +0100

    Added LISA tests

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/LitmusLISATest.java
@@ -0,0 +1,34 @@
+package com.dat3m.dartagnan;
+
+import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.utils.rules.Provider;
+import com.dat3m.dartagnan.utils.rules.Providers;
+import com.dat3m.dartagnan.wmm.Wmm;
+import com.dat3m.dartagnan.configuration.Arch;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+
+@RunWith(Parameterized.class)
+public class LitmusLISATest extends AbstractLitmusTest {
+
+    @Parameterized.Parameters(name = "{index}: {0}, {1}")
+    public static Iterable<Object[]> data() throws IOException {
+		return buildLitmusTests("litmus/LISA/");
+    }
+
+    @Override
+    protected Provider<Arch> getTargetProvider() {
+        return () -> Arch.NONE;
+    }
+
+    @Override
+    protected Provider<Wmm> getWmmProvider() {
+        return Providers.createWmmFromName(() -> "linux-kernel");
+    }
+
+    public LitmusLISATest(String path, Result expected) {
+        super(path, expected);
+    }
+}
