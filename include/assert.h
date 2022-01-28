7bac2ec2d1bb95ad66da41f86bcc3b1c3b4a82a4
Hernan Ponce de Leon <zeta96@gmail.com>
2022-01-28 09:17:44 +0100

    Added spinloop specific events

--- include/assert.h
+++ include/assert.h
@@ -1,2 +1,7 @@
 extern void __VERIFIER_assert(int exp);
 extern void assert (int exp) {__VERIFIER_assert(exp);};
+
+// Used for spinloops
+extern void __VERIFIER_loop_begin(void);
+extern void __VERIFIER_spin_start(void);
+extern void __VERIFIER_spin_end(int);
