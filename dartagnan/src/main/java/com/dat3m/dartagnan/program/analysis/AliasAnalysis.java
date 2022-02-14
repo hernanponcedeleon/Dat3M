d53c7fc5ac2ebc7b955c02f983d8dcde24d6d16f
Hernan Ponce de Leon <zeta96@gmail.com>
2022-02-14 10:18:38 +0100

    Alias method as OptionInterface

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/AliasAnalysis.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/AliasAnalysis.java
@@ -1,7 +1,11 @@
 package com.dat3m.dartagnan.program.analysis;
 
+import com.dat3m.dartagnan.configuration.Alias;
 import com.dat3m.dartagnan.program.Program;
 import com.dat3m.dartagnan.program.event.core.MemEvent;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
@@ -11,29 +15,29 @@ import static com.dat3m.dartagnan.configuration.OptionNames.ALIAS_METHOD;
 
 public interface AliasAnalysis {
 
+    static final Logger logger = LogManager.getLogger(AliasAnalysis.class);
+
     boolean mustAlias(MemEvent a, MemEvent b);
     boolean mayAlias(MemEvent a, MemEvent b);
 
     static AliasAnalysis fromConfig(Program program, Configuration config) throws InvalidConfigurationException {
         InjectionTarget o = new InjectionTarget();
         config.inject(o);
-        try {
-            return (AliasAnalysis)o.method
-            .getDeclaredMethod("fromConfig",Program.class,Configuration.class)
-            .invoke(null,program,config);
-        } catch(ReflectiveOperationException x) {
-            throw new InvalidConfigurationException(String.format("invalid %s#fromConfig(%s,%s)",
-                    o.method.getName(),
-                    Program.class.getSimpleName(),
-                    Configuration.class.getSimpleName()),
-                x);
-        }
+		logger.info("Selected Alias Analysis: " + o.method);
+    	switch (o.method) {
+		case FIELD_SENSITIVE:
+			return FieldSensitiveAndersen.fromConfig(program, config);
+		case FIELD_INSENSITIVE:
+			return AndersenAliasAnalysis.fromConfig(program, config);
+		default:
+			throw new UnsupportedOperationException("Alias method not recognized");
+		}
     }
 
     @Options
     final class InjectionTarget {
         @Option(name = ALIAS_METHOD,
                 description = "General type of analysis that approximates the 'loc' relationship between memory events.")
-        private Class<?extends AliasAnalysis> method = FieldSensitiveAndersen.class;
+        private Alias method = Alias.FIELD_SENSITIVE;
     }
 }
