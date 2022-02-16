1616396a94cbe297d4587a0ecc4e4bef3ff586f5
hernanponcedeleon <zeta96@gmail.com>
2022-02-16 11:41:45 +0100

    Add log entries for WmmEncoder

--- dartagnan/src/main/java/com/dat3m/dartagnan/encoding/WmmEncoder.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/encoding/WmmEncoder.java
@@ -8,6 +8,9 @@ import com.dat3m.dartagnan.wmm.relation.Relation;
 import com.dat3m.dartagnan.wmm.utils.RecursiveGroup;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.java_smt.api.BooleanFormula;
@@ -16,6 +19,8 @@ import org.sosy_lab.java_smt.api.SolverContext;
 
 
 public class WmmEncoder implements Encoder {
+
+    private static final Logger logger = LogManager.getLogger(WmmEncoder.class);
 
     private final Wmm memoryModel;
     private boolean isInitialized = false;
@@ -75,6 +80,7 @@ public class WmmEncoder implements Encoder {
     // It does NOT encode the axioms nor any non-base relation yet!
     public BooleanFormula encodeAnarchicSemantics(SolverContext ctx) {
         checkInitialized();
+        logger.info("Encoding anarchic semantics");
         BooleanFormulaManager bmgr = ctx.getFormulaManager().getBooleanFormulaManager();
         BooleanFormula enc = bmgr.makeTrue();
         for(String relName : Wmm.BASE_RELATIONS){
@@ -89,6 +95,7 @@ public class WmmEncoder implements Encoder {
     // NOTE: It avoids encoding relations that do NOT affect the axioms, i.e. unused relations
     public BooleanFormula encodeRelations(SolverContext ctx) {
         checkInitialized();
+        logger.info("Encoding relations");
         BooleanFormulaManager bmgr = ctx.getFormulaManager().getBooleanFormulaManager();
         BooleanFormula enc = encodeAnarchicSemantics(ctx);
         for (Axiom ax : memoryModel.getAxioms()) {
@@ -100,6 +107,7 @@ public class WmmEncoder implements Encoder {
     // Encodes all axioms. This should be called after <encodeRelations>
     public BooleanFormula encodeConsistency(SolverContext ctx) {
         checkInitialized();
+        logger.info("Encoding consistency");
         BooleanFormulaManager bmgr = ctx.getFormulaManager().getBooleanFormulaManager();
         BooleanFormula expr = bmgr.makeTrue();
         for (Axiom ax : memoryModel.getAxioms()) {
