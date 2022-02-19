e91b1c2e299f1ebfe806519065d29207f793dc92
hernanponcedeleon <zeta96@gmail.com>
2022-02-19 16:37:09 +0100

    Fix typo in method call

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/ProcessingManager.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/ProcessingManager.java
@@ -51,7 +51,7 @@ public class ProcessingManager implements ProgramProcessor {
                 Simplifier.fromConfig(config),
                 LoopUnrolling.fromConfig(config),
                 constantPropagation ? ConstantPropagation.fromConfig(config) : null,
-                DeadAssignementElimination.fromConfig(config),
+                DeadAssignmentElimination.fromConfig(config),
                 Compilation.fromConfig(config),
                 reduceSymmetry ? SymmetryReduction.fromConfig(config) : null
         ));
