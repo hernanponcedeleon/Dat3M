daa9255987cfd0ec167c5024c7ef8fb8b3282bb6
Thomas Haas <tomy.haas@t-online.de>
2022-02-21 18:00:58 +0100

    Renaming of InjectionTarget to AliasAnalysis.Config Some minor refactor

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/AliasAnalysis.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/AliasAnalysis.java
@@ -3,7 +3,6 @@ package com.dat3m.dartagnan.program.analysis;
 import com.dat3m.dartagnan.configuration.Alias;
 import com.dat3m.dartagnan.program.Program;
 import com.dat3m.dartagnan.program.event.core.MemEvent;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.sosy_lab.common.configuration.Configuration;
@@ -15,29 +14,33 @@ import static com.dat3m.dartagnan.configuration.OptionNames.ALIAS_METHOD;
 
 public interface AliasAnalysis {
 
-    static final Logger logger = LogManager.getLogger(AliasAnalysis.class);
+    Logger logger = LogManager.getLogger(AliasAnalysis.class);
 
     boolean mustAlias(MemEvent a, MemEvent b);
     boolean mayAlias(MemEvent a, MemEvent b);
 
     static AliasAnalysis fromConfig(Program program, Configuration config) throws InvalidConfigurationException {
-        InjectionTarget o = new InjectionTarget();
-        config.inject(o);
-		logger.info("Selected Alias Analysis: " + o.method);
-    	switch (o.method) {
-		case FIELD_SENSITIVE:
-			return FieldSensitiveAndersen.fromConfig(program, config);
-		case FIELD_INSENSITIVE:
-			return AndersenAliasAnalysis.fromConfig(program, config);
-		default:
-			throw new UnsupportedOperationException("Alias method not recognized");
-		}
+        Config c = new Config(config);
+		logger.info("Selected Alias Analysis: " + c.method);
+    	switch (c.method) {
+            case FIELD_SENSITIVE:
+                return FieldSensitiveAndersen.fromConfig(program, config);
+            case FIELD_INSENSITIVE:
+                return AndersenAliasAnalysis.fromConfig(program, config);
+            default:
+                throw new UnsupportedOperationException("Alias method not recognized");
+        }
     }
 
     @Options
-    final class InjectionTarget {
+    final class Config {
         @Option(name = ALIAS_METHOD,
                 description = "General type of analysis that approximates the 'loc' relationship between memory events.")
         private Alias method = Alias.FIELD_SENSITIVE;
+
+        private Config(Configuration config) throws InvalidConfigurationException {
+            config.inject(this);
+        }
     }
+
 }
