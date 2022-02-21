8dbae27d44553007dbbfe07be42756e167e856dd
Thomas Haas <tomy.haas@t-online.de>
2022-02-21 19:36:19 +0100

    Simplified DeadAssignmentElimination

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/DeadAssignmentElimination.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/DeadAssignmentElimination.java
@@ -1,20 +1,22 @@
 package com.dat3m.dartagnan.program.processing;
 
 import com.dat3m.dartagnan.program.Program;
+import com.dat3m.dartagnan.program.Register;
 import com.dat3m.dartagnan.program.Thread;
 import com.dat3m.dartagnan.program.event.core.Event;
 import com.dat3m.dartagnan.program.event.core.MemEvent;
 import com.dat3m.dartagnan.program.event.core.utils.RegReaderData;
 import com.dat3m.dartagnan.program.event.core.utils.RegWriter;
-import static com.dat3m.dartagnan.program.event.Tag.*;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import com.google.common.collect.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.dat3m.dartagnan.program.event.Tag.VISIBLE;
 
 // This is just Dead Store Elimination, but the use of the term "Store" can be confusing in our setting 
 public class DeadAssignmentElimination implements ProgramProcessor {
@@ -47,33 +49,34 @@ public class DeadAssignmentElimination implements ProgramProcessor {
     }
 
     private void eliminateDeadStores(Thread thread) {
-    	
-    	List<Event> removed = new ArrayList<>();
-    	
-    	// We traverse the events in inverse order and mark those that can be removed
-    	ArrayList<Event> sorted = new ArrayList<Event>(thread.getEvents());
-    	sorted.sort(Comparator.reverseOrder());
-    	for(Event e : sorted) {
-    		// We cannot remove load events
-    		if(e.is(REG_WRITER) && !e.is(READ)) {
-    			RegWriter rw = (RegWriter)e;
-    			boolean notUsedByRegReader = e.getSuccessors().stream()
-    					.filter(s -> s instanceof RegReaderData && !removed.contains(s))
-    					.noneMatch(s -> ((RegReaderData)s).getDataRegs().contains(rw.getResultRegister()));
-    			boolean notUsedByMemEvent = e.getSuccessors().stream()
-    					.filter(s -> s instanceof MemEvent && !removed.contains(s))
-    					.noneMatch(s -> ((MemEvent)s).getAddress().getRegs().contains(rw.getResultRegister()));
-    			// We use register to pass the parameters to new threads
-    			boolean notUsedByOtherThread = program.getEvents().stream()
-    					.filter(o -> !o.getThread().equals(e.getThread()) && o instanceof RegReaderData && !removed.contains(o))
-    					.noneMatch(s -> ((RegReaderData)s).getDataRegs().contains(rw.getResultRegister()));
-    			boolean notUsedByAssertion = !program.getAss().getRegs().contains(rw.getResultRegister());
-				if(notUsedByRegReader && notUsedByMemEvent && notUsedByOtherThread && notUsedByAssertion) {
-    				removed.add(e);
-    			}
-    		}
-    	}
-    	// Here is the actual removal
+        Set<Event> removed = new HashSet<>();
+
+        // Registers that are used by other threads or assertions cannot be removed
+        Set<Register> usedRegs = new HashSet<>(program.getAss().getRegs());
+        program.getEvents().stream()
+                .filter(o -> !o.getThread().equals(thread))
+                .filter(o -> o instanceof RegReaderData)
+                .forEach(o -> usedRegs.addAll(((RegReaderData)o).getDataRegs()));
+
+        for(Event e : Lists.reverse(thread.getEvents())) {
+            if (e instanceof RegWriter && !e.is(VISIBLE) && !usedRegs.contains(((RegWriter)e).getResultRegister())) {
+                // Invisible RegWriters that write to an unused reg can get removed
+                removed.add(e);
+            }
+            // An event that was not removed adds its dependencies to the used registers
+            if (!removed.contains(e)) {
+                if (e instanceof RegReaderData) {
+                    // Data & Ctrl dependencies
+                    usedRegs.addAll(((RegReaderData) e).getDataRegs());
+                }
+                if (e instanceof MemEvent) {
+                    // Address dependencies
+                    usedRegs.addAll(((MemEvent) e).getAddress().getRegs());
+                }
+            }
+        }
+
+        // Here is the actual removal
         Event pred = null;
         Event cur = thread.getEntry();
         while (cur != null) {
