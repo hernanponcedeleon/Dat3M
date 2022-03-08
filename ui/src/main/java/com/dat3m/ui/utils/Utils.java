b60f9a1265350cf92ca1e31ae175c3a1430ee643
hernanponcedeleon <zeta96@gmail.com>
2022-03-08 11:24:31 +0100

    Allow scrolling in error msg

--- ui/src/main/java/com/dat3m/ui/utils/Utils.java
+++ ui/src/main/java/com/dat3m/ui/utils/Utils.java
@@ -1,19 +1,19 @@
 package com.dat3m.ui.utils;
 
-import javax.swing.JOptionPane;
+import java.awt.Dimension;
 
-import com.dat3m.ui.icon.IconCode;
-import com.dat3m.ui.icon.IconHelper;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class Utils {
 	
 	public static void showError(String msg, String title){
-        JOptionPane.showMessageDialog(
-                null,
-                msg,
-                title,
-                JOptionPane.ERROR_MESSAGE);
-//                IconHelper.getIcon(IconCode.DAT3M, 60));
+		JTextArea textArea = new JTextArea(msg);
+		JScrollPane scrollPane = new JScrollPane(textArea);  
+		scrollPane.setPreferredSize( new Dimension( 1000 , 500 ) );
+		JOptionPane.showMessageDialog(null, scrollPane, title,  
+				JOptionPane.ERROR_MESSAGE);
     }
 
 	public static void showError(String msg){
