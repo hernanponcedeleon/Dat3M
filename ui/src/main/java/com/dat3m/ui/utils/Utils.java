e295494b21534cceac566f53fbc94eb138365d25
hernanponcedeleon <zeta96@gmail.com>
2022-03-11 20:49:03 +0100

    Improve size of error msg in UI

--- ui/src/main/java/com/dat3m/ui/utils/Utils.java
+++ ui/src/main/java/com/dat3m/ui/utils/Utils.java
@@ -11,7 +11,9 @@ public class Utils {
 	public static void showError(String msg, String title){
 		JTextArea textArea = new JTextArea(msg);
 		JScrollPane scrollPane = new JScrollPane(textArea);  
-		scrollPane.setPreferredSize( new Dimension( 1000 , 500 ) );
+		Dimension size = new Dimension( Math.min(textArea.getPreferredSize().width + 1, 1000) , 
+										Math.min(textArea.getPreferredSize().height + 1, 500) );
+		scrollPane.setPreferredSize( size );
 		JOptionPane.showMessageDialog(null, scrollPane, title,  
 				JOptionPane.ERROR_MESSAGE);
     }
