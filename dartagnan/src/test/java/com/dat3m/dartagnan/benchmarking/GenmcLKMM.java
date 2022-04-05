61a53ff783908b1a35aa8594e87e9c73a78cf8e3
hernanponcedeleon <zeta96@gmail.com>
2022-04-05 14:45:43 +0200

    Removed extra parameter from genmc LKMM test

--- dartagnan/src/test/java/com/dat3m/dartagnan/benchmarking/GenmcLKMM.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/benchmarking/GenmcLKMM.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.dat3m.dartagnan.configuration.Arch.NONE;
 import static com.dat3m.dartagnan.utils.Result.*;
 
 @RunWith(Parameterized.class)
@@ -42,16 +41,16 @@ public class GenmcLKMM extends AbstractExternalTool {
 	@Parameterized.Parameters(name = "{index}: {0}, target={1}")
     public static Iterable<Object[]> data() throws IOException {
 		return Arrays.asList(new Object[][]{
-            {"lkmm/lkmm-ttas-5", NONE, UNKNOWN},
-            {"lkmm/lkmm-ticketlock-6", NONE, PASS},
-            {"lkmm/lkmm-mutex-4", NONE, UNKNOWN},
-            {"lkmm/lkmm-spinlock-5", NONE, UNKNOWN},
-            {"lkmm/lkmm-linuxrwlock-3", NONE, UNKNOWN},
-            {"lkmm/lkmm-mutex_musl-4", NONE, UNKNOWN},
-//            {"lfds/safestack-3", TSO, FAIL},
-//            {"lfds/dglm-3", TSO, UNKNOWN},
-//            {"lfds/ms-3", TSO, UNKNOWN},
-//            {"lfds/treiber-3", TSO, UNKNOWN}
+            {"lkmm/lkmm-ttas-5", UNKNOWN},
+            {"lkmm/lkmm-ticketlock-6", PASS},
+            {"lkmm/lkmm-mutex-4", UNKNOWN},
+            {"lkmm/lkmm-spinlock-5", UNKNOWN},
+            {"lkmm/lkmm-linuxrwlock-3", UNKNOWN},
+            {"lkmm/lkmm-mutex_musl-4", UNKNOWN},
+//            {"lfds/safestack-3", FAIL},
+//            {"lfds/dglm-3", UNKNOWN},
+//            {"lfds/ms-3", UNKNOWN},
+//            {"lfds/treiber-3", UNKNOWN}
 		});
     }
 }
