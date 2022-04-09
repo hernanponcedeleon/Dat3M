70ad15a8473f2c9f2870b714a2135abfcc8d8929
hernanponcedeleon <zeta96@gmail.com>
2022-04-09 19:37:39 +0200

    Add last tests to nidhugg and genmc

--- dartagnan/src/test/java/com/dat3m/dartagnan/benchmarking/Genmc.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/benchmarking/Genmc.java
@@ -48,6 +48,7 @@ public class Genmc extends AbstractExternalTool {
             {"locks/linuxrwlock-3", UNKNOWN},
             {"locks/mutex_musl-4", UNKNOWN},
             {"lfds/safe_stack-3", FAIL},
+            {"lfds/chase-lev-5", PASS},
             {"lfds/dglm-3", UNKNOWN},
             {"lfds/harris_linked_list-3", UNKNOWN},
             {"lfds/ms-3", UNKNOWN},
