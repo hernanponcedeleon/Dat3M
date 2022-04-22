953f6f5641174ae52c38bfb48e77b9c9b6a3c1a8
René Pascal Maseli <r.maseli@tu-bs.de>
2022-04-22 18:39:30 +0200

    Remove strong assertions from Dependency

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/Dependency.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/Dependency.java
@@ -12,6 +12,7 @@ import com.dat3m.dartagnan.program.event.core.utils.RegWriter;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Verify.verify;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.range;
@@ -93,26 +94,16 @@ public final class Dependency {
                 Map<Register,State> result = new HashMap<>();
                 for(Register register : registers) {
                     if(register.getThreadId() == Register.NO_THREAD) {
-                        verify(state.stream().noneMatch(w -> w.register.equals(register)),
+                        checkArgument(state.stream().noneMatch(w -> w.register.equals(register)),
                                 "Thread %s cannot update the global constant %s.",
                                 thread.getId(),
-                                register.getName());
-                        verify(!finalWriters.containsKey(register),
-                                "No thread is allowed to update the global constant %s.",
                                 register.getName());
                         continue;
                     }
                     State writers;
                     if(register.getThreadId() != event.getThread().getId()) {
-                        verify(state.stream().noneMatch(w -> w.register.equals(register)),
-                                "Helper thread %s cannot update register %s of thread %s.",
-                                thread.getId(),
-                                register.getName(),
-                                register.getThreadId());
-                        //TODO verify that this is a helper thread
-                        //FIXME fetch state of the associated Create
                         writers = finalWriters.get(register);
-                        verify(writers != null,
+                        checkArgument(writers != null,
                                 "Helper thread %s should be listed after their creator thread %s.",
                                 thread.getId(),
                                 register.getThreadId());
@@ -134,7 +125,6 @@ public final class Dependency {
             }
             //copy state, if branching
             if(event instanceof CondJump) {
-                verify(!((CondJump) event).isDead(), "dead jumps after preprocessing");
                 jumps.computeIfAbsent(((CondJump) event).getLabel(), k -> new HashSet<>()).addAll(state);
                 if(((CondJump) event).isGoto()) {
                     state.clear();
@@ -155,7 +145,7 @@ public final class Dependency {
         .filter(e -> e.register.equals(register))
         .map(e -> e.event)
         .collect(toList());
-        verify(!candidates.isEmpty(), "Events must at least be able to read the uninitialized value of a register.");
+        //NOTE if candidates is empty, the reader is unreachable
         List<Event> mays = candidates.stream()
         .filter(Objects::nonNull)
         .sorted(Comparator.comparingInt(Event::getCId))
@@ -214,7 +204,6 @@ public final class Dependency {
         private State(boolean initialized, List<Event> may, List<Event> must) {
             verify(new HashSet<>(may).containsAll(must), "Each must-writer must also be a may-writer.");
             verify(may.isEmpty() || must.contains(may.get(may.size()-1)), "The last may-writer must also be a must-writer.");
-            verify(!initialized || !may.isEmpty(), "Initialized states must have at least one may-writer.");
             this.initialized = initialized;
             this.may = may;
             this.must = must;
