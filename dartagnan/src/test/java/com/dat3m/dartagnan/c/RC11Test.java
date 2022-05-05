bdee90fe953680d6f1cd17eb1975352e00525409
hernanponcedeleon <zeta96@gmail.com>
2022-05-05 14:05:15 +0200

    Unit tests for RC11

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/c/RC11Test.java
@@ -0,0 +1,64 @@
+package com.dat3m.dartagnan.c;
+
+import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.utils.rules.CSVLogger;
+import com.dat3m.dartagnan.utils.rules.Provider;
+import com.dat3m.dartagnan.verification.RefinementTask;
+import com.dat3m.dartagnan.verification.solving.AssumeSolver;
+import com.dat3m.dartagnan.verification.solving.RefinementSolver;
+import com.dat3m.dartagnan.configuration.Arch;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static com.dat3m.dartagnan.utils.ResourceHelper.TEST_RESOURCE_PATH;
+import static com.dat3m.dartagnan.utils.Result.*;
+import static com.dat3m.dartagnan.configuration.Arch.*;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class RC11Test extends AbstractCTest {
+	
+    public RC11Test(String name, Arch target, Result expected) {
+        super(name, target, expected);
+    }
+
+    @Override
+    protected Provider<String> getProgramPathProvider() {
+        return Provider.fromSupplier(() -> TEST_RESOURCE_PATH + "rc11/" + name + ".bpl");
+    }
+
+    @Override
+    protected long getTimeout() {
+        return 60000;
+    }
+
+	@Parameterized.Parameters(name = "{index}: {0}, target={1}")
+    public static Iterable<Object[]> data() throws IOException {
+    	return Arrays.asList(new Object[][]{
+	            {"2+2W", NONE, PASS},
+	            {"IRIW-acq-sc", NONE, FAIL},
+	            {"RWC+syncs", NONE, PASS},
+	            {"SB", NONE, PASS},
+	            {"W+RWC", NONE, PASS},
+	            {"WWmerge", NONE, FAIL},
+	            {"Z6.U", NONE, FAIL},
+		});
+    }
+
+//    @Test
+	@CSVLogger.FileName("csv/assume")
+	public void testAssume() throws Exception {
+		assertEquals(expected, AssumeSolver.run(contextProvider.get(), proverProvider.get(), taskProvider.get()));
+	}
+
+    @Test
+	@CSVLogger.FileName("csv/refinement")
+	public void testRefinement() throws Exception {
+		assertEquals(expected, RefinementSolver.run(contextProvider.get(), proverProvider.get(),
+				RefinementTask.fromVerificationTaskWithDefaultBaselineWMM(taskProvider.get())));
+	}
+}
