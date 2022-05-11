6fa390841a57668d51362c7dd83412702da0b9f4
hernanponcedeleon <zeta96@gmail.com>
2022-05-11 19:14:00 +0200

    Add unit tests for IMM

--- dartagnan/src/test/java/com/dat3m/dartagnan/c/IMMTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/c/IMMTest.java
@@ -25,15 +25,15 @@ import static com.dat3m.dartagnan.configuration.Arch.*;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
-public class RC11Test extends AbstractCTest {
+public class IMMTest extends AbstractCTest {
 	
-    public RC11Test(String name, Arch target, Result expected) {
+    public IMMTest(String name, Arch target, Result expected) {
         super(name, target, expected);
     }
 
     @Override
     protected Provider<String> getProgramPathProvider() {
-        return Provider.fromSupplier(() -> TEST_RESOURCE_PATH + "rc11/" + name + ".bpl");
+        return Provider.fromSupplier(() -> TEST_RESOURCE_PATH + "imm/" + name + ".bpl");
     }
 
     @Override
@@ -43,22 +43,25 @@ public class RC11Test extends AbstractCTest {
 
     @Override
     protected Provider<Wmm> getWmmProvider() {
-        return Provider.fromSupplier(() -> new ParserCat().parse(new File(CAT_RESOURCE_PATH + "cat/rc11.cat")));
+        return Provider.fromSupplier(() -> new ParserCat().parse(new File(CAT_RESOURCE_PATH + "cat/imm.cat")));
     }
 
 	@Parameterized.Parameters(name = "{index}: {0}, target={1}")
     public static Iterable<Object[]> data() throws IOException {
     	return Arrays.asList(new Object[][]{
-	            {"2+2W", NONE, PASS},
-	            {"IRIW-acq-sc", NONE, FAIL},
-	            {"LB", NONE, PASS},
-	            {"LB+deps", NONE, PASS},
-	            {"RWC+syncs", NONE, PASS},
-	            {"SB", NONE, PASS},
-	            {"SB+rfis", NONE, FAIL},
-	            {"W+RWC", NONE, PASS},
-	            {"WWmerge", NONE, FAIL},
-	            {"Z6.U", NONE, FAIL},
+	            {"paper-E3.1", NONE, PASS},
+	            {"paper-E3.2", NONE, PASS},
+	            {"paper-E3.3", NONE, PASS},
+	            {"paper-E3.4", NONE, PASS},
+	            {"paper-E3.5", NONE, PASS},
+	            {"paper-E3.6", NONE, FAIL},
+	            {"paper-E3.7", NONE, PASS},
+	            {"paper-E3.8", NONE, PASS},
+	            {"paper-E3.8-alt", NONE, FAIL},
+	            {"paper-E3.9", NONE, PASS},
+//	            {"paper-E3.10", NONE, FAIL},
+	            {"paper-R2", NONE, PASS},
+	            {"paper-R2-alt", NONE, PASS},
 		});
     }
 
