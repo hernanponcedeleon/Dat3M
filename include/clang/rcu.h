62e682a5ab4718acbbc9ec97f793dd2eb3506121
hernanponcedeleon <zeta96@gmail.com>
2022-05-26 11:56:55 -0700

    Improve rcu.h

--- include/clang/rcu.h
+++ include/clang/rcu.h
@@ -1,17 +1,14 @@
-#ifndef MAX_THREADS
-#define MAX_THREADS 2
-#endif
+__thread int tid;
 
+#ifdef RCUIMP
 #define GP_PHASE 0x10000
 #define CS_MASK 0x0ffff
 static unsigned long rc[MAX_THREADS] = {0};
 static unsigned long gc = 1;
 static spinlock_t gp_lock;
 
-__thread int tid;
 #define get_my_tid() tid;
 
-#ifdef RCUIMP
 void rcu_read_lock(void) {
     unsigned int i = get_my_tid();
     unsigned long tmp = READ_ONCE(rc[i]);
