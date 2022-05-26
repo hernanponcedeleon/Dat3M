8fbcfc37daa448d386192280244d6b572d03b4ad
hernanponcedeleon <zeta96@gmail.com>
2022-05-25 21:54:51 -0700

    Update LKMM unit tests

--- benchmarks/lkmm/LB+fencembonceonce+ctrlonceonce.c
+++ benchmarks/lkmm/LB+fencembonceonce+ctrlonceonce.c
@@ -10,15 +10,15 @@ int r0, r1;
 
 void *thread_1(void *unused)
 {
-	r0 = READ_ONCE(x);
-	if (r0)
+	WRITE_ONCE(r0,  READ_ONCE(x));
+	if (READ_ONCE(r0))
 		WRITE_ONCE(y, 1);
 	return NULL;
 }
 
 void *thread_2(void *unused)
 {
-	r1 = READ_ONCE(y);
+	WRITE_ONCE(r1, READ_ONCE(y));
 	smp_mb();
 	WRITE_ONCE(x, 1);
 	return NULL;
@@ -28,17 +28,13 @@ int main()
 {
 	pthread_t t1, t2;
 
-	if (pthread_create(&t1, NULL, thread_1, NULL))
-		abort();
-	if (pthread_create(&t2, NULL, thread_2, NULL))
-		abort();
+	pthread_create(&t1, NULL, thread_1, NULL);
+	pthread_create(&t2, NULL, thread_2, NULL);
 
-	if (pthread_join(t1, NULL))
-		abort();
-	if (pthread_join(t2, NULL))
-		abort();
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
 
-	assert(!(r0 == 1 && r1 == 1));
+	assert(!(READ_ONCE(r0) == 1 && READ_ONCE(r1) == 1));
 
 	return 0;
 }
