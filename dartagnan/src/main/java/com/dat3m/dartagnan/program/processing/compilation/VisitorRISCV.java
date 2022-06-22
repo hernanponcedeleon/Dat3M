37e114a378d47efe6122c77ba181890b3ffe71d6
hernanponcedeleon <zeta96@gmail.com>
2022-06-22 07:19:31 +0200

    Add simple RISCV compilation visitor

---
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/compilation/VisitorRISCV.java
@@ -0,0 +1,12 @@
+package com.dat3m.dartagnan.program.processing.compilation;
+
+import com.dat3m.dartagnan.program.event.core.*;
+import com.dat3m.dartagnan.program.event.visitors.EventVisitor;
+
+import java.util.List;
+
+class VisitorRISCV extends VisitorBase implements EventVisitor<List<Event>> {
+
+	protected VisitorRISCV() {}
+
+}
