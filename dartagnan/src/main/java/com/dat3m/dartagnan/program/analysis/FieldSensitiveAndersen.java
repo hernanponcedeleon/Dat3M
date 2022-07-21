9eb27d313cab75ed038168c15bfdde665e854a4f
hernanponcedeleon <zeta96@gmail.com>
2022-07-21 16:01:19 +0200

    Remove hack

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/FieldSensitiveAndersen.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/FieldSensitiveAndersen.java
@@ -5,7 +5,6 @@ import com.dat3m.dartagnan.expression.processing.ExpressionVisitor;
 import com.dat3m.dartagnan.program.Program;
 import com.dat3m.dartagnan.program.Register;
 import com.dat3m.dartagnan.program.event.Tag;
-import com.dat3m.dartagnan.program.event.arch.riscv.AmoAbstract;
 import com.dat3m.dartagnan.program.event.core.*;
 import com.dat3m.dartagnan.program.event.core.utils.RegWriter;
 import com.dat3m.dartagnan.program.filter.FilterBasic;
@@ -84,10 +83,6 @@ public class FieldSensitiveAndersen implements AliasAnalysis {
 
     @Override
     public boolean mayAlias(MemEvent x, MemEvent y) {
-    	// TODO make this properly
-    	if(x instanceof AmoAbstract || y instanceof AmoAbstract) {
-    		return true;
-    	}
         return !Sets.intersection(getMaxAddressSet(x), getMaxAddressSet(y)).isEmpty();
     }
 
@@ -125,8 +120,6 @@ public class FieldSensitiveAndersen implements AliasAnalysis {
                 addAllAddresses(l,value.address());
             }
         }
-    	System.out.println("Loads: " + loads);
-    	System.out.println("Stores: " + stores);
     }
 
     protected void processRegs(Local e) {
