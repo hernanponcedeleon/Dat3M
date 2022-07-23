f64a6f52e9968c84c2599a6e2f5db3db1339bdde
hernanponcedeleon <zeta96@gmail.com>
2022-07-23 14:16:09 +0200

    Assert successor of SC is ExecutionStatus

--- dartagnan/src/main/java/com/dat3m/dartagnan/wmm/relation/base/local/RelIdd.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/wmm/relation/base/local/RelIdd.java
@@ -3,6 +3,7 @@ package com.dat3m.dartagnan.wmm.relation.base.local;
 import com.dat3m.dartagnan.program.Register;
 import com.dat3m.dartagnan.program.event.Tag;
 import com.dat3m.dartagnan.program.event.core.Event;
+import com.dat3m.dartagnan.program.event.core.ExecutionStatus;
 import com.dat3m.dartagnan.program.event.core.utils.RegReaderData;
 import com.dat3m.dartagnan.program.filter.FilterBasic;
 import com.dat3m.dartagnan.wmm.utils.Tuple;
@@ -34,6 +35,7 @@ public class RelIdd extends BasicRegRelation {
     	super.getMaxTupleSet();
     	// RISCV store conditional generate idd dependencies if they are executed
         for(Event sc : task.getProgram().getCache().getEvents(FilterBasic.get(Tag.RISCV.STCOND))){
+        	assert sc.getSuccessor() instanceof ExecutionStatus;
         	maxTupleSet.add(new Tuple(sc, sc.getSuccessor()));
         }
         return maxTupleSet;
