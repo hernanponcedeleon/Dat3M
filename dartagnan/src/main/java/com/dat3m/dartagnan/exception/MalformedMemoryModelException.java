32056ed0e69bf16e517ed04dbebaec545ff41910
Thomas Haas <tomy.haas@t-online.de>
2022-08-22 15:33:27 +0200

    Added Wellformedness check into WmmAnalysis

---
+++ dartagnan/src/main/java/com/dat3m/dartagnan/exception/MalformedMemoryModelException.java
@@ -0,0 +1,8 @@
+package com.dat3m.dartagnan.exception;
+
+public class MalformedMemoryModelException extends RuntimeException {
+
+    public MalformedMemoryModelException(String msg){
+        super(msg);
+    }
+}
--- dartagnan/src/main/java/com/dat3m/dartagnan/wmm/analysis/WmmAnalysis.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/wmm/analysis/WmmAnalysis.java
@@ -1,10 +1,17 @@
 package com.dat3m.dartagnan.wmm.analysis;
 
+import com.dat3m.dartagnan.exception.MalformedMemoryModelException;
+import com.dat3m.dartagnan.utils.dependable.DependencyGraph;
 import com.dat3m.dartagnan.wmm.Wmm;
+import com.dat3m.dartagnan.wmm.relation.Relation;
+import com.dat3m.dartagnan.wmm.relation.binary.RelMinus;
+import com.dat3m.dartagnan.wmm.relation.unary.UnaryRelation;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
+
+import java.util.Set;
 
 import static com.dat3m.dartagnan.configuration.OptionNames.LOCALLY_CONSISTENT;
 
@@ -27,22 +34,43 @@ public class WmmAnalysis {
     // =====================================================================
 
     public boolean isLocallyConsistent() {
-        // For now we return a configured value. Ideally, we would like to
+        // For now, we return a configured value. Ideally, we would like to
         // find this property automatically.
         return assumeLocalConsistency;
     }
 
     public boolean doesRespectAtomicBlocks() {
-        // For now we return a configured value. Ideally, we would like to
+        // For now, we return a configured value. Ideally, we would like to
         // find this property automatically. This is currently only relevant for SVCOMP
         return respectsAtomicBlocks;
     }
 
-    private WmmAnalysis(Configuration config) throws InvalidConfigurationException {
+    private WmmAnalysis(Wmm memoryModel, Configuration config) throws InvalidConfigurationException {
         config.inject(this);
+        checkWellformedness(memoryModel);
     }
 
     public static WmmAnalysis fromConfig(Wmm memoryModel, Configuration config) throws InvalidConfigurationException {
-        return new WmmAnalysis(config);
+        return new WmmAnalysis(memoryModel, config);
+    }
+
+    private void checkWellformedness(Wmm memoryModel) {
+        final DependencyGraph<Relation> depGraph = DependencyGraph.from(memoryModel.getRelations());
+        for (Set<DependencyGraph<Relation>.Node> scc : depGraph.getSCCs()) {
+            for (DependencyGraph<Relation>.Node node : scc) {
+                final Relation rel = node.getContent();
+                if (rel instanceof UnaryRelation && scc.size() > 1) {
+                    // Unary relations are not implemented in recursions right now
+                    throw new UnsupportedOperationException(String.format(
+                            "Unary relation %s not supported in recursive definitions.", rel
+                    ));
+                } else if (rel instanceof RelMinus && scc.contains(depGraph.get(rel.getSecond()))) {
+                    // Non-monotonic recursion gives ill-defined memory models.
+                    throw new MalformedMemoryModelException(String.format(
+                            "Non-monotonic recursion is not supported: %s", rel
+                    ));
+                }
+            }
+        }
     }
 }
