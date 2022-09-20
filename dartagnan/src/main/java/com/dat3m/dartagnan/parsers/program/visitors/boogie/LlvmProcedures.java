c4fbf868f70321939bb45820445c5d6508350257
Hernan Ponce de Leon <zeta96@gmail.com>
2022-09-20 17:17:02 +0200

    Changes for the new version of the atomic-replace pass

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/boogie/LlvmProcedures.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/boogie/LlvmProcedures.java
@@ -16,15 +16,14 @@ import java.util.List;
 public class LlvmProcedures {
 
 	public static List<String> LLVMPROCEDURES = Arrays.asList(
-			"my_load_atomic",
-			"my_store_atomic",
-			"my_atomicrmw_cmpxchg",
-			"my_atomicrmw_xchg",
-			"my_atomicrmw_add",
-			"my_atomicrmw_sub",
-			"my_atomicrmw_and",
-			"my_atomicrmw_or",
-			"my_atomicrmw_xor");
+			"__llvm_atomic32_load",
+			"__llvm_atomic64_load",
+			"__llvm_atomic32_store",
+			"__llvm_atomic64_store",
+			"__llvm_atomic32_cmpxchg",
+			"__llvm_atomic64_cmpxchg",
+			"__llvm_atomic32_rmw",
+			"__llvm_atomic64_rmw");
 
 	public static void handleLlvmFunction(VisitorBoogie visitor, Call_cmdContext ctx) {
 		String name = ctx.call_params().Define() == null ? ctx.call_params().Ident(0).getText() : ctx.call_params().Ident(1).getText();
@@ -40,59 +39,58 @@ public class LlvmProcedures {
 		String mo;
 		
 		switch (name) {
-		case "my_load_atomic":
-			mo = C11.intToMo(((IConst) p1).getValueAsInt());
-			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newLoad(reg, (IExpr) p0, mo))
-	        	.setCLine(visitor.currentLine)
-	        	.setSourceCodeFile(visitor.sourceCodeFile);
-	        return;
-		case "my_store_atomic":
-			mo = C11.intToMo(((IConst) p2).getValueAsInt());
-			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newStore((IExpr) p0, (ExprInterface) p1, mo))
+			case "__llvm_atomic32_load":
+			case "__llvm_atomic64_load":
+				mo = C11.intToMo(((IConst) p1).getValueAsInt());
+				visitor.programBuilder.addChild(visitor.threadCount, Llvm.newLoad(reg, (IExpr) p0, mo))
+	        		.setCLine(visitor.currentLine)
+	        		.setSourceCodeFile(visitor.sourceCodeFile);
+	        	return;
+			case "__llvm_atomic32_store":
+			case "__llvm_atomic64_store":
+				mo = C11.intToMo(((IConst) p2).getValueAsInt());
+				visitor.programBuilder.addChild(visitor.threadCount, Llvm.newStore((IExpr) p0, (ExprInterface) p1, mo))
+					.setCLine(visitor.currentLine)
+					.setSourceCodeFile(visitor.sourceCodeFile);
+	        	return;
+			case "__llvm_atomic32_cmpxchg":
+			case "__llvm_atomic64_cmpxchg":
+				mo = C11.intToMo(((IConst) p3).getValueAsInt());
+				visitor.programBuilder.addChild(visitor.threadCount, Llvm.newCompareExchange(reg, (IExpr) p0, (IExpr) p1, (IExpr) p2, mo, true))
+	        		.setCLine(visitor.currentLine)
+	        		.setSourceCodeFile(visitor.sourceCodeFile);
+				return;		
+			case "__llvm_atomic32_rmw":
+			case "__llvm_atomic64_rmw":
+				mo = C11.intToMo(((IConst) p2).getValueAsInt());
+				IOpBin op;
+				switch (((IConst) p3).getValueAsInt()) {
+					case 0:
+						visitor.programBuilder.addChild(visitor.threadCount, Llvm.newExchange(reg, (IExpr) p0, (IExpr) p1, mo))
+							.setCLine(visitor.currentLine)
+							.setSourceCodeFile(visitor.sourceCodeFile);
+						return;
+					case 1:
+						op = IOpBin.PLUS;
+						break;
+					case 2:
+						op = IOpBin.MINUS;
+						break;
+					case 3:
+						op = IOpBin.AND;
+						break;
+					case 4:
+						op = IOpBin.OR;
+						break;
+					case 5:
+						op = IOpBin.XOR;
+						break;
+					default:
+                	    throw new UnsupportedOperationException("Operation " + params.get(3).getText() + " is not recognized.");
+				}
+			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newRMW(reg, (IExpr) p0, (IExpr) p1, op, mo))
 				.setCLine(visitor.currentLine)
 				.setSourceCodeFile(visitor.sourceCodeFile);
-	        return;
-		case "my_atomicrmw_cmpxchg":
-			mo = C11.intToMo(((IConst) p3).getValueAsInt());
-			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newCompareExchange(reg, (IExpr) p0, (IExpr) p1, (IExpr) p2, mo, true))
-	        	.setCLine(visitor.currentLine)
-	        	.setSourceCodeFile(visitor.sourceCodeFile);
-			return;		
-		case "my_atomicrmw_xchg":
-			mo = C11.intToMo(((IConst) p2).getValueAsInt());
-			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newExchange(reg, (IExpr) p0, (IExpr) p1, mo))
-	        	.setCLine(visitor.currentLine)
-	        	.setSourceCodeFile(visitor.sourceCodeFile);
-			return;
-		case "my_atomicrmw_add":
-			mo = C11.intToMo(((IConst) p2).getValueAsInt());
-			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newRMW(reg, (IExpr) p0, (IExpr) p1, IOpBin.PLUS, mo))
-	        	.setCLine(visitor.currentLine)
-	        	.setSourceCodeFile(visitor.sourceCodeFile);
-			return;
-		case "my_atomicrmw_sub":
-			mo = C11.intToMo(((IConst) p2).getValueAsInt());
-			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newRMW(reg, (IExpr) p0, (IExpr) p1, IOpBin.MINUS, mo))
-	        	.setCLine(visitor.currentLine)
-	        	.setSourceCodeFile(visitor.sourceCodeFile);
-			return;
-		case "my_atomicrmw_and":
-			mo = C11.intToMo(((IConst) p2).getValueAsInt());
-			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newRMW(reg, (IExpr) p0, (IExpr) p1, IOpBin.AND, mo))
-	        	.setCLine(visitor.currentLine)
-	        	.setSourceCodeFile(visitor.sourceCodeFile);
-			return;
-		case "my_atomicrmw_or":
-			mo = C11.intToMo(((IConst) p2).getValueAsInt());
-			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newRMW(reg, (IExpr) p0, (IExpr) p1, IOpBin.OR, mo))
-	        	.setCLine(visitor.currentLine)
-	        	.setSourceCodeFile(visitor.sourceCodeFile);
-			return;
-		case "my_atomicrmw_xor":
-			mo = C11.intToMo(((IConst) p2).getValueAsInt());
-			visitor.programBuilder.addChild(visitor.threadCount, Llvm.newRMW(reg, (IExpr) p0, (IExpr) p1, IOpBin.XOR, mo))
-	        	.setCLine(visitor.currentLine)
-	        	.setSourceCodeFile(visitor.sourceCodeFile);
 			return;
 		default:
 			throw new UnsupportedOperationException(name + " procedure is not part of LKMMPROCEDURES");
