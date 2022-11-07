9807504c6e3ea95c0414a059e7485777bff671d3
Hernan Ponce de Leon <zeta96@gmail.com>
2022-11-07 17:02:21 +0100

    Use riscv-orig.cat in xra script

--- scripts/xra-all.sh
+++ scripts/xra-all.sh
@@ -2,5 +2,5 @@
  
 bash scripts/xra.sh tso.cat TSO
 bash scripts/xra.sh power.cat POWER
-bash scripts/xra.sh riscv.cat RISCV
+bash scripts/xra.sh riscv-orig.cat RISCV
 bash scripts/xra.sh aarch64.cat ARM8
