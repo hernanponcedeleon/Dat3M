697e5618eaaacb470e25ab10c59319ebd9b4babf
Hernan Ponce de Leon <zeta96@gmail.com>
2022-11-20 13:48:18 +0100

    Simplify compilation code

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/utils/Compilation.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/utils/Compilation.java
@@ -18,8 +18,7 @@ public class Compilation {
 	private static final Logger logger = LogManager.getLogger(Compilation.class);
 	
 	public static File compileWithClang(File file, String cflags) throws Exception {
-		String output = System.getenv("DAT3M_OUTPUT") + "/" + 
-			file.getName().substring(0, file.getName().lastIndexOf('.')) + ".ll";		
+		String output = getOutputName(file, ".ll");		
 		
 		ArrayList<String> cmd = new ArrayList<String>();
 		cmd.addAll(asList("clang", "-Xclang", "-disable-O0-optnone", "-S", 
@@ -32,22 +31,11 @@ public class Compilation {
 		}
 		cmd.add(file.getAbsolutePath());
 		
-		ProcessBuilder processBuilder = new ProcessBuilder(cmd);
-		logger.info("Compiling with clang");
-		logger.debug("Running " + String.join(" ", cmd));
-		Process proc = processBuilder.start();
-		proc.waitFor();
-		if(proc.exitValue() == 1) {
-			String errorString = CharStreams.toString(new InputStreamReader(proc.getErrorStream(), Charsets.UTF_8));
-			throw new Exception(errorString);
-		}
-
-		return new File(output);
+		return runCmd(cmd, output);
 	}
 
 	public static File applyLlvmPasses(File file) throws Exception {
-		String output = System.getenv("DAT3M_OUTPUT") + "/" + 
-			file.getName().substring(0, file.getName().lastIndexOf('.')) + "-opt.ll";		
+		String output = getOutputName(file, "-opt.ll");		
 
 		List<String> optPasses = Arrays.asList( 
 			"-mem2reg",
@@ -57,27 +45,16 @@ public class Compilation {
 			"-gvn");
 
 		ArrayList<String> cmd = new ArrayList<String>();
-    	cmd.addAll(asList("opt", "-enable-new-pm=0", "-load=" + System.getenv("DAT3M_PASSES_HOME") + 
-			"/libatomic-replace.so", "-atomic-replace"));
+    	cmd.addAll(asList("opt", "-enable-new-pm=0", "-load=" + System.getenv("DAT3M_HOME") + 
+			"/llvm-passes/bin/libatomic-replace.so", "-atomic-replace"));
 		cmd.addAll(optPasses);
 		cmd.addAll(asList(file.getAbsolutePath(), "-S", "-o", output));
-    	
-		ProcessBuilder processBuilder = new ProcessBuilder(cmd);
-    	logger.info("Running LLVM passes");
-    	logger.debug("Running " + String.join(" ", cmd));
-    	Process proc = processBuilder.start();
-    	proc.waitFor();
-    	if(proc.exitValue() == 1) {
-    		String errorString = CharStreams.toString(new InputStreamReader(proc.getErrorStream(), Charsets.UTF_8));
-			throw new Exception(errorString);
-    	}
-		
-		return new File(output);
+
+		return runCmd(cmd, output);
 	}
 
 	public static File compileWithSmack(File file, String cflags) throws Exception {
-		String output = System.getenv("DAT3M_OUTPUT") + "/" + 
-			file.getName().substring(0, file.getName().lastIndexOf('.')) + ".bpl";
+		String output = getOutputName(file, ".bpl");
 
     	ArrayList<String> cmd = new ArrayList<String>();
     	cmd.add("smack");
@@ -88,27 +65,23 @@ public class Compilation {
 		cmd.add("--clang-options=" + cflags);
     	cmd.addAll(asList("-bpl", output, file.getAbsolutePath()));
     	
-		logger.info("Compiling with smack");
-    	logger.debug("Running " + String.join(" ", cmd));
+		return runCmd(cmd, output);
+	}	
 
-    	ProcessBuilder processBuilder = new ProcessBuilder(cmd); 
+	private static String getOutputName(File file, String postfix) {
+		return System.getenv("DAT3M_OUTPUT") + "/" + 
+			file.getName().substring(0, file.getName().lastIndexOf('.')) + postfix;
+	}
+
+	private static File runCmd(ArrayList<String> cmd, String outputFileName) throws Exception {
+		ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+    	logger.info("Running " + String.join(" ", cmd));
     	Process proc = processBuilder.start();
     	proc.waitFor();
-		// TODO: get rid of this once smack works properly on MacOS
-    	int tries = 1 ;
-    	while(proc.exitValue() != 0 && tries < 100) {
-    		try (InputStreamReader reader = new InputStreamReader(proc.getErrorStream(), Charsets.UTF_8)) {
-				String errorString = CharStreams.toString(reader);
-				if(errorString.contains("compile_to_bc")) {
-					throw new Exception(errorString);
-				}
-			}
-    		logger.info("Compiling with smack");
-    		tries++;
-        	proc = processBuilder.start();
-        	proc.waitFor();
+    	if(proc.exitValue() == 1) {
+    		String errorString = CharStreams.toString(new InputStreamReader(proc.getErrorStream(), Charsets.UTF_8));
+			throw new Exception(errorString);
     	}
-
-		return new File(output);
-	}	
+		return new File(outputFileName);	
+	}
 }
