605dc6a467033f47ca04baeb20eb390f6a88e14b
Hernan Ponce de Leon <zeta96@gmail.com>
2022-11-22 17:39:21 +0100

    Remove unused imports

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/event/lang/pthread/Join.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/lang/pthread/Join.java
@@ -2,13 +2,8 @@ package com.dat3m.dartagnan.program.event.lang.pthread;
 
 import com.dat3m.dartagnan.program.Register;
 import com.dat3m.dartagnan.program.event.Tag;
-import com.dat3m.dartagnan.program.event.core.Load;
 import com.dat3m.dartagnan.program.event.core.Local;
 import com.dat3m.dartagnan.program.event.visitors.EventVisitor;
-import com.dat3m.dartagnan.program.memory.MemoryObject;
-
-import static com.dat3m.dartagnan.program.event.Tag.C11.MO_SC;
-
 import com.dat3m.dartagnan.expression.IExpr;
 
 public class Join extends Local {
