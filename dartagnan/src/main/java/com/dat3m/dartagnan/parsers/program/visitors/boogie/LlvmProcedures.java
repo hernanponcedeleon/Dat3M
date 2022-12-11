d6f2374025e3fc3d6542a633b7cf4be4f44f2653
Hernan Ponce de Leon <zeta96@gmail.com>
2022-12-11 14:19:08 +0100

    Solve problem after merge

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/boogie/LlvmProcedures.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/boogie/LlvmProcedures.java
@@ -61,21 +61,18 @@ public class LlvmProcedures {
 			case "__llvm_atomic64_load":
 				mo = C11.intToMo(((IConst) p1).getValueAsInt());
 				visitor.programBuilder.addChild(visitor.threadCount, Llvm.newLoad(reg, (IExpr) p0, mo))
-	        		.setCLine(visitor.currentLine)
-	        		.setSourceCodeFile(visitor.sourceCodeFile);
+						.setCFileInformation(visitor.currentLine, visitor.sourceCodeFile);
 	        	return;
 			case "__llvm_atomic32_store":
 			case "__llvm_atomic64_store":
 				mo = C11.intToMo(((IConst) p2).getValueAsInt());
 				visitor.programBuilder.addChild(visitor.threadCount, Llvm.newStore((IExpr) p0, (ExprInterface) p1, mo))
-					.setCLine(visitor.currentLine)
-					.setSourceCodeFile(visitor.sourceCodeFile);
+						.setCFileInformation(visitor.currentLine, visitor.sourceCodeFile);
 				return;
 			case "__llvm_atomic_fence":
 				mo = C11.intToMo(((IConst) p0).getValueAsInt());
 				visitor.programBuilder.addChild(visitor.threadCount, Llvm.newFence(mo))
-					.setCLine(visitor.currentLine)
-					.setSourceCodeFile(visitor.sourceCodeFile);
+						.setCFileInformation(visitor.currentLine, visitor.sourceCodeFile);
 	        	return;
 			case "__llvm_atomic32_cmpxchg":
 			case "__llvm_atomic64_cmpxchg":
@@ -88,8 +85,7 @@ public class LlvmProcedures {
 				// assign the correct values to the registers above
 				mo = C11.intToMo(((IConst) p3).getValueAsInt());
 				visitor.programBuilder.addChild(visitor.threadCount, Llvm.newCompareExchange(oldValueRegister, cmpRegister, (IExpr) p0, (IExpr) p1, (IExpr) p2, mo, true))
-	        		.setCLine(visitor.currentLine)
-	        		.setSourceCodeFile(visitor.sourceCodeFile);
+						.setCFileInformation(visitor.currentLine, visitor.sourceCodeFile);
 				return;		
 			case "__llvm_atomic32_rmw":
 			case "__llvm_atomic64_rmw":
@@ -98,8 +94,7 @@ public class LlvmProcedures {
 				switch (((IConst) p3).getValueAsInt()) {
 					case 0:
 						visitor.programBuilder.addChild(visitor.threadCount, Llvm.newExchange(reg, (IExpr) p0, (IExpr) p1, mo))
-							.setCLine(visitor.currentLine)
-							.setSourceCodeFile(visitor.sourceCodeFile);
+								.setCFileInformation(visitor.currentLine, visitor.sourceCodeFile);
 						return;
 					case 1:
 						op = IOpBin.PLUS;
@@ -120,8 +115,7 @@ public class LlvmProcedures {
                 	    throw new UnsupportedOperationException("Operation " + params.get(3).getText() + " is not recognized.");
 				}
 				visitor.programBuilder.addChild(visitor.threadCount, Llvm.newRMW(reg, (IExpr) p0, (IExpr) p1, op, mo))
-					.setCLine(visitor.currentLine)
-					.setSourceCodeFile(visitor.sourceCodeFile);
+						.setCFileInformation(visitor.currentLine, visitor.sourceCodeFile);
 				return;
 			case "llvm.smax.i32":
 			case "llvm.smax.i64":
@@ -131,8 +125,7 @@ public class LlvmProcedures {
 				i2 = (IExpr)p1;
 				cond = name.contains("max") ? new Atom(i1, COpBin.GTE, i2) : new Atom(i1, COpBin.LTE, i2);
 				visitor.programBuilder.addChild(visitor.threadCount, EventFactory.newLocal(reg, new IfExpr(cond, i1, i2)))
-					.setCLine(visitor.currentLine)
-					.setSourceCodeFile(visitor.sourceCodeFile);
+						.setCFileInformation(visitor.currentLine, visitor.sourceCodeFile);
 				return;
 			default:
 				throw new UnsupportedOperationException(name + " procedure is not part of LLVMPROCEDURES");
