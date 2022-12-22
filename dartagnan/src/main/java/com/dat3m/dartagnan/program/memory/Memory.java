b10c1242ceb51991c369934b95a7b3828a010564
Hernan Ponce de Leon <zeta96@gmail.com>
2022-12-22 10:33:36 +0100

    Addresses must be even

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/memory/Memory.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/memory/Memory.java
@@ -55,7 +55,11 @@ public class Memory {
             BigInteger i = BigInteger.ONE;
             for(MemoryObject a : program.getMemory().objects) {
                 a.address = i;
-                i = i.add(BigInteger.valueOf(a.size()));
+                // Addresses are tipically at least two byte aligned
+                //      https://stackoverflow.com/questions/23315939/why-2-lsbs-of-32-bit-arm-instruction-address-not-used
+                // Many algorithms rely on this assumption for correctness
+                int padding = (a.size() % 2);
+                i = i.add(BigInteger.valueOf(a.size() + padding));
             }
         }
     }
