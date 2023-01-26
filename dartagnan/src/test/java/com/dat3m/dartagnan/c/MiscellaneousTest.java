fd43c4e1ce96d05ae56d6e863c5a653b1db6d96f
Thomas Haas <tomy.haas@t-online.de>
2023-01-26 19:19:46 +0100

    Added new MiscellaneousTest Added idd_dynamic.c as test which breaks when data-dependencies are not treated dynamically.

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/c/MiscellaneousTest.java
@@ -0,0 +1,57 @@
+package com.dat3m.dartagnan.c;
+
+import com.dat3m.dartagnan.configuration.Arch;
+import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.utils.rules.Provider;
+import com.dat3m.dartagnan.verification.solving.AssumeSolver;
+import com.dat3m.dartagnan.verification.solving.RefinementSolver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static com.dat3m.dartagnan.configuration.Arch.ARM8;
+import static com.dat3m.dartagnan.utils.ResourceHelper.TEST_RESOURCE_PATH;
+import static com.dat3m.dartagnan.utils.Result.FAIL;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class MiscellaneousTest extends AbstractCTest {
+
+    public MiscellaneousTest(String name, Arch target, Result expected) {
+        super(name, target, expected);
+    }
+
+    @Override
+    protected Provider<String> getProgramPathProvider() {
+        return Provider.fromSupplier(() -> TEST_RESOURCE_PATH + "miscellaneous/" + name + ".bpl");
+    }
+
+    @Override
+    protected long getTimeout() {
+        return 60000;
+    }
+
+	@Parameterized.Parameters(name = "{index}: {0}, target={1}")
+    public static Iterable<Object[]> data() throws IOException {
+    	return Arrays.asList(new Object[][]{
+	            {"idd_dynamic", ARM8, FAIL},
+		});
+    }
+
+    @Test
+	//@CSVLogger.FileName("csv/assume")
+	public void testAssume() throws Exception {
+		AssumeSolver s = AssumeSolver.run(contextProvider.get(), proverProvider.get(), taskProvider.get());
+		assertEquals(expected, s.getResult());
+	}
+
+    @Test
+	//@CSVLogger.FileName("csv/refinement")
+	public void testRefinement() throws Exception {
+		RefinementSolver s = RefinementSolver.run(contextProvider.get(), proverProvider.get(), taskProvider.get());
+		assertEquals(expected, s.getResult());
+	}
+}
