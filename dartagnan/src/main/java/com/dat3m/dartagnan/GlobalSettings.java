ea678f29172d83c74e11dab8e5f8555b820b42aa
Thomas Haas <tomy.haas@t-online.de>
2023-02-05 16:10:23 +0100

    Restored ARCH_PRECISION = -1

--- dartagnan/src/main/java/com/dat3m/dartagnan/GlobalSettings.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/GlobalSettings.java
@@ -12,7 +12,7 @@ public class GlobalSettings {
     // === Encoding ===
 	// This has to be in sync with whatever smack generated for references, 
 	// i.e. if type ref = X, then ARCH_PRECISION = X (or -1 if type = intX). 
-    public static final int ARCH_PRECISION = 64;
+    public static final int ARCH_PRECISION = -1;
 
     // === Parsing ===
     public static final boolean ATOMIC_AS_LOCK = false;
