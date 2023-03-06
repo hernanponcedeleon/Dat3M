c9d11c6a12108f93f511ab012e7696d55798baab
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2023-03-06 17:22:19 +0100

    Add pthread header
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

---
+++ include/pthread.h
@@ -0,0 +1,27 @@
+extern int pthread_create(pthread_t *restrict thread,
+                          const pthread_attr_t *restrict attr,
+                          void *(*start_routine)(void *),
+                          void *restrict arg);
+
+extern int pthread_join(pthread_t thread, void **retval);
+
+#ifdef USE_PTHREAD_MUTEX_IMPLEMENTATION
+#include<stdatomic.h>
+#define pthread_mutex_t atomic_int
+int pthread_mutex_lock(pthread_mutex_t *mutex) {
+    while(atomic_compare_exchange_strong_explicit(mutex, 0, 1, memory_order_acquire, memory_order_acquire));
+    return 0;
+}
+int pthread_mutex_unlock(pthread_mutex_t *mutex) {
+    atomic_store_explicit(mutex, 0, memory_order_release);
+    return 0;
+}
+extern int pthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr_t *restrict attr) {
+    atomic_store_explicit(mutex, 0, memory_order_release);
+    return 0;
+}
+#else
+extern int pthread_mutex_lock(pthread_mutex_t *mutex);
+extern int pthread_mutex_unlock(pthread_mutex_t *mutex);
+extern int pthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr_t *restrict attr);
+#endif
