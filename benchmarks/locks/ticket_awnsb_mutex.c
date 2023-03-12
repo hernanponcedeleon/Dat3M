ab758c896bf6ac169b29847a68ed54d750ede1e0
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2023-03-12 20:14:40 +0100

    Add ticket_awnsb_mutex to lock benchmarks
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

---
+++ benchmarks/locks/ticket_awnsb_mutex.c
@@ -0,0 +1,41 @@
+#include <pthread.h>
+#include "ticket_awnsb_mutex.h"
+#include <assert.h>
+
+#ifndef NTHREADS
+#define NTHREADS 3
+#endif
+
+int shared;
+ticket_awnsb_mutex_t lock;
+int sum = 0;
+
+void *thread_n(void *arg)
+{
+    intptr_t index = ((intptr_t) arg);
+
+    ticket_awnsb_mutex_lock(&lock);
+    shared = index;
+    int r = shared;
+    assert(r == index);
+    sum++;
+    ticket_awnsb_mutex_unlock(&lock);
+    return NULL;
+}
+
+int main()
+{
+    pthread_t t[NTHREADS];
+
+    ticket_awnsb_mutex_init(&lock, DEFAULT_MAX_WAITERS);
+
+    for (int i = 0; i < NTHREADS; i++)
+        pthread_create(&t[i], 0, thread_n, (void *)i);
+
+    for (int i = 0; i < NTHREADS; i++)
+        pthread_join(t[i], 0);
+
+    assert(sum == NTHREADS);
+
+    return 0;
+}
