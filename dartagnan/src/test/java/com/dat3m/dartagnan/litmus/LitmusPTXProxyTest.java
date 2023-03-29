9713eb9c408d41e17cce914c12ec9fbcaaaac14d
Haining <haining.tong@helsinki.fi>
2023-03-29 18:03:12 +0300

    add unit test for ptx proxy

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/litmus/LitmusPTXProxyTest.java
@@ -0,0 +1,27 @@
+package com.dat3m.dartagnan.litmus;
+
+import com.dat3m.dartagnan.configuration.Arch;
+import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.utils.rules.Provider;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+
+@RunWith(Parameterized.class)
+public class LitmusPTXProxyTest extends AbstractLitmusTest {
+
+    @Parameterized.Parameters(name = "{index}: {0}, {1}")
+    public static Iterable<Object[]> data() throws IOException {
+        return buildLitmusTests("litmus/PTX-PROXY/", "PTX");
+    }
+
+    @Override
+    protected Provider<Arch> getTargetProvider() {
+        return () -> Arch.PTX;
+    }
+
+    public LitmusPTXProxyTest(String path, Result expected) {
+        super(path, expected);
+    }
+}
