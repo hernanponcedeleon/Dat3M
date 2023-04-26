66cc2577289c48f8bb16cfa61281801000206a5a
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2023-04-26 15:25:09 +0200

    Allow several pair of threads

--- benchmarks/lfds/hash_table.c
+++ benchmarks/lfds/hash_table.c
@@ -2,18 +2,14 @@
 #include <pthread.h>
 #include <assert.h>
 
-#ifndef SETTERS
-#define SETTERS 2
+#ifndef PAIRS
+#define PAIRS 2
 #endif
 
-#ifndef GETTERS
-#define GETTERS 2
-#endif
+#define NTHREADS (2 * PAIRS)
 
-#define NTHREADS (SETTERS + GETTERS)
-
-atomic_int x;
-int a,b;
+atomic_int data[PAIRS];
+int read_flag[PAIRS],read_data[PAIRS];
 
 // The hash_table is resilient against memory reordering, but externally, the
 // caller may still need to enforce memory ordering using fence instructions. 
@@ -21,35 +17,47 @@ int a,b;
 // by storing a flag in the collection (MP pattern), you must place release 
 // semantics on that store by issuing a release fence immediately beforehand.
 
-void *thread_1(void *unused)
+void *thread_1(void *arg)
 {
-	atomic_store_explicit(&x, 1, memory_order_relaxed);
+    intptr_t idx = ((intptr_t) arg);
+	atomic_store_explicit(&data[idx], 1, memory_order_relaxed);
 #ifndef FAIL
 	atomic_thread_fence(memory_order_release);
 #endif
-	set(1, 1);
+    // Set flag. +1 because keys cannot be 0
+	set(idx+1, 1);
 	return NULL;
 }
 
-void *thread_2(void *unused)
+void *thread_2(void *arg)
 {
-	a = get(1);
+    intptr_t idx = ((intptr_t) arg);
+    // Read flag. +1 because keys cannot be 0
+	read_flag[idx] = get(idx+1);
 	atomic_thread_fence(memory_order_acquire);
-	b = atomic_load_explicit(&x, memory_order_relaxed);
+	read_data[idx] = atomic_load_explicit(&data[idx], memory_order_relaxed);
 	return NULL;
 }
 
 int main()
 {
-	pthread_t t1, t2;
+    pthread_t t[NTHREADS];
 
-	pthread_create(&t1, NULL, thread_1, NULL);
-	pthread_create(&t2, NULL, thread_2, NULL);
+    init();
 
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
+    for (int i = 0; i < PAIRS; i++)
+        pthread_create(&t[i], 0, thread_1, (void *)i);
+    
+    for (int i = 0; i < PAIRS; i++)
+        pthread_create(&t[PAIRS + i], 0, thread_2, (void *)i);
 
-	assert(!(a == 1 && b == 0));
+    for (int i = 0; i < NTHREADS; i++)
+        pthread_join(t[i], 0);
+
+    for (int i = 0; i < PAIRS; i++)
+    	assert(!(read_flag[i] == 1 && read_data[i] == 0));
+
+    assert(count() == PAIRS);
 
 	return 0;
 }
