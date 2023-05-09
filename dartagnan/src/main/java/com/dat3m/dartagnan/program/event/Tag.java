67c332180bbf9772db80d979c0a7b3cbaae979a5
Thomas Haas <tomy.haas@t-online.de>
2023-05-09 18:42:00 +0200

    Removed Tag.ANNOTATION

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/event/Tag.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/Tag.java
@@ -21,7 +21,6 @@ public final class Tag {
     public static final String SPINLOOP         = "SPINLOOP";
     // Some events should not be optimized (e.g. fake dependencies) or deleted (e.g. bounds)
     public static final String NOOPT            = "NOOPT";
-    public static final String ANNOTATION       = "ANNOTATION";
     public static final String STARTLOAD        = "STARTLOAD";
 
     // =============================================================================================
