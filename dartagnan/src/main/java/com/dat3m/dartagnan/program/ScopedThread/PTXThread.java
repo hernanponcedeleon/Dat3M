c28a38f1b5182ad7917978f501ec186a5ba8410a
Haining <haining.tong@helsinki.fi>
2023-05-15 13:53:28 +0300

    change ScopedThread from interface to class

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/ScopedThread/PTXThread.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/ScopedThread/PTXThread.java
@@ -1,20 +1,15 @@
 package com.dat3m.dartagnan.program.ScopedThread;
 
-import com.dat3m.dartagnan.program.Thread;
 import com.dat3m.dartagnan.program.event.Tag;
 import com.dat3m.dartagnan.program.event.core.Event;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
-public class PTXThread extends Thread implements ScopedThread {
-    private final ArrayList<String> scopes =
-            new ArrayList<>(Arrays.asList(Tag.PTX.SYS, Tag.PTX.GPU, Tag.PTX.CTA));
-    private final HashMap<String, Integer> scopeIds = new HashMap<>();
+public class PTXThread extends ScopedThread {
 
     public PTXThread(String name, int id, Event entry, int GpuId, int CtaId) {
         super(name, id, entry);
+        scopes.addAll(Arrays.asList(Tag.PTX.SYS, Tag.PTX.GPU, Tag.PTX.CTA));
         this.scopeIds.put(Tag.PTX.SYS, 0);
         this.scopeIds.put(Tag.PTX.GPU, GpuId);
         this.scopeIds.put(Tag.PTX.CTA, CtaId);
@@ -22,21 +17,9 @@ public class PTXThread extends Thread implements ScopedThread {
 
     public PTXThread(int id, Event entry, int GpuId, int CtaId) {
         super(id, entry);
+        scopes.addAll(Arrays.asList(Tag.PTX.SYS, Tag.PTX.GPU, Tag.PTX.CTA));
         this.scopeIds.put(Tag.PTX.SYS, 0);
         this.scopeIds.put(Tag.PTX.GPU, GpuId);
         this.scopeIds.put(Tag.PTX.CTA, CtaId);
-    }
-
-    @Override
-    public ArrayList<String> getScopes() {
-        return scopes;
-    }
-
-    @Override
-    public int getScopeIds(String scope) {
-        if (!this.scopeIds.containsKey(scope)) {
-            return -1;
-        }
-        return this.scopeIds.get(scope);
     }
 }
