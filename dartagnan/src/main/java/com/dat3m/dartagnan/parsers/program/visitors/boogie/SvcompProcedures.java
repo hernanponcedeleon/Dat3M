ce82628cf97eafb8b4f3f5d7c7c5546b93967ca8
René Pascal Maseli <r.maseli@tu-bs.de>
2023-05-24 15:07:21 +0200

    __VERIFIER_nondet_bool() returns bv1

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/boogie/SvcompProcedures.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/boogie/SvcompProcedures.java
@@ -169,7 +169,8 @@ public class SvcompProcedures {
     private static void __VERIFIER_nondet_bool(VisitorBoogie visitor, Call_cmdContext ctx) {
         String registerName = ctx.call_params().Ident(0).getText();
         Optional<Register> register = visitor.thread.getRegister( visitor.currentScope.getID() + ":" + registerName);
-        register.ifPresent(value -> visitor.append(EventFactory.newLocal(value, visitor.program.newConstant(visitor.types.getBooleanType(), false, null, null))));
+        Expression expression = visitor.program.newConstant(visitor.types.getIntegerType(1), false, BigInteger.ZERO, BigInteger.ONE);
+        register.ifPresent(value -> visitor.append(EventFactory.newLocal(value, expression)));
 
     }
 
