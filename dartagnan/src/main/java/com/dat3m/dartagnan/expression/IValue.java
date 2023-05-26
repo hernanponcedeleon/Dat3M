85c6e085ffc93d7c76c546ee872488b996ebfca0
René Pascal Maseli <r.maseli@tu-bs.de>
2023-05-26 14:31:56 +0200

    Add IValue(Type,BigInteger)

--- dartagnan/src/main/java/com/dat3m/dartagnan/expression/IValue.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/expression/IValue.java
@@ -1,8 +1,8 @@
 package com.dat3m.dartagnan.expression;
 
 import com.dat3m.dartagnan.expression.processing.ExpressionVisitor;
-
-import static com.dat3m.dartagnan.GlobalSettings.*;
+import com.dat3m.dartagnan.expression.type.IntegerType;
+import com.dat3m.dartagnan.expression.type.TypeFactory;
 
 import java.math.BigInteger;
 
@@ -11,20 +11,27 @@ import java.math.BigInteger;
  */
 public final class IValue extends IConst {
 
+    private static final TypeFactory types = TypeFactory.getInstance();
+
     // TODO(TH): not sure where this are used, but why do you set the precision to 0?
     // TH: This was a temporary try for integer logic only
     // However, it is impossible to define general constants, we need a function that produces
     // a constant for each precision degree
     // HP: agree. I assume you wanted this to improve code readability, but having one constant per precision won't help.
-    public static IConst ZERO = new IValue(BigInteger.ZERO, getArchPrecision());
-    public static IConst ONE = new IValue(BigInteger.ONE, getArchPrecision());
+    public static IConst ZERO = new IValue(types.getArchType(), BigInteger.ZERO);
+    public static IConst ONE = new IValue(types.getArchType(), BigInteger.ONE);
 
     private final BigInteger value;
-    private final int precision;
+    private final IntegerType type;
 
+    @Deprecated
     public IValue(BigInteger v, int p) {
+        this(p == -1 ? types.getIntegerType() : types.getIntegerType(p), v);
+    }
+
+    public IValue(IntegerType t, BigInteger v) {
+        type = t;
         value = v;
-        precision = p;
     }
 
     @Override
@@ -34,7 +41,7 @@ public final class IValue extends IConst {
 
     @Override
     public int getPrecision() {
-        return precision;
+        return type.isMathematical() ? -1 : type.getBitWidth();
     }
 
     @Override
@@ -44,16 +51,16 @@ public final class IValue extends IConst {
 
     @Override
     public int hashCode() {
-        return value.hashCode() + (precision > 0 ? 0 : precision);
+        return type.hashCode() ^ 0xa185f6b3 + value.hashCode();
     }
 
     @Override
     public boolean equals(Object o) {
-        return this == o || o instanceof IValue && value.equals(((IValue)o).value) && precision == ((IValue)o).precision;
+        return this == o || o instanceof IValue && type.equals(((IValue) o).type) && value.equals(((IValue) o).value);
     }
 
     @Override
     public String toString() {
-        return value.toString();
+        return value.toString() + type.toString();
     }
 }
