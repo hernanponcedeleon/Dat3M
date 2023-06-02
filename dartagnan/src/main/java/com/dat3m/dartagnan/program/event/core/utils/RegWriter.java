bcc8b821c59ee8c795b5252d44ba29488869a971
Thomas Haas <tomy.haas@t-online.de>
2023-06-02 12:31:05 +0200

    Made RegReader and RegWriter extend new Event interface

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/event/core/utils/RegWriter.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/core/utils/RegWriter.java
@@ -1,8 +1,9 @@
 package com.dat3m.dartagnan.program.event.core.utils;
 
 import com.dat3m.dartagnan.program.Register;
+import com.dat3m.dartagnan.program.event.core.Event;
 
-public interface RegWriter {
+public interface RegWriter extends Event {
 
     Register getResultRegister();
 }
