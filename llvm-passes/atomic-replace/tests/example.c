b24ea02d38481629bcdd803db3e361c93e181d8b
Tomasz Idziaszek <tomasz.idziaszek.ext@huawei.com>
2023-06-22 10:59:12 +0200

    Add support for i8 and i16 types in atomic-replace LLVM pass
    Signed-off-by: Tomasz Idziaszek <tomasz.idziaszek.ext@huawei.com>

--- llvm-passes/atomic-replace/tests/example.c
+++ llvm-passes/atomic-replace/tests/example.c
@@ -1,13 +1,31 @@
 #include <stdatomic.h>
 
-atomic_int x, y;
+atomic_char x1, y1;
+atomic_short x2, y2;
+atomic_int x4, y4;
 atomic_llong x8, y8;
 
+int __attribute__((noinline)) check_atomic_8() {
+  char a    = atomic_load(&x1);
+  char b    = atomic_exchange_explicit(&y1, a, memory_order_acquire);
+  _Bool res = atomic_compare_exchange_strong(&x1, &a, b);
+  atomic_store_explicit(&x1, b, memory_order_relaxed);
+  return res;
+}
+
+int __attribute__((noinline)) check_atomic_16() {
+  short a   = atomic_load(&x2);
+  short b   = atomic_exchange_explicit(&y2, a, memory_order_acquire);
+  _Bool res = atomic_compare_exchange_strong(&x2, &a, b);
+  atomic_store_explicit(&x2, b, memory_order_relaxed);
+  return res;
+}
+
 int __attribute__((noinline)) check_atomic_32() {
-  int a     = atomic_load(&x);
-  int b     = atomic_exchange_explicit(&y, a, memory_order_acquire);
-  _Bool res = atomic_compare_exchange_strong(&x, &a, b);
-  atomic_store_explicit(&x, b, memory_order_relaxed);
+  int a     = atomic_load(&x4);
+  int b     = atomic_exchange_explicit(&y4, a, memory_order_acquire);
+  _Bool res = atomic_compare_exchange_strong(&x4, &a, b);
+  atomic_store_explicit(&x4, b, memory_order_relaxed);
   return res;
 }
 
@@ -24,6 +42,8 @@ void __attribute__((noinline)) check_fence() {
 }
 
 int main() {
+  check_atomic_8();
+  check_atomic_16();
   check_atomic_32();
   check_atomic_64();
   check_fence();
