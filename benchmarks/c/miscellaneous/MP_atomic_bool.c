dd9d01fb6799b72642267ffb43b36ce86cd3d5a6
Hernan Ponce de Leon <zeta96@gmail.com>
2023-06-22 21:50:21 +0200

    Llvm atomic (#472)
    * Add __llvm_atomic8 and __llvm_atomic16
    
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

---
+++ benchmarks/c/miscellaneous/MP_atomic_bool.c
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include <stdatomic.h>
+
+atomic_bool flag;
+atomic_int data;
+
+void *producer(void *arg)
+{
+    atomic_store_explicit(&data, 42, memory_order_relaxed);
+#ifdef FAIL
+    atomic_store_explicit(&flag, 1, memory_order_relaxed);
+#else
+    atomic_store_explicit(&flag, 1, memory_order_release);
+#endif
+}
+
+void *consumer(void *arg)
+{
+    while (atomic_load_explicit(&flag, memory_order_acquire) != 1) ;
+    assert(atomic_load_explicit(&data, memory_order_relaxed) == 42);
+}
+
+int main()
+{
+    atomic_init(&flag, 0);
+    pthread_t producer_t, consumer_t;
+    pthread_create(&producer_t, NULL, producer, NULL);
+    pthread_create(&consumer_t, NULL, consumer, NULL);
+}
