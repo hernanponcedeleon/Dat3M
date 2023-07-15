1d683678fc9d43bad6b6b5fb63da35a1f0217915
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2023-07-15 16:56:39 +0200

    Remove prototype folder

--- dartagnan/src/main/java/com/dat3m/dartagnan/prototype/expr/Literal.java
+++
@@ -1,9 +0,0 @@
-package com.dat3m.dartagnan.prototype.expr;
-
-
-/*
-   A Literal is a constant that has a fixed and known value.
- */
-public interface Literal<T> extends Constant {
-   T getValue();
-}
--- dartagnan/src/main/java/com/dat3m/dartagnan/prototype/program/event/core/annotation/CodeAnnotation.java
+++
@@ -1,6 +0,0 @@
-package com.dat3m.dartagnan.prototype.program.event.core.annotation;
-
-import com.dat3m.dartagnan.prototype.program.event.AbstractEvent;
-
-public abstract class CodeAnnotation extends AbstractEvent {
-}
