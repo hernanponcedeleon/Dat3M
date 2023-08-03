a25123442ddc7d499ea82afe8f95fea8640f76c2
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2023-08-03 10:46:56 +0200

    Fix compiler warnings in ticket_awnsb_mutex

--- benchmarks/locks/ticket_awnsb_mutex.h
+++ benchmarks/locks/ticket_awnsb_mutex.h
@@ -131,7 +131,7 @@ typedef struct
     atomic_int egress;
     char padding2[8];
     int maxArrayWaiters;
-    _Atomic(struct awnsb_node_t *)* waitersArray;
+    _Atomic(awnsb_node_t *)* waitersArray;
 } ticket_awnsb_mutex_t;
 
 /*
@@ -155,7 +155,7 @@ void ticket_awnsb_mutex_init(ticket_awnsb_mutex_t * self, int maxArrayWaiters)
     atomic_init(&self->ingress, 0);
     atomic_init(&self->egress, 0);
     self->maxArrayWaiters = maxArrayWaiters;
-    self->waitersArray = (awnsb_node_t **)malloc(self->maxArrayWaiters*sizeof(awnsb_node_t *));
+    self->waitersArray = (_Atomic(awnsb_node_t *)*)malloc(self->maxArrayWaiters*sizeof(awnsb_node_t *));
     __VERIFIER_loop_bound(DEFAULT_MAX_WAITERS+1);
     for (int i = 0; i < self->maxArrayWaiters; i++) atomic_init(&self->waitersArray[i], NULL);
 }
