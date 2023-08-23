f45b74d427cda875a4c81db76db251d248ddd7fd
Thomas Haas <tomy.haas@t-online.de>
2023-08-23 17:59:17 +0200

    Added helper class ExpressionInspector to traverse expression tress without modifying them. Changed RemoveUnusedMemory to make use of ExpressionInspector.

---
+++ dartagnan/src/main/java/com/dat3m/dartagnan/expression/processing/ExpressionInspector.java
@@ -0,0 +1,72 @@
+package com.dat3m.dartagnan.expression.processing;
+
+import com.dat3m.dartagnan.expression.*;
+import com.dat3m.dartagnan.program.Function;
+import com.dat3m.dartagnan.program.Register;
+import com.dat3m.dartagnan.program.memory.Location;
+import com.dat3m.dartagnan.program.memory.MemoryObject;
+
+/*
+    This is a helper class to be used to traverse/inspect expression with RegReader.transformExpressions,
+    without changing the expressions.
+ */
+public interface ExpressionInspector extends ExpressionVisitor<Expression>{
+    default Expression visit(Atom atom) {
+        atom.getLHS().accept(this);
+        atom.getRHS().accept(this);
+        return atom;
+    }
+
+    default Expression visit(BExprBin bBin) {
+        bBin.getLHS().accept(this);
+        bBin.getRHS().accept(this);
+        return bBin;
+    }
+
+    default Expression visit(BExprUn bUn) {
+        bUn.getInner().accept(this);
+        return bUn;
+    }
+
+    default Expression visit(IExprBin iBin) {
+        iBin.getLHS().accept(this);
+        iBin.getRHS().accept(this);
+        return iBin;
+    }
+
+    default Expression visit(IExprUn iUn) {
+        iUn.getInner().accept(this);
+        return iUn;
+    }
+
+    default Expression visit(IfExpr ifExpr) {
+        ifExpr.getGuard().accept(this);
+        ifExpr.getFalseBranch().accept(this);
+        ifExpr.getTrueBranch().accept(this);
+        return ifExpr;
+    }
+
+    default Expression visit(Construction construction) {
+        construction.getArguments().forEach(arg -> arg.accept(this));
+        return construction;
+    }
+    default Expression visit(Extraction extraction) {
+        extraction.getObject().accept(this);
+        return extraction;
+    }
+
+    default Expression visit(GEPExpression getElementPointer) {
+        getElementPointer.getBaseExpression().accept(this);
+        getElementPointer.getOffsetExpressions().forEach(o -> o.accept(this));
+        return getElementPointer;
+    }
+
+    default Expression visit(BConst bConst) { return bConst;  }
+    default Expression visit(BNonDet bNonDet) { return bNonDet; }
+    default Expression visit(IValue iValue) { return iValue; }
+    default Expression visit(INonDet iNonDet) { return iNonDet; }
+    default Expression visit(Register reg) { return reg; }
+    default Expression visit(MemoryObject address) { return address; }
+    default Expression visit(Location location) { return location; }
+    default Expression visit(Function function) { return function; }
+}
--- dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/RemoveUnusedMemory.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/RemoveUnusedMemory.java
@@ -1,12 +1,9 @@
 package com.dat3m.dartagnan.program.processing;
 
-import com.dat3m.dartagnan.expression.*;
-import com.dat3m.dartagnan.expression.processing.ExpressionVisitor;
-import com.dat3m.dartagnan.program.Function;
+import com.dat3m.dartagnan.expression.Expression;
+import com.dat3m.dartagnan.expression.processing.ExpressionInspector;
 import com.dat3m.dartagnan.program.Program;
-import com.dat3m.dartagnan.program.Register;
 import com.dat3m.dartagnan.program.event.core.utils.RegReader;
-import com.dat3m.dartagnan.program.memory.Location;
 import com.dat3m.dartagnan.program.memory.Memory;
 import com.dat3m.dartagnan.program.memory.MemoryObject;
 import com.google.common.collect.Sets;
@@ -37,108 +34,14 @@ public class RemoveUnusedMemory implements ProgramProcessor {
         Sets.difference(memory.getObjects(), collector.memoryObjects).forEach(memory::deleteMemoryObject);
     }
 
-    private static class MemoryObjectCollector implements ExpressionVisitor<Expression> {
+    private static class MemoryObjectCollector implements ExpressionInspector {
 
         private final HashSet<MemoryObject> memoryObjects = new HashSet<>();
-
-        @Override
-        public Expression visit(Atom atom) {
-            atom.getLHS().accept(this);
-            atom.getRHS().accept(this);
-            return atom;
-        }
-
-        @Override
-        public Expression visit(BConst bConst) {
-            return bConst;
-        }
-
-        @Override
-        public Expression visit(BExprBin bBin) {
-            bBin.getLHS().accept(this);
-            bBin.getRHS().accept(this);
-            return bBin;
-        }
-
-        @Override
-        public Expression visit(BExprUn bUn) {
-            bUn.getInner().accept(this);
-            return bUn;
-        }
-
-        @Override
-        public Expression visit(BNonDet bNonDet) {
-            return bNonDet;
-        }
-
-        @Override
-        public Expression visit(IValue iValue) {
-            return iValue;
-        }
-
-        @Override
-        public Expression visit(IExprBin iBin) {
-            iBin.getLHS().accept(this);
-            iBin.getRHS().accept(this);
-            return iBin;
-        }
-
-        @Override
-        public Expression visit(IExprUn iUn) {
-            iUn.getInner().accept(this);
-            return iUn;
-        }
-
-        @Override
-        public Expression visit(IfExpr ifExpr) {
-            ifExpr.getGuard().accept(this);
-            ifExpr.getFalseBranch().accept(this);
-            ifExpr.getTrueBranch().accept(this);
-            return ifExpr;
-        }
-
-        @Override
-        public Expression visit(INonDet iNonDet) {
-            return iNonDet;
-        }
-
-        @Override
-        public Expression visit(Register reg) {
-            return reg;
-        }
 
         @Override
         public Expression visit(MemoryObject address) {
             memoryObjects.add(address);
             return address;
-        }
-
-        @Override
-        public Expression visit(Location location) {
-            return location;
-        }
-
-        @Override
-        public Expression visit(Function function) {
-            return function;
-        }
-
-        @Override
-        public Expression visit(Construction construction) {
-            construction.getArguments().forEach(c -> c.accept(this));
-            return construction;
-        }
-
-        @Override
-        public Expression visit(Extraction extraction) {
-            extraction.getObject().accept(this);
-            return extraction;
-        }
-
-        @Override
-        public Expression visit(GEPExpression getElementPointer) {
-            getElementPointer.getBaseExpression().accept(this);
-            return getElementPointer;
         }
     }
 }
