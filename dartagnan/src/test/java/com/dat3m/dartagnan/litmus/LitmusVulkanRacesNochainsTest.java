4bc8b7dbca45088621e4bed580868a7314e200a1
Haining <haining.tong@helsinki.fi>
2023-08-23 14:28:46 +0300

    add NOCHAINS tests

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/litmus/LitmusVulkanRacesNochainsTest.java
@@ -0,0 +1,41 @@
+package com.dat3m.dartagnan.litmus;
+
+import com.dat3m.dartagnan.configuration.Arch;
+import com.dat3m.dartagnan.configuration.Property;
+import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.utils.rules.Provider;
+import com.dat3m.dartagnan.utils.rules.Providers;
+import com.dat3m.dartagnan.wmm.Wmm;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.util.EnumSet;
+
+@RunWith(Parameterized.class)
+public class LitmusVulkanRacesNochainsTest extends AbstractLitmusTest {
+
+    @Parameterized.Parameters(name = "{index}: {0}, {1}")
+    public static Iterable<Object[]> data() throws IOException {
+        return buildLitmusTests("litmus/VULKAN/", "VULKAN", "-DR-NOCHAINS");
+    }
+
+    @Override
+    protected Provider<Arch> getTargetProvider() {
+        return () -> Arch.VULKAN;
+    }
+
+    @Override
+    protected Provider<EnumSet<Property>> getPropertyProvider() {
+        return Provider.fromSupplier(() -> EnumSet.of(Property.CAT_SPEC));
+    }
+
+    public LitmusVulkanRacesNochainsTest(String path, Result expected) {
+        super(path, expected);
+    }
+
+    @Override
+    protected Provider<Wmm> getWmmProvider() {
+        return Providers.createWmmFromName(() -> "spirv-nochains");
+    }
+}
