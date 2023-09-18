c692f98a9923f411891fa0f336f7d0ecb293c8b5
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2023-09-18 20:23:17 +0200

    Partially restore FenceBase implementation
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/event/common/FenceBase.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/common/FenceBase.java
@@ -1,36 +1,37 @@
 package com.dat3m.dartagnan.program.event.common;
 
 import com.dat3m.dartagnan.program.event.Tag;
-import com.dat3m.dartagnan.program.event.core.Fence;
+import com.dat3m.dartagnan.program.event.core.AbstractEvent;
 import com.google.common.base.Preconditions;
 
 @NoInterface
-public abstract class FenceBase extends Fence {
+public abstract class FenceBase extends AbstractEvent {
 
+    protected final String name;
     protected final String mo; // For fences that do not support a mo, use Fence instead.
 
     public FenceBase(String name, String mo) {
-        super(name);
-        Preconditions.checkNotNull(mo);
         Preconditions.checkArgument(!mo.isEmpty());
+        this.name = name;
         this.mo = mo;
-        addTags(mo);
+        addTags(Tag.VISIBLE, Tag.FENCE, mo);
     }
 
     protected FenceBase(FenceBase other) {
         super(other);
+        this.name = other.name;
         this.mo = other.mo;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getMo() { return mo; }
 
     @Override
     public String defaultString() {
-        if (mo == null || mo.isEmpty()) {
-            return name;
-        } else {
-            return String.format("%s(%s)", name, mo);
-        }
+        return String.format("%s(%s)", name, mo);
     }
 
 }
