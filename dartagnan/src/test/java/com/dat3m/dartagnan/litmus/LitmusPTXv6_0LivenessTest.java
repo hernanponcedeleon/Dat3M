a9bb5816ee62296957944b9f1ad84ea6aa59df8e
Haining <haining.tong@helsinki.fi>
2023-10-24 15:27:30 +0300

    add ptx6_0 liveness tests

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/litmus/LitmusPTXv6_0LivenessTest.java
@@ -0,0 +1,41 @@
+package com.dat3m.dartagnan.litmus;
+
+import com.dat3m.dartagnan.configuration.Arch;
+import com.dat3m.dartagnan.configuration.Property;
+import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.utils.rules.Provider;
+import com.dat3m.dartagnan.utils.rules.Providers;
+import com.dat3m.dartagnan.wmm.Wmm;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.util.EnumSet;
+
+@RunWith(Parameterized.class)
+public class LitmusPTXv6_0LivenessTest extends AbstractLitmusTest {
+
+    public LitmusPTXv6_0LivenessTest(String path, Result expected) {
+        super(path, expected);
+    }
+
+    @Parameterized.Parameters(name = "{index}: {0}, {1}")
+    public static Iterable<Object[]> data() throws IOException {
+        return buildLitmusTests("litmus/PTX/", "PTXv6_0-Liveness");
+    }
+
+    @Override
+    protected Provider<Arch> getTargetProvider() {
+        return () -> Arch.PTX;
+    }
+
+    @Override
+    protected Provider<EnumSet<Property>> getPropertyProvider() {
+        return Provider.fromSupplier(() -> EnumSet.of(Property.LIVENESS));
+    }
+
+    @Override
+    protected Provider<Wmm> getWmmProvider() {
+        return Providers.createWmmFromName(() -> "ptx-v6.0");
+    }
+}
