c964d0b37ab75ae409060615f625ff03503d3848
René Pascal Maseli <r.maseli@tu-bs.de>
2023-10-26 19:29:52 +0100

    Add TypeFactory.getPrimitiveFields(Type)
    Add .getPrimitiveFields(Type,int)
    Add .getByteOffset(Type,List)
    Add memory size to BooleanType

--- dartagnan/src/main/java/com/dat3m/dartagnan/expression/type/TypeFactory.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/expression/type/TypeFactory.java
@@ -4,7 +4,9 @@ import com.dat3m.dartagnan.utils.Normalizer;
 import com.google.common.math.IntMath;
 
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -82,6 +84,117 @@ public final class TypeFactory {
         return getIntegerType(8);
     }
 
+    public record PrimitiveFieldInfo(Type type, List<Integer> elementOffsets) {}
+
+    public List<Type> getPrimitiveFields(Type type) {
+        if (type instanceof ArrayType array) {
+            final List<Type> result = new ArrayList<>();
+            final List<Type> elementFields = getPrimitiveFields(array.getElementType());
+            for (int i = 0; i < array.getNumElements(); i++) {
+                result.addAll(elementFields);
+            }
+            return result;
+        }
+        if (type instanceof AggregateType struct) {
+            final List<Type> result = new ArrayList<>();
+            for (final Type field : struct.getDirectFields()) {
+                result.addAll(getPrimitiveFields(field));
+            }
+            return result;
+        }
+        return List.of(type);
+    }
+
+    /**
+     * @param type Any type made from this.
+     * @param offset Non-negative number of bytes the field is supposed to have.
+     * @return Description of the most primitive field inside the boundaries of {@code type},
+     * if it exists such that it is offset by {@code offset} bytes.
+     */
+    public Optional<PrimitiveFieldInfo> getPrimitiveField(Type type, int offset) {
+        checkArgument(offset >= 0, "Field offset out of bounds");
+        final int elementBytes = getMemorySizeInBytes(type);
+        final List<Integer> elementPointer = new ArrayList<>();
+        elementPointer.add(offset / elementBytes);
+        Type currentType = type;
+        int remainder = offset % elementBytes;
+        while (currentType instanceof ArrayType || currentType instanceof AggregateType) {
+            if (currentType instanceof ArrayType array) {
+                final int bytes = getMemorySizeInBytes(array.getElementType());
+                currentType = array.getElementType();
+                elementPointer.add(remainder / bytes);
+                remainder = remainder % bytes;
+                continue;
+            }
+            int currentOffset = 0;
+            for (final Type field : ((AggregateType) currentType).getDirectFields()) {
+                final int bytes = getMemorySizeInBytes(field);
+                if (remainder < bytes) {
+                    currentType = field;
+                    break;
+                }
+                remainder -= bytes;
+                currentOffset++;
+            }
+            elementPointer.add(currentOffset);
+        }
+        return remainder == 0 ? Optional.of(new PrimitiveFieldInfo(currentType, elementPointer)) : Optional.empty();
+    }
+
+    /**
+     * @param type Element type of the figurative array serving as a base.
+     * @param offsets Non-empty list of type-dependent offsets, as given in a GEP instruction, starting with the index.
+     */
+    public int getByteOffset(Type type, List<? extends Number> offsets) {
+        checkArgument(!offsets.isEmpty(), "Missing offsets");
+        int result = getMemorySizeInBytes(type) * offsets.get(0).intValue();
+        Type currentType = type;
+        for (final Number offset : offsets.subList(1, offsets.size())) {
+            if (currentType instanceof ArrayType array) {
+                currentType = array.getElementType();
+                result += offset.intValue() * getMemorySizeInBytes(currentType);
+                continue;
+            }
+            checkArgument(currentType instanceof AggregateType,
+                    "Unexpected primitive type in %s with offsets %s", type, offsets);
+            final List<Type> fields = ((AggregateType) currentType).getDirectFields();
+            final int index = offset.intValue();
+            checkArgument(index >= 0 && index < fields.size(),
+                    "Aggregate index out of bounds in %s with offsets %s", type, offsets);
+            currentType = fields.get(index);
+            for (final Type field : fields.subList(0, index)) {
+                result += getMemorySizeInBytes(field);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param type Datatype of the object pointed to.
+     * @param constants Translated from a GEP expression, where non-constant expressions should be translated to 1.
+     *                  Must be non-empty: type is treated as the element type of an array.
+     * @return Numbers of bytes per constant.
+     */
+    public List<Integer> getByteCounts(Type type, List<Integer> constants) {
+        checkArgument(!constants.isEmpty(), "Missing element pointer");
+        final List<Integer> result = new ArrayList<>(constants.size());
+        result.add(getMemorySizeInBytes(type) * constants.get(0));
+        Type currentType = type;
+        for (final Integer constant : constants.subList(1, constants.size())) {
+            checkArgument(currentType instanceof ArrayType || currentType instanceof AggregateType,
+                    "Insufficient container type %s in GEP %s", type, constants);
+            if (currentType instanceof ArrayType array) {
+                currentType = array.getElementType();
+                result.add(constant * getMemorySizeInBytes(currentType));
+            } else {
+                final List<Type> fields = ((AggregateType) currentType).getDirectFields();
+                result.add(constant * fields.subList(0, constant).stream().mapToInt(this::getMemorySizeInBytes).sum());
+                currentType = fields.get(constant);
+            }
+        }
+        return result;
+    }
+
     public int getMemorySizeInBytes(Type type) {
         final int sizeInBytes;
         if (type instanceof ArrayType arrayType) {
@@ -107,6 +220,8 @@ public final class TypeFactory {
             } else {
                 sizeInBytes = IntMath.divide(integerType.getBitWidth(), 8, RoundingMode.CEILING);
             }
+        } else if (type instanceof BooleanType) {
+            sizeInBytes = 1;
         } else {
             throw new UnsupportedOperationException("Cannot compute the size of " + type);
         }
