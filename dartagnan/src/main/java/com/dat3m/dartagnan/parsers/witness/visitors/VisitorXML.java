016e43732a93e0e1e0347fddaf8a0099ab155d77
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2024-01-02 22:15:35 +0100

    Cleanup VisitorXML
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/witness/visitors/VisitorXML.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/witness/visitors/VisitorXML.java
@@ -5,6 +5,7 @@ import com.dat3m.dartagnan.parsers.XMLParser;
 import com.dat3m.dartagnan.parsers.XMLParser.ElementContext;
 import com.dat3m.dartagnan.parsers.XMLParserBaseVisitor;
 import com.dat3m.dartagnan.witness.Edge;
+import com.dat3m.dartagnan.witness.ElemWithAttributes;
 import com.dat3m.dartagnan.witness.Node;
 import com.dat3m.dartagnan.witness.WitnessGraph;
 
@@ -16,7 +17,7 @@ import java.util.stream.Collectors;
 public class VisitorXML extends XMLParserBaseVisitor<Object> {
 
     private final WitnessGraph graph = new WitnessGraph();
-    private String lastAddedNodeId;
+    private ElemWithAttributes current = graph;
 
     @Override
     public WitnessGraph visitDocument(XMLParser.DocumentContext ctx) {
@@ -29,52 +30,38 @@ public class VisitorXML extends XMLParserBaseVisitor<Object> {
 
     @Override
     public Object visitElement(XMLParser.ElementContext ctx) {
-        if (ctx.content() != null) {
-            if (ctx.Name(0).getText().equals("data")) {
-                if (ctx.content() != null) {
-                    String key = ctx.attribute(0).STRING().getText();
-                    key = key.substring(1, key.length() - 1);
-                    String value = ctx.content().getText();
-                    if (key.equals(PROGRAMFILE.toString()) || key.equals(PRODUCER.toString())
-                            || key.equals(UNROLLBOUND.toString())) {
-                        graph.addAttribute(key, value);
-                    }
-                    if (key.equals(WITNESSTYPE.toString()) && !value.equals("violation_witness")) {
-                        throw new ParsingException("Dartagnan can only validate violation witnesses");
-                    }
-                    if (key.equals(ENTRY.toString()) || key.equals(VIOLATION.toString())) {
-                        graph.getNode(lastAddedNodeId).addAttribute(key, value);
-                    }
-                }
+        if (ctx.Name(0).getText().equals("data")) {
+            String key = ctx.attribute(0).STRING().getText();
+            key = key.substring(1, key.length() - 1);
+            String value = ctx.content().getText();
+            if (key.equals(WITNESSTYPE.toString()) && !value.equals("violation_witness")) {
+                throw new ParsingException("Dartagnan can only validate violation witnesses");
             }
-            if (ctx.Name(0).getText().equals("node")) {
-                String name = ctx.attribute(0).STRING().toString();
-                lastAddedNodeId = name.substring(1, name.length() - 1);
-                graph.addNode(lastAddedNodeId);
-            }
-            if (ctx.Name(0).getText().equals("edge")) {
-                int idx = ctx.attribute().stream().map(a -> a.Name().toString()).collect(Collectors.toList())
-                        .indexOf("source");
-                String name = ctx.attribute(idx).STRING().toString();
-                name = name.substring(1, name.length() - 1);
-                Node v0 = graph.hasNode(name) ? graph.getNode(name) : new Node(name);
-                idx = ctx.attribute().stream().map(a -> a.Name().toString()).collect(Collectors.toList())
-                        .indexOf("target");
-                name = ctx.attribute(idx).STRING().toString();
-                name = name.substring(1, name.length() - 1);
-                Node v1 = graph.hasNode(name) ? graph.getNode(name) : new Node(name);
-                Edge edge = new Edge(v0, v1);
-                if (ctx.content() != null) {
-                    for (ElementContext elem : ctx.content().element()) {
-                        String key = elem.attribute(0).STRING().getText();
-                        key = key.substring(1, key.length() - 1);
-                        String value = elem.content().getText();
-                        edge.addAttribute(key, value);
-                    }
-                }
-                graph.addEdge(edge);
-            }
+            current.addAttribute(key, value);
+            return null;
+        } else if (ctx.Name(0).getText().equals("node")) {
+            String name = ctx.attribute(0).STRING().toString();
+            name = name.substring(1, name.length() - 1);
+            graph.addNode(name);
+            current = graph.getNode(name);
+            visitChildren(ctx);
+            current = graph;
+            return null;
+        } else if (ctx.Name(0).getText().equals("edge")) {
+            Edge edge = new Edge(getNode(ctx, "source"), getNode(ctx, "target"));
+            graph.addEdge(edge);
+            current = edge;
+            visitChildren(ctx);
+            current = graph;
+            return null;
         }
         return visitChildren(ctx);
+    }
+
+    private Node getNode(ElementContext ctx, String vertex) {
+        int idx = ctx.attribute().stream().map(a -> a.Name().toString()).collect(Collectors.toList()).indexOf(vertex);
+        String name = ctx.attribute(idx).STRING().toString();
+        name = name.substring(1, name.length() - 1);
+        return graph.hasNode(name) ? graph.getNode(name) : new Node(name);
     }
 }
