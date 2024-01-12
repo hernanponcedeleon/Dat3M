fb2c60ece9a58dbf8b679a7626961e97c600ddfc
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2024-01-12 14:25:07 +0100

    Add dot to supportedFormat entry
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/Dartagnan.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/Dartagnan.java
@@ -63,7 +63,7 @@ public class Dartagnan extends BaseOptions {
     private static final Logger logger = LogManager.getLogger(Dartagnan.class);
 
     private static final Set<String> supportedFormats =
-            ImmutableSet.copyOf(Arrays.asList(".litmus", ".c", ".i", "ll"));
+            ImmutableSet.copyOf(Arrays.asList(".litmus", ".c", ".i", ".ll"));
 
     private Dartagnan(Configuration config) throws InvalidConfigurationException {
         config.recursiveInject(this);
