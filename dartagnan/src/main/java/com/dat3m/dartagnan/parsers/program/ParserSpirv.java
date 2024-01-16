968b5c216228298a697b86f0c7a59a00a19c5533
Natalia Gavrilenko <natalia.gavrilenko@huawei.com>
2024-01-16 13:54:59 +0100

    Make parsers fail on error in lexer

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/ParserSpirv.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/ParserSpirv.java
@@ -1,24 +1,23 @@
 package com.dat3m.dartagnan.parsers.program;
 
-import com.dat3m.dartagnan.exception.ParserErrorListener;
+import com.dat3m.dartagnan.exception.AbortErrorListener;
 import com.dat3m.dartagnan.parsers.*;
 import com.dat3m.dartagnan.parsers.program.visitors.VisitorSpirv;
 import com.dat3m.dartagnan.program.Program;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.DiagnosticErrorListener;
-import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.*;
 
 public class ParserSpirv implements ParserInterface {
 
     @Override
     public Program parse(CharStream charStream) {
         SpirvLexer lexer = new SpirvLexer(charStream);
+        lexer.addErrorListener(new AbortErrorListener());
+        lexer.addErrorListener(new DiagnosticErrorListener(true));
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
 
         SpirvParser parser = new SpirvParser(tokenStream);
+        parser.addErrorListener(new AbortErrorListener());
         parser.addErrorListener(new DiagnosticErrorListener(true));
-        parser.addErrorListener(new ParserErrorListener());
         ParserRuleContext parserEntryPoint = parser.spv();
         VisitorSpirv visitor = new VisitorSpirv();
 
