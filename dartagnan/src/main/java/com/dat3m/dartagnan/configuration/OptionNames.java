dc3456f1a2cf00d36d87d0933778ed5718501eba
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2024-01-26 10:52:06 +0100

    Trivial implementation of flattening pass
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/configuration/OptionNames.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/configuration/OptionNames.java
@@ -35,6 +35,7 @@ public class OptionNames {
     public static final String DETERMINISTIC_REORDERING = "program.processing.detReordering";
     public static final String REDUCE_SYMMETRY = "program.processing.reduceSymmetry";
     public static final String CONSTANT_PROPAGATION = "program.processing.constantPropagation";
+    public static final String FLATTENNING = "program.processing.flattening";
     public static final String DEAD_ASSIGNMENT_ELIMINATION = "program.processing.dce";
     public static final String DYNAMIC_PURE_LOOP_CUTTING = "program.processing.dplc";
     public static final String PROPAGATE_COPY_ASSIGNMENTS = "program.processing.propagateCopyAssignments";
