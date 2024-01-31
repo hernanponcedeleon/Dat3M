d965682258e8ae55102e3a5837cd2f4efaf328e3
Thomas Haas <tomy.haas@t-online.de>
2024-01-31 23:57:17 +0100

    Fixed encoding wrong type size

--- dartagnan/src/main/java/com/dat3m/dartagnan/encoding/EncodingContext.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/encoding/EncodingContext.java
@@ -219,7 +219,7 @@ public final class EncodingContext {
         //TODO match this with the actual type stored at the memory address
         // (we do not know and guess the arch type right now)
         TypeFactory types = TypeFactory.getInstance();
-        int archSize = types.getMemorySizeInBytes(types.getArchType());
+        int archSize = types.getMemorySizeInBytes(types.getArchType()) * 8;
         return formulaManager.getBitvectorFormulaManager().makeVariable(archSize, name);
     }
 
