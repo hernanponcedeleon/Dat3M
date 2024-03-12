134f8f945b4e420f20c52e3b2affec0b78329689
Thomas Haas <tomy.haas@t-online.de>
2024-03-12 14:55:00 +0100

    - Improved the ugly RA fix (issue#523): Still ugly but now hopefully correct

--- dartagnan/src/main/java/com/dat3m/dartagnan/wmm/definition/Difference.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/wmm/definition/Difference.java
@@ -26,6 +26,8 @@ public class Difference extends Definition {
         return List.of(definedRelation, minuend, subtrahend);
     }
 
+    public Relation getSubtrahend() { return complement; }
+
     @Override
     public <T> T accept(Visitor<? extends T> v) {
         return v.visitDifference(this);
