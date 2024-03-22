03d4d40d7a02480792fd7f303d62581851a3921e
Thomas Haas <tomy.haas@t-online.de>
2024-03-22 10:27:39 +0100

    Fixed uninitRead.c benchmark and added clarifying comment

--- benchmarks/c/miscellaneous/uninitRead.c
+++ benchmarks/c/miscellaneous/uninitRead.c
@@ -5,6 +5,7 @@
     This test shows that we can read from uninitialized memory.
     EXPECTED: FAIL
     PREVIOUSLY: We returned PASS because the array read had no possible rf-edge and thus the program had no valid executions.
+    WARNING: We still cannot correctly handle overflowing accesses to initialized(!) memory.
 */
 
 #define SIZE 10
@@ -12,6 +13,6 @@ volatile int array[SIZE];
 
 int main()
 {
-    assert (array[SIZE] == 42);
+    assert (array[SIZE + 100000] == 42);
     return 0;
 }
