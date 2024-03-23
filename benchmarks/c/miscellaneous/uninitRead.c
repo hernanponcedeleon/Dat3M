543ed768909bc0d3bdcb7cebc2fb59eda6d66336
Thomas Haas <tomy.haas@t-online.de>
2024-03-23 11:21:29 +0100

    Changed uninitRead.c to read from malloc'd but uninitialized memory.

--- benchmarks/c/miscellaneous/uninitRead.c
+++ benchmarks/c/miscellaneous/uninitRead.c
@@ -4,15 +4,14 @@
 /*
     This test shows that we can read from uninitialized memory.
     EXPECTED: FAIL
-    PREVIOUSLY: We returned PASS because the array read had no possible rf-edge and thus the program had no valid executions.
-    WARNING: We still cannot correctly handle overflowing accesses to initialized(!) memory.
 */
 
 #define SIZE 10
-volatile int array[SIZE];
+volatile int *array;
 
 int main()
 {
-    assert (array[SIZE + 100000] == 42);
+    array = (int*)malloc(SIZE * sizeof(int));
+    assert (array[0] == 42);
     return 0;
 }
