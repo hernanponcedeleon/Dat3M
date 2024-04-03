18c097c57b03adc6ac922240d4aed780de3e1b7f
Natalia Gavrilenko <natalia.gavrilenko@huawei.com>
2024-04-04 00:25:06 +0200

    Moved integer incoding config to spirv tests

--- dartagnan/src/main/java/com/dat3m/dartagnan/encoding/EncodingContext.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/encoding/EncodingContext.java
@@ -69,7 +69,7 @@ public final class EncodingContext {
     @Option(name = USE_INTEGERS,
             description = "Data is encoded with mathematical integers instead of bitvectors.  Default: false.",
             secure = true)
-    boolean useIntegers = true;
+    boolean useIntegers = false;
 
     private final Map<Event, BooleanFormula> controlFlowVariables = new HashMap<>();
     private final Map<Event, BooleanFormula> executionVariables = new HashMap<>();
