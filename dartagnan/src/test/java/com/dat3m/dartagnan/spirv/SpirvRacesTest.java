66c6a143caac340e86f835a41e631cc379989d3a
Natalia Gavrilenko <natalia.gavrilenko@huawei.com>
2024-04-08 16:54:04 +0200

    Tests cleanup

--- dartagnan/src/test/java/com/dat3m/dartagnan/spirv/SpirvRacesTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/spirv/SpirvRacesTest.java
@@ -72,21 +72,16 @@ public class SpirvRacesTest {
                 // Unsupported decoration 'WorkgroupId'
                 // {"xf-barrier.spv.dis", 1, PASS},
                 // {"xf-barrier-opt.spv.dis", 1, PASS},
+
+                // TODO: Support missing semantics
                 // {"gpu-verify/alignement/race_location.spv.dis", 1, FAIL},
-
-                // {"gpu-verify/atomics/atomic_read_race.spv.dis", 1, FAIL},
-                // {"gpu-verify/atomics/counter.spv.dis", 1, PASS},
-                // {"gpu-verify/atomics/definitions_atom_int.spv.dis", 1, PASS},
-                // {"gpu-verify/atomics/displaced.spv.dis", 1, FAIL},
-                // {"gpu-verify/atomics/forloop.spv.dis", 1, FAIL},
-                // {"gpu-verify/atomics/histo.spv.dis", 1, PASS},
-
                 {"gpu-verify/atomics/atomic_read_race.spv.dis", 1, FAIL},
                 // TODO: Check me
                 // {"gpu-verify/atomics/counter.spv.dis", 1, PASS},
                 // {"gpu-verify/atomics/definitions_atom_int.spv.dis", 1, PASS},
                 // {"gpu-verify/atomics/displaced.spv.dis", 1, FAIL},
                 {"gpu-verify/atomics/forloop.spv.dis", 1, FAIL},
+                {"gpu-verify/atomics/histo.spv.dis", 1, PASS},
                 // {"gpu-verify/atomics/pointers.spv.dis", 1, FAIL},
 
                 {"gpu-verify/barrier_intervals/test1.spv.dis", 1, PASS},
@@ -94,15 +89,12 @@ public class SpirvRacesTest {
                 //{"gpu-verify/barrier_intervals/test2.spv.dis", 1, FAIL},
                 {"gpu-verify/barrier_intervals/test3.spv.dis", 2, PASS},
                 {"gpu-verify/barrier_intervals/test4.spv.dis", 2, PASS},
+
+                // TODO: Support missing semantics
                 {"gpu-verify/beningn_race_tests/fail/writeafterread_addition.spv.dis", 1, FAIL},
                 {"gpu-verify/beningn_race_tests/fail/writeafterread_otherval.spv.dis", 1, FAIL},
                 // {"gpu-verify/beningn_race_tests/fail/writetiddiv64_offbyone.spv.dis", 1, FAIL},
                 {"gpu-verify/beningn_race_tests/fail/writezero_nobening.spv.dis", 1, FAIL},
-                {"gpu-verify/globalarray-fail.spv.dis", 1, FAIL},
-                {"gpu-verify/globalarray-pass.spv.dis", 1, PASS},
-                {"gpu-verify/globalarray-pass2.spv.dis", 1, PASS},
-
-                // TODO: Support missing semantics
                 // {"gpu-verify/inter_group_and_barrier_flag_tests/fail/bad_read_then_write.spv.dis", 1, FAIL},
                 // {"gpu-verify/inter_group_and_barrier_flag_tests/fail/bad_write_then_read.spv.dis", 1, FAIL},
                 // {"gpu-verify/inter_group_and_barrier_flag_tests/fail/local_id.spv.dis", 1, FAIL},
@@ -110,18 +102,19 @@ public class SpirvRacesTest {
                 {"gpu-verify/inter_group_and_barrier_flag_tests/fail/missing_local_barrier_flag.spv.dis", 1, FAIL},
                 {"gpu-verify/inter_group_and_barrier_flag_tests/fail/no_barrier_flag.spv.dis", 1, FAIL},
                 {"gpu-verify/inter_group_and_barrier_flag_tests/fail/sync.spv.dis", 1, FAIL},
-                // TODO: Support missing semantics
-                // {"gpu-verify/inter_group_and_barrier_flag_tests/fail/sync_within_group_wong_flag.spv.dis", 1, PASS},
+                // {"gpu-verify/inter_group_and_barrier_flag_tests/fail/sync_within_group_wrong_flag.spv.dis", 1, FAIL},
+
                 // {"gpu-verify/inter_group_and_barrier_flag_tests/pass/global_barrier.spv.dis", 1, PASS},
-                
-                // Check why FAIL 
+                // TODO: Check why FAIL
                 // {"gpu-verify/inter_group_and_barrier_flag_tests/pass/local_barrier_flag.spv.dis", 1, PASS},
                 // {"gpu-verify/inter_group_and_barrier_flag_tests/pass/local_id_benign_write_write.spv.dis", 1, PASS},
-               
-                // TODO: Support missing semantics
+
                 // {"gpu-verify/inter_group_and_barrier_flag_tests/pass/read_then_write.spv.dis", 1, PASS},
                 // {"gpu-verify/inter_group_and_barrier_flag_tests/pass/sync_within_group.spv.dis", 1, PASS},
                 // {"gpu-verify/inter_group_and_barrier_flag_tests/pass/write_then_read.spv.dis", 1, PASS},
+                {"gpu-verify/globalarray-fail.spv.dis", 1, FAIL},
+                {"gpu-verify/globalarray-pass.spv.dis", 1, PASS},
+                {"gpu-verify/globalarray-pass2.spv.dis", 1, PASS},
 
         });
     }
