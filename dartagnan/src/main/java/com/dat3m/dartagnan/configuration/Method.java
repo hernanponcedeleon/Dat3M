1a1e96a319b9d61770b6bed2d9e9a5d6f7e53588
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2024-04-20 21:46:40 +0200

    Formatting
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/configuration/Method.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/configuration/Method.java
@@ -3,41 +3,41 @@ package com.dat3m.dartagnan.configuration;
 import java.util.Arrays;
 
 public enum Method implements OptionInterface {
-	EAGER, LAZY;
-	
-	// Used for options in the console
-	@Override
-	public String asStringOption() {
-        switch(this) {
-        	case EAGER:
-        		return "eager";
-			case LAZY:
-				return "lazy";
-        }
-        throw new UnsupportedOperationException("Unrecognized analysis " + this);
-	}
+    EAGER, LAZY;
 
-	// Used to display in UI
-	@Override
-	public String toString() {
-        switch(this) {
-			case EAGER:
-            	return "Eager analysis";
+    // Used for options in the console
+    @Override
+    public String asStringOption() {
+        switch (this) {
+            case EAGER:
+                return "eager";
             case LAZY:
-            	return "Lazy analysis";
+                return "lazy";
         }
         throw new UnsupportedOperationException("Unrecognized analysis " + this);
-	}
+    }
 
-	public static Method getDefault() {
-		return LAZY;
-	}
-	
-	// Used to decide the order shown by the selector in the UI
-	public static Method[] orderedValues() {
-		Method[] order = { LAZY, EAGER};
-		// Be sure no element is missing
-		assert(Arrays.asList(order).containsAll(Arrays.asList(values())));
-		return order;
-	}
+    // Used to display in UI
+    @Override
+    public String toString() {
+        switch (this) {
+            case EAGER:
+                return "Eager analysis";
+            case LAZY:
+                return "Lazy analysis";
+        }
+        throw new UnsupportedOperationException("Unrecognized analysis " + this);
+    }
+
+    public static Method getDefault() {
+        return LAZY;
+    }
+
+    // Used to decide the order shown by the selector in the UI
+    public static Method[] orderedValues() {
+        Method[] order = { LAZY, EAGER };
+        // Be sure no element is missing
+        assert (Arrays.asList(order).containsAll(Arrays.asList(values())));
+        return order;
+    }
 }
