0a6fc1f73a6c8dc97fcd7b8d272677ab214130f0
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2024-04-30 22:40:42 +0200

    Add test class for tables and updated benchmarks
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/spirv/SpirvAssertionsTestPaper.java
@@ -0,0 +1,113 @@
+package com.dat3m.dartagnan.spirv;
+
+import com.dat3m.dartagnan.configuration.Arch;
+import com.dat3m.dartagnan.parsers.cat.ParserCat;
+import com.dat3m.dartagnan.parsers.program.ProgramParser;
+import com.dat3m.dartagnan.program.Program;
+import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.verification.VerificationTask;
+import com.dat3m.dartagnan.verification.solving.AssumeSolver;
+import com.dat3m.dartagnan.verification.solving.IncrementalSolver;
+import com.dat3m.dartagnan.verification.solving.RefinementSolver;
+import com.dat3m.dartagnan.verification.solving.TwoSolvers;
+import com.dat3m.dartagnan.wmm.Wmm;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.sosy_lab.common.ShutdownManager;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.log.BasicLogManager;
+import org.sosy_lab.java_smt.SolverContextFactory;
+import org.sosy_lab.java_smt.api.ProverEnvironment;
+import org.sosy_lab.java_smt.api.SolverContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.EnumSet;
+
+import static com.dat3m.dartagnan.configuration.OptionNames.USE_INTEGERS;
+import static com.dat3m.dartagnan.configuration.Property.*;
+import static com.dat3m.dartagnan.utils.ResourceHelper.getRootPath;
+import static com.dat3m.dartagnan.utils.ResourceHelper.getTestResourcePath;
+import static com.dat3m.dartagnan.utils.Result.*;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class SpirvAssertionsTestPaper {
+
+    private final String modelPath = getRootPath("cat/spirv.cat");
+    private final String programPath;
+    private final int bound;
+    private final Result expected;
+
+    public SpirvAssertionsTestPaper(String file, int bound, Result expected) {
+        this.programPath = getTestResourcePath("spirv/benchmarks/" + file);
+        this.bound = bound;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters(name = "{index}: {0}, {1}, {2}")
+    public static Iterable<Object[]> data() throws IOException {
+        return Arrays.asList(new Object[][]{
+                {"caslock-2.3.spv.dis", 2, PASS},
+                {"caslock-acq2rx-4.2.spv.dis", 1, FAIL},
+                {"caslock-rel2rx-4.2.spv.dis", 1, FAIL},
+                {"caslock-dv2wg-4.1.spv.dis", 2, PASS},
+                {"caslock-dv2wg-4.2.spv.dis", 1, FAIL},
+
+                {"ticketlock-2.3.spv.dis", 1, PASS},
+                {"ticketlock-acq2rx-4.2.spv.dis", 1, FAIL},
+                {"ticketlock-rel2rx-4.2.spv.dis", 1, FAIL},
+                {"ticketlock-dv2wg-4.1.spv.dis", 2, PASS},
+                {"ticketlock-dv2wg-4.2.spv.dis", 1, FAIL},
+
+                {"ttaslock-2.2.spv.dis", 4, PASS},
+                {"ttaslock-acq2rx-4.2.spv.dis", 1, FAIL},
+                {"ttaslock-rel2rx-4.2.spv.dis", 1, FAIL},
+                {"ttaslock-dv2wg-4.1.spv.dis", 4, PASS},
+                {"ttaslock-dv2wg-4.2.spv.dis", 1, FAIL},
+
+                {"xf-barrier-3.3.spv.dis", 9, PASS},
+                {"xf-barrier-acq2rx-1.spv.dis", 4, FAIL},
+                {"xf-barrier-acq2rx-2.spv.dis", 4, FAIL},
+                {"xf-barrier-rel2rx-1.spv.dis", 4, FAIL},
+                {"xf-barrier-rel2rx-2.spv.dis", 4, FAIL},
+
+        });
+    }
+
+    @Test
+    public void testAllSolvers() throws Exception {
+        try (SolverContext ctx = mkCtx(); ProverEnvironment prover = mkProver(ctx)) {
+            assertEquals(expected, AssumeSolver.run(ctx, prover, mkTask()).getResult());
+        }
+    }
+
+    private SolverContext mkCtx() throws InvalidConfigurationException {
+        Configuration cfg = Configuration.builder().build();
+        return SolverContextFactory.createSolverContext(
+                cfg,
+                BasicLogManager.create(cfg),
+                ShutdownManager.create().getNotifier(),
+                SolverContextFactory.Solvers.Z3);
+    }
+
+    private ProverEnvironment mkProver(SolverContext ctx) {
+        return ctx.newProverEnvironment(SolverContext.ProverOptions.GENERATE_MODELS);
+    }
+
+    private VerificationTask mkTask() throws Exception {
+        Configuration config = Configuration.builder()
+                .setOption(USE_INTEGERS, "true")
+                .build();
+        VerificationTask.VerificationTaskBuilder builder = VerificationTask.builder()
+                .withConfig(config)
+                .withBound(bound)
+                .withTarget(Arch.VULKAN);
+        Program program = new ProgramParser().parse(new File(programPath));
+        Wmm mcm = new ParserCat().parse(new File(modelPath));
+        return builder.build(program, mcm, EnumSet.of(PROGRAM_SPEC, CAT_SPEC, LIVENESS));
+    }
+}
