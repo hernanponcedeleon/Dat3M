97544490572253867497d4eaa24cbabb734a570f
Thomas Haas <tomy.haas@t-online.de>
2024-05-28 15:05:49 +0200

    Formatting of UI code. Deleted unused UI class.

--- ui/src/main/java/com/dat3m/ui/options/utils/Helper.java
+++ ui/src/main/java/com/dat3m/ui/options/utils/Helper.java
@@ -1,16 +1,16 @@
 package com.dat3m.ui.options.utils;
 
+import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
-import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
-
 public class Helper {
 
-	// Used to decide the solvers order shown by the selector in the UI
-	public static Solvers[] solversOrderedValues() {
-		return Arrays.stream(Solvers.values())
-		.sorted(Comparator.comparing(Solvers::toString))
-		.toArray(Solvers[]::new);
-	}
+    // Used to decide the solvers order shown by the selector in the UI
+    public static Solvers[] solversOrderedValues() {
+        return Arrays.stream(Solvers.values())
+                .sorted(Comparator.comparing(Solvers::toString))
+                .toArray(Solvers[]::new);
+    }
 }
