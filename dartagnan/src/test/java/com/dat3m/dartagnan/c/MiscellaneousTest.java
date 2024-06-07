d962b73e6a64c1b265649c7be620a52d6ac6a070
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2024-06-07 16:22:56 +0200

    Add unit tests
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/test/java/com/dat3m/dartagnan/c/MiscellaneousTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/c/MiscellaneousTest.java
@@ -49,7 +49,7 @@ public class MiscellaneousTest extends AbstractCTest {
     protected Provider<Configuration> getConfigurationProvider() {
         return Provider.fromSupplier(() -> {
             ConfigurationBuilder builder = Configuration.builder();
-            if (!name.equals("pthread")) {
+            if (!name.equals("pthread") && !name.equals("ctlz") && !name.equals("cttz")) {
                 builder.setOption(OptionNames.USE_INTEGERS, "true");
             }
             if (name.equals("recursion")) {
@@ -87,7 +87,9 @@ public class MiscellaneousTest extends AbstractCTest {
                 {"uninitRead", IMM, FAIL, 1},
                 {"multipleBackJumps", IMM, UNKNOWN, 1},
                 {"memcpy_s", IMM, PASS, 1},
-                {"offsetof", IMM, PASS, 1}
+                {"offsetof", IMM, PASS, 1},
+                {"ctlz", IMM, PASS, 1},
+                {"cttz", IMM, PASS, 1}
         });
     }
 
