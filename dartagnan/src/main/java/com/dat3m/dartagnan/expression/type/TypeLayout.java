2751324c41e37e483de17c21dd25b19410aa991b
Thomas Haas <t.haas@tu-bs.de>
2024-06-12 19:58:15 +0200

    Fix size and offset computations of types (#691)

---
+++ dartagnan/src/main/java/com/dat3m/dartagnan/expression/type/TypeLayout.java
@@ -0,0 +1,59 @@
+package com.dat3m.dartagnan.expression.type;
+
+import com.dat3m.dartagnan.expression.Type;
+import com.google.common.math.IntMath;
+
+import java.math.RoundingMode;
+
+public record TypeLayout(int unpaddedSize, int alignment) {
+
+    public int totalSizeInBytes() { return paddedSize(unpaddedSize, alignment); }
+
+    @Override
+    public String toString() {
+        return String.format("[totalSize = %s bytes, unpaddedSize = %s bytes, alignment = %s bytes]",
+                totalSizeInBytes(), unpaddedSize(), alignment());
+    }
+
+    public static TypeLayout of(Type type) {
+        final int unpaddedSize;
+        final int alignment;
+
+        // For primitives, we assume that size and alignment requirement coincide
+        if (type instanceof IntegerType integerType) {
+            unpaddedSize = IntMath.divide(integerType.getBitWidth(), 8, RoundingMode.CEILING);
+            alignment = unpaddedSize;
+        } else if (type instanceof FloatType floatType) {
+            unpaddedSize = IntMath.divide(floatType.getBitWidth(), 8, RoundingMode.CEILING);
+            alignment = unpaddedSize;
+        } else if (type instanceof ArrayType arrayType) {
+            final TypeLayout elemTypeLayout = of(arrayType.getElementType());
+            unpaddedSize = elemTypeLayout.totalSizeInBytes() * arrayType.getNumElements();
+            alignment = elemTypeLayout.alignment();
+        } else if (type instanceof AggregateType aggregateType) {
+            return of(aggregateType.getDirectFields());
+        } else {
+            throw new UnsupportedOperationException("Cannot compute memory layout of type " + type);
+        }
+
+        return new TypeLayout(unpaddedSize, alignment);
+    }
+
+    public static TypeLayout of(Iterable<Type> aggregate) {
+        int aggregateSize = 0;
+        int maxAlignment = 1;
+        for (Type fieldType : aggregate) {
+            final TypeLayout layout = of(fieldType);
+            aggregateSize = paddedSize(aggregateSize, layout.alignment()) + layout.totalSizeInBytes();
+            maxAlignment = Math.max(maxAlignment, layout.alignment());
+        }
+        return new TypeLayout(aggregateSize, maxAlignment);
+    }
+
+    public static int paddedSize(int size, int alignment) {
+        final int mod = size % alignment;
+        final int padding = mod == 0 ? 0 : (alignment - mod);
+        return size + padding;
+    }
+
+}
--- dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/GEPToAddition.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/GEPToAddition.java
@@ -7,10 +7,7 @@ import com.dat3m.dartagnan.expression.Type;
 import com.dat3m.dartagnan.expression.integers.IntLiteral;
 import com.dat3m.dartagnan.expression.misc.GEPExpr;
 import com.dat3m.dartagnan.expression.processing.ExprTransformer;
-import com.dat3m.dartagnan.expression.type.AggregateType;
-import com.dat3m.dartagnan.expression.type.ArrayType;
-import com.dat3m.dartagnan.expression.type.IntegerType;
-import com.dat3m.dartagnan.expression.type.TypeFactory;
+import com.dat3m.dartagnan.expression.type.*;
 import com.dat3m.dartagnan.program.Function;
 import com.dat3m.dartagnan.program.Program;
 import com.dat3m.dartagnan.program.event.RegReader;
@@ -75,13 +72,9 @@ public class GEPToAddition implements ProgramProcessor {
                     throw new MalformedProgramException(
                             String.format("Non-constant field index %s for aggregate of type %s.", offset, type));
                 }
-                final int value = constant.getValueAsInt();
-                type = aggregateType.getDirectFields().get(value);
-                int o = 0;
-                for (final Type elementType : aggregateType.getDirectFields().subList(0, value)) {
-                    o += types.getMemorySizeInBytes(elementType);
-                }
-                result = expressions.makeAdd(result, expressions.makeValue(o, archType));
+                final TypeOffset typeOffset = TypeOffset.of(aggregateType, constant.getValueAsInt());
+                type = typeOffset.type();
+                result = expressions.makeAdd(result, expressions.makeValue(typeOffset.offset(), archType));
             }
             return result;
         }
