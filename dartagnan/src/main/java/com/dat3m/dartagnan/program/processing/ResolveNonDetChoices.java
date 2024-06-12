09c554ae80f3c6b07b5895069a45667e47824f84
Thomas Haas <tomy.haas@t-online.de>
2024-06-12 15:08:02 +0200

    Added correct signedness handling

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/ResolveNonDetChoices.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/ResolveNonDetChoices.java
@@ -2,11 +2,13 @@ package com.dat3m.dartagnan.program.processing;
 
 import com.dat3m.dartagnan.expression.Expression;
 import com.dat3m.dartagnan.expression.Type;
+import com.dat3m.dartagnan.expression.processing.ExpressionInspector;
 import com.dat3m.dartagnan.program.Function;
 import com.dat3m.dartagnan.program.Program;
 import com.dat3m.dartagnan.program.event.EventFactory;
 import com.dat3m.dartagnan.program.event.core.Local;
 import com.dat3m.dartagnan.program.event.lang.svcomp.NonDetChoice;
+import com.dat3m.dartagnan.program.misc.NonDetValue;
 
 public class ResolveNonDetChoices implements ProgramProcessor {
 
@@ -26,6 +28,15 @@ public class ResolveNonDetChoices implements ProgramProcessor {
         for (NonDetChoice choice : func.getEvents(NonDetChoice.class)) {
             final Type valueType = choice.getResultRegister().getType();
             final Expression constant = prog.newConstant(valueType);
+
+            final ExpressionInspector signednessMarker = new ExpressionInspector() {
+                @Override
+                public Expression visitNonDetValue(NonDetValue nonDet) {
+                    nonDet.setIsSigned(choice.isSigned());
+                    return nonDet;
+                }
+            };
+            constant.accept(signednessMarker);
 
             final Local assignment = EventFactory.newLocal(choice.getResultRegister(), constant);
             assignment.copyAllMetadataFrom(choice);
