88da3ea94b00ad7362a223f38e30341c4891f80d
Natalia Gavrilenko <natgavrilenko@gmail.com>
2024-07-05 10:07:30 +0200

    Cleanup unused memory

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/RemoveUnusedMemory.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/processing/RemoveUnusedMemory.java
@@ -1,16 +1,13 @@
 package com.dat3m.dartagnan.program.processing;
 
-import com.dat3m.dartagnan.expression.Expression;
-import com.dat3m.dartagnan.expression.processing.ExpressionInspector;
 import com.dat3m.dartagnan.program.Program;
-import com.dat3m.dartagnan.program.event.RegReader;
-import com.dat3m.dartagnan.program.memory.Location;
+import com.dat3m.dartagnan.program.event.MemoryEvent;
 import com.dat3m.dartagnan.program.memory.Memory;
 import com.dat3m.dartagnan.program.memory.MemoryObject;
-import com.dat3m.dartagnan.program.specification.*;
 import com.google.common.collect.Sets;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class RemoveUnusedMemory implements ProgramProcessor {
 
@@ -22,55 +19,25 @@ public class RemoveUnusedMemory implements ProgramProcessor {
 
     @Override
     public void run(Program program) {
+        final Set<MemoryObject> memoryObjects = new HashSet<>();
         final Memory memory = program.getMemory();
-        final MemoryObjectCollector collector = new MemoryObjectCollector();
-        program.getThreadEvents(RegReader.class).forEach(r -> r.transformExpressions(collector));
-        // Also add MemoryObjects referenced by initial values (this does happen in Litmus code)
-        for (MemoryObject obj : memory.getObjects()) {
-            for (Integer field : obj.getInitializedFields()) {
-                obj.getInitialValue(field).accept(collector);
-            }
-        }
-        // Traverse the program spec for references to memory objects
-        addSpecMemoryObjects(program.getSpecification(), collector);
-        Sets.difference(memory.getObjects(), collector.memoryObjects).forEach(memory::deleteMemoryObject);
-    }
 
-    private void addSpecMemoryObjects(AbstractAssert spec, MemoryObjectCollector collector) {
-        if (spec instanceof AssertInline assertInline) {
-            assertInline.getAssertion().getExpression().accept(collector);
-        }
-        if (spec instanceof AssertBasic assertBasic) {
-            assertBasic.getLeft().accept(collector);
-            assertBasic.getRight().accept(collector);
-        }
-        if (spec instanceof AssertNot assertNot) {
-            addSpecMemoryObjects(assertNot.getChild(), collector);
-        }
-        if (spec instanceof AssertCompositeAnd assertCompositeAnd) {
-            addSpecMemoryObjects(assertCompositeAnd.getLeft(), collector);
-            addSpecMemoryObjects(assertCompositeAnd.getRight(), collector);
-        }
-        if (spec instanceof AssertCompositeOr assertCompositeOr) {
-            addSpecMemoryObjects(assertCompositeOr.getLeft(), collector);
-            addSpecMemoryObjects(assertCompositeOr.getRight(), collector);
-        }
-    }
+        // Threads
+        program.getThreadEvents(MemoryEvent.class)
+                .forEach(e -> e.getMemoryAccesses()
+                        .forEach(a -> memoryObjects.addAll(a.address().getMemoryObjects())));
 
-    private static class MemoryObjectCollector implements ExpressionInspector {
+        // Initial values
+        memory.getObjects()
+                .forEach(o -> o.getInitializedFields()
+                        .forEach(f -> memoryObjects.addAll(o.getInitialValue(f).getMemoryObjects())));
 
-        private final HashSet<MemoryObject> memoryObjects = new HashSet<>();
-
-        @Override
-        public Expression visitMemoryObject(MemoryObject address) {
-            memoryObjects.add(address);
-            return address;
+        // Assertions
+        if (program.getSpecification() != null) {
+            memoryObjects.addAll(program.getSpecification().getMemoryObjects());
         }
 
-        @Override
-        public Expression visitLocation(Location location) {
-            memoryObjects.add(location.getMemoryObject());
-            return location;
-        }
+        // Remove unused objects
+        Sets.difference(memory.getObjects(), memoryObjects).forEach(memory::deleteMemoryObject);
     }
 }
