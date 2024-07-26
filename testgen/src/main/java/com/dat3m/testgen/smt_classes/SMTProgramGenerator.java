027f1c4733cd8a5e1fdde4eaed1d29612bcc6ebd
Andrej Shekerov a50041818 <andrej.shekerov2@huawei.com>
2024-07-26 16:03:21 +0200

    Add java_smt classes to SMTProgramGenerator

--- testgen/src/main/java/com/dat3m/testgen/smt_classes/SMTProgramGenerator.java
+++ testgen/src/main/java/com/dat3m/testgen/smt_classes/SMTProgramGenerator.java
@@ -1,14 +1,45 @@
 package com.dat3m.testgen.smt_classes;
 
+import org.sosy_lab.common.ShutdownNotifier;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.java_smt.SolverContextFactory;
+import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
+import org.sosy_lab.java_smt.api.BooleanFormulaManager;
+import org.sosy_lab.java_smt.api.FormulaManager;
+import org.sosy_lab.java_smt.api.IntegerFormulaManager;
+import org.sosy_lab.java_smt.api.SolverContext;
+
 public class SMTProgramGenerator {
     
     Cycle cycle;
+
+    Configuration config;
+    LogManager logger;
+    ShutdownNotifier shutdown;
+    SolverContext context;
+
+    FormulaManager formula_mngr;
+    IntegerFormulaManager int_mngr;
+    BooleanFormulaManager bool_mngr;
     
     public SMTProgramGenerator(
         final Cycle r_cycle
     ) throws Exception {
         if( r_cycle == null ) throw new Exception();
         cycle = r_cycle;
+
+        config = Configuration.defaultConfiguration();
+        logger = LogManager.createNullLogManager();
+        shutdown = ShutdownNotifier.createDummy();
+
+        context = SolverContextFactory.createSolverContext(
+            config, logger, shutdown, Solvers.SMTINTERPOL
+        );
+
+        formula_mngr = context.getFormulaManager();
+        bool_mngr = formula_mngr.getBooleanFormulaManager();
+        int_mngr = formula_mngr.getIntegerFormulaManager();
     }
 
     public String generate_program()
