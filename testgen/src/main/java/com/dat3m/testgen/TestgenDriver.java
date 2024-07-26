8301d7013ccb6e7708398ada809fdd9e5dc4da88
Andrej Shekerov a50041818 <andrej.shekerov2@huawei.com>
2024-07-26 10:52:48 +0200

    Read cat model from dat3m parser

--- testgen/src/main/java/com/dat3m/testgen/TestgenDriver.java
+++ testgen/src/main/java/com/dat3m/testgen/TestgenDriver.java
@@ -1,7 +1,26 @@
 package com.dat3m.testgen;
 
+import java.io.*;
+import java.util.*;
+
+import com.dat3m.dartagnan.wmm.Wmm;
+import com.dat3m.dartagnan.parsers.cat.ParserCat;
+
 public class TestgenDriver {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws Exception {
+        
+        File model_file = new File( 
+            Arrays.stream( args )
+                .filter( a -> a.endsWith( ".cat" ) )
+                .findFirst()
+                .orElseThrow( () -> new IllegalArgumentException( "CAT model not given or format not recognized" ) )
+        );
+                
+        Wmm mcm = new ParserCat().parse( model_file );
+
+        System.out.println( mcm.toString() );
+
     }
+
 }
