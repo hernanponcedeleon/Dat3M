3b88269585a583a61115c5ab65a1189d1edd6119
Andrej Shekerov a50041818 <andrej.shekerov2@huawei.com>
2024-08-12 13:14:51 +0200

    Add new examples

--- testgen/src/main/java/com/dat3m/testgen/TestgenDriver.java
+++ testgen/src/main/java/com/dat3m/testgen/TestgenDriver.java
@@ -6,12 +6,54 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import com.dat3m.testgen.cycle_gen.CNF;
+import com.dat3m.testgen.program_gen.SMTProgramGenerator;
+import com.dat3m.testgen.util.Cycle;
+import com.dat3m.testgen.util.Relation;
 
 public class TestgenDriver {
 
     public static void main(
         String[] args
     ) throws Exception {
+        
+        example_program_generation();
+
+    }
+
+    static void example_program_generation()
+    throws Exception {
+        Cycle cycle;
+
+        /*
+        cycle = new Cycle( 10 );
+        cycle.create_relation( 0, Relation.relation_t.po, 1 );
+        cycle.create_relation( 1, Relation.relation_t.co, 2 );
+        cycle.create_relation( 2, Relation.relation_t.po, 3 );
+        cycle.create_relation( 3, Relation.relation_t.co, 0 ); // */
+
+        /*
+        cycle = new Cycle( 10 );
+        cycle.create_relation( 0, Relation.relation_t.po, 1 );
+        cycle.create_relation( 1, Relation.relation_t.rf, 2 );
+        cycle.create_relation( 2, Relation.relation_t.rf_inv, 3 );
+        cycle.create_relation( 3, Relation.relation_t.co, 0 ); // */
+
+        // /*
+        cycle = new Cycle( 10 );
+        cycle.create_relation( 0, Relation.relation_t.po, 1 );
+        cycle.create_relation( 1, Relation.relation_t.rf_inv, 2 );
+        cycle.create_relation( 2, Relation.relation_t.co, 3 );
+        cycle.create_relation( 3, Relation.relation_t.po, 4 );
+        cycle.create_relation( 4, Relation.relation_t.rf_inv, 5 );
+        cycle.create_relation( 5, Relation.relation_t.co, 0 ); // */
+
+        SMTProgramGenerator smt = new SMTProgramGenerator( cycle );
+
+        System.out.println( smt.generate_program() );
+    }
+
+    static void example_cycles()
+    throws Exception {
         Set <String> all_cycles = new TreeSet<>();
         
         CNF cnf = new CNF( "HB_SC" );
