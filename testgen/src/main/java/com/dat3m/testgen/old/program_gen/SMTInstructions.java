191317305cd10cbbda55b114208147fd7d8e140c
Andrej Shekerov a50041818 <andrej.shekerov2@huawei.com>
2024-08-28 11:05:49 +0200

    Remove old code and classes
    commit 1470975702a010b3b661a8e6420a77013839032b
    Author: Andrej Shekerov a50041818 <andrej.shekerov2@huawei.com>
    Date:   Wed Aug 28 11:05:16 2024 +0200
    
        Remove old code and classes

--- testgen/src/main/java/com/dat3m/testgen/old/program_gen/SMTInstructions.java
+++
@@ -1,8 +0,0 @@
-package com.dat3m.testgen.old.program_gen;
-
-public class SMTInstructions {
-    static int UNDEFINED = 0;
-    static int READ = 1;
-    static int WRITE = 2;
-    static int OBSERVER_READ = 3;
-}
