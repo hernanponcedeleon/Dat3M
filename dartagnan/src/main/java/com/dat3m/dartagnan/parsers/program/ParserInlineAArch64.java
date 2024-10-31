10955dccecce41eecd36b84943acd91e157bec1a
StefanoDalMas <stefano.dalmas01@gmail.com>
2024-10-31 17:00:54 +0100

    first basic implementation to add events from InlineAArch64Parser

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/ParserInlineAArch64.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/ParserInlineAArch64.java
@@ -9,9 +9,20 @@ import com.dat3m.dartagnan.exception.AbortErrorListener;
 import com.dat3m.dartagnan.parsers.InlineAArch64Lexer;
 import com.dat3m.dartagnan.parsers.InlineAArch64Parser;
 import com.dat3m.dartagnan.parsers.program.visitors.VisitorInlineAArch64;
+import com.dat3m.dartagnan.program.Function;
 import com.dat3m.dartagnan.program.Program;
 
 public class ParserInlineAArch64 implements ParserInterface {
+
+    private VisitorInlineAArch64 visitor;
+    private final Function llvmFunction;
+
+    public ParserInlineAArch64(Function function){
+        this.llvmFunction = function;
+    }
+    public VisitorInlineAArch64 getVisitor(){
+        return this.visitor;
+    }
 
     @Override
     public Program parse(CharStream charStream) {
@@ -23,7 +34,7 @@ public class ParserInlineAArch64 implements ParserInterface {
         InlineAArch64Parser parser = new InlineAArch64Parser(tokenStream);
         parser.addErrorListener(new AbortErrorListener());
         ParserRuleContext parserEntryPoint = parser.asm();
-        VisitorInlineAArch64 visitor = new VisitorInlineAArch64();
+        visitor = new VisitorInlineAArch64(this.llvmFunction);
         return (Program) parserEntryPoint.accept(visitor);
     }
 }
--- dartagnan/src/test/java/com/dat3m/dartagnan/parsers/InlineAArch64Test.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/parsers/InlineAArch64Test.java
@@ -22,8 +22,7 @@ public class InlineAArch64Test {
         String input = Files.readString(path, StandardCharsets.UTF_8);
         System.out.println(input);
         CharStream charStream = CharStreams.fromString(input);
-        ParserInlineAArch64 parser = new ParserInlineAArch64();
+        ParserInlineAArch64 parser = new ParserInlineAArch64(null); // this breaks the test because its going to try and create registers under null function
         parser.parse(charStream);
-
     }
 }
