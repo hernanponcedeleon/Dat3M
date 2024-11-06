d2ccf4f0e2b62f5fc5d00573ef2a2be95cc108d4
René Maseli <r.maseli@tu-bs.de>
2024-11-06 09:29:21 +0100

    Fix witnesses excluding the rf edges they enforce. (#771)

--- dartagnan/src/main/java/com/dat3m/dartagnan/wmm/analysis/NativeRelationAnalysis.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/wmm/analysis/NativeRelationAnalysis.java
@@ -875,7 +875,7 @@ public class NativeRelationAnalysis implements RelationAnalysis {
                 EventGraph g = witness.getReadFromKnowledge(program, alias);
                 must.addAll(g);
                 for (Event r : g.getRange()) {
-                    may.removeIf((e1, e2) -> e2 == r);
+                    may.removeIf((e1, e2) -> e2 == r && !g.contains(e1, e2));
                 }
             }
 
