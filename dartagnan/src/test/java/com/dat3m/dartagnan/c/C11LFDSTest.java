a4af45a6689be85ec7f764fda2ad903b59950c73
Tianrui Zheng <107266531+CapZTr@users.noreply.github.com>
2024-11-21 19:48:10 +0100

    Fix some unit tests (#784)
    * Use C11 as a target for vmm code
    
    * Set option INIT_DYNAMIC_ALLOCATIONS for C11LFDSTest and C11LocksTest 
    
    ---------
    
    Signed-off-by: Tianrui Zheng <tianrui.zheng@huawei.com>
    Co-authored-by: Tianrui Zheng <tianrui.zheng@huawei.com>

--- dartagnan/src/test/java/com/dat3m/dartagnan/c/C11LFDSTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/c/C11LFDSTest.java
@@ -1,6 +1,7 @@
 package com.dat3m.dartagnan.c;
 
 import com.dat3m.dartagnan.configuration.Arch;
+import com.dat3m.dartagnan.configuration.OptionNames;
 import com.dat3m.dartagnan.utils.Result;
 import com.dat3m.dartagnan.utils.rules.Provider;
 import com.dat3m.dartagnan.utils.rules.Providers;
@@ -10,14 +11,16 @@ import com.dat3m.dartagnan.wmm.Wmm;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
 
 import java.io.IOException;
 import java.util.Arrays;
 
 import static com.dat3m.dartagnan.configuration.Arch.C11;
 import static com.dat3m.dartagnan.utils.ResourceHelper.getTestResourcePath;
-import static com.dat3m.dartagnan.utils.Result.FAIL;
-import static com.dat3m.dartagnan.utils.Result.PASS;
+import static com.dat3m.dartagnan.utils.Result.*;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
@@ -25,6 +28,13 @@ public class C11LFDSTest extends AbstractCTest {
 
     public C11LFDSTest(String name, Arch target, Result expected) {
         super(name, target, expected);
+    }
+
+    @Override
+    protected Configuration getConfiguration() throws InvalidConfigurationException {
+        return Configuration.builder()
+                            .setOption(OptionNames.INIT_DYNAMIC_ALLOCATIONS, "true")
+                            .build();
     }
 
     @Override
@@ -46,15 +56,19 @@ public class C11LFDSTest extends AbstractCTest {
         return Providers.createWmmFromName(() -> "c11");
     }
 
+    @Override
+    protected Provider<Solvers> getSolverProvider() {
+        return () -> Solvers.YICES2;
+    }
+
     @Parameterized.Parameters(name = "{index}: {0}, target={1}")
     public static Iterable<Object[]> data() throws IOException {
-        // Commented ones take too long ATM
         return Arrays.asList(new Object[][]{
-                // {"dglm", C11, UNKNOWN},
+                {"dglm", C11, UNKNOWN},
                 {"dglm-CAS-relaxed", C11, FAIL},
-                // {"ms", C11, UNKNOWN},
+                {"ms", C11, UNKNOWN},
                 {"ms-CAS-relaxed", C11, FAIL},
-                // {"treiber", C11, UNKNOWN},
+                {"treiber", C11, UNKNOWN},
                 {"treiber-CAS-relaxed", C11, FAIL},
                 {"chase-lev", C11, PASS},
                 // These have an extra thief that violate the assertion
