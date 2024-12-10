768b0904c23edf86cbd0c1ded26bf5b030325f89
Thomas Haas <tomy.haas@t-online.de>
2024-12-10 14:22:22 +0100

    Minor changes to nontermination_weak.c benchmark

--- benchmarks/nontermination/nontermination_weak.c
+++ benchmarks/nontermination/nontermination_weak.c
@@ -9,21 +9,30 @@
 volatile int x = 0;
 atomic_int signal = 0;
 atomic_int success = 0;
+atomic_int sendAgain = 0;
 
 void *thread(void *unused)
 {
-    while(!success) {
+    while(1) {
         x = 1;
         atomic_store_explicit(&signal, 1, memory_order_relaxed);
+        // Wait for acknowledgement
+        while (!sendAgain && !success) { }
+        if (sendAgain) {
+            // Reset message, then try again
+            x = 0;
+            sendAgain = 0;
+        }
+        if (success) break;
     }
     return 0;
 }
 
 void *thread2(void *unused) {
     while (atomic_load_explicit(&signal, memory_order_relaxed) == 1 && x == 0) {
-        // Message was wrong, reset and wait for new one.
-        x = 0;
+        // Message was wrong, ask for a new message
         signal = 0;
+        sendAgain = 1;
     }
     success = 1;
     return 0;
