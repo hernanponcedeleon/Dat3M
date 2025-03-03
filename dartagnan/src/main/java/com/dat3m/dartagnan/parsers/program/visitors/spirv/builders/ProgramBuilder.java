539c7622a688367c1ba059ef42f1ca5db88ee870
Haining <haining.tong@helsinki.fi>
2025-03-03 10:14:09 +0100

    Add Supports for OpenCL Alignment Decoration
    Signed-off-by: Haining Tong <haining.tong@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/spirv/builders/ProgramBuilder.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/spirv/builders/ProgramBuilder.java
@@ -5,9 +5,8 @@ import com.dat3m.dartagnan.exception.ParsingException;
 import com.dat3m.dartagnan.expression.Expression;
 import com.dat3m.dartagnan.expression.ExpressionFactory;
 import com.dat3m.dartagnan.expression.Type;
-import com.dat3m.dartagnan.expression.type.FunctionType;
-import com.dat3m.dartagnan.expression.type.ScopedPointerType;
-import com.dat3m.dartagnan.expression.type.TypeFactory;
+import com.dat3m.dartagnan.expression.aggregates.ConstructExpr;
+import com.dat3m.dartagnan.expression.type.*;
 import com.dat3m.dartagnan.parsers.program.visitors.spirv.decorations.BuiltIn;
 import com.dat3m.dartagnan.program.*;
 import com.dat3m.dartagnan.program.event.Event;
@@ -20,9 +19,11 @@ import com.dat3m.dartagnan.program.memory.ScopedPointerVariable;
 import com.dat3m.dartagnan.program.processing.transformers.MemoryTransformer;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static com.dat3m.dartagnan.parsers.program.visitors.spirv.decorations.DecorationType.BUILT_IN;
 
@@ -47,6 +48,34 @@ public class ProgramBuilder {
         this.program = new Program(new Memory(), Program.SourceLanguage.SPV);
         this.controlFlowBuilder = new ControlFlowBuilder(expressions);
         this.decorationsBuilder = new DecorationsBuilder(grid);
+    }
+
+    public Expression getAlignedValue(String id, Expression base, Type type) {
+        if (type instanceof AggregateType aggregateType && base instanceof ConstructExpr constructExpr) {
+            List<Expression> elements = aggregateType.getFields().stream()
+                    .map(field -> {
+                        int index = aggregateType.getFields().indexOf(field);
+                        return getAlignedValue(id, constructExpr.getOperands().get(index), field.type());
+                    })
+                    .collect(Collectors.toList());
+            return ExpressionFactory.getInstance().makeConstruct(type, elements);
+        }
+        if (type instanceof ArrayType arrayType && base instanceof ConstructExpr constructExpr) {
+            int numOperands = constructExpr.getOperands().size();
+            if (arrayType.getNumElements() < numOperands) {
+                throw new ParsingException("Array initializer has too many elements for variable '%s'", id);
+            }
+            List<Expression> elements = IntStream.range(0, arrayType.getNumElements())
+                    .mapToObj(i -> i < numOperands
+                            ? getAlignedValue(id, constructExpr.getOperands().get(i), arrayType.getElementType())
+                            : makeUndefinedValue(arrayType.getElementType()))
+                    .collect(Collectors.toList());
+            return ExpressionFactory.getInstance().makeArray(arrayType.getElementType(), elements, true);
+        }
+        if (base.getType().equals(type)) {
+            return base;
+        }
+        throw new ParsingException("Cannot align initializer for variable '" + id + "' of type " + type);
     }
 
     public Program build() {
@@ -180,22 +209,18 @@ public class ProgramBuilder {
                 .collect(Collectors.toSet());
     }
 
-    public MemoryObject allocateVariable(String id, int bytes) {
-        MemoryObject memObj = program.getMemory().allocateVirtual(bytes, true, null);
+    public ScopedPointerVariable allocateScopedPointerVariable(String id, Expression initValue,
+                                                               Expression alignment, String storageClass, Type pointedType) {
+        int bytes = TypeFactory.getInstance().getMemorySizeInBytes(pointedType);
+        MemoryObject memObj = program.getMemory().allocateVirtual(bytes, true, alignment, null);
         memObj.setName(id);
-        return memObj;
-    }
-
-    public ScopedPointerVariable allocateScopedPointerVariable(String id, Expression initValue, String storageClass, Type pointedType) {
-        MemoryObject memObj = allocateVariable(id, TypeFactory.getInstance().getMemorySizeInBytes(pointedType));
         memObj.setIsThreadLocal(false);
         memObj.setInitialValue(0, initValue);
         if (arch == Arch.OPENCL) {
             String openCLSpace = Tag.Spirv.toOpenCLTag(Tag.Spirv.getStorageClassTag(Set.of(storageClass)));
             memObj.addFeatureTag(openCLSpace);
         }
-        return ExpressionFactory.getInstance().makeScopedPointerVariable(
-                id, storageClass, pointedType, memObj);
+        return ExpressionFactory.getInstance().makeScopedPointerVariable(id, storageClass, pointedType, memObj);
     }
 
     public String getPointerStorageClass(String id) {
