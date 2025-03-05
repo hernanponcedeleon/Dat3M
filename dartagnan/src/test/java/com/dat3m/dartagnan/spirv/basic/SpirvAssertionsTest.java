8c5eace62043a60410d9952e07ec8dc573d3aae0
natgavrilenko <natgavrilenko@users.noreply.github.com>
2025-03-05 19:48:57 +0100

    Use ExtractExpr for spirv OpCompositeExtract (#815)

--- dartagnan/src/test/java/com/dat3m/dartagnan/spirv/basic/SpirvAssertionsTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/spirv/basic/SpirvAssertionsTest.java
@@ -71,6 +71,7 @@ public class SpirvAssertionsTest {
                 {"array-of-vector.spv.dis", 1, PASS},
                 {"array-of-vector1.spv.dis", 1, PASS},
                 {"vector-read-write.spv.dis", 1, PASS},
+                {"composite-extract.spv.dis", 1, PASS},
                 {"spec-id-integer.spv.dis", 1, PASS},
                 {"spec-id-boolean.spv.dis", 1, PASS},
                 {"mixed-size.spv.dis", 1, PASS},
