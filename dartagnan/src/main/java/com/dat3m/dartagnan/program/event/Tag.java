14f6d07c75204e416cd965e141ddc07c79e17e9e
haining <haining.tong@helsinki.fi>
2025-03-07 21:30:31 +0100

    update opencl barrier to use single event (#818)
    Signed-off-by: Haining Tong <haining.tong@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/event/Tag.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/Tag.java
@@ -382,9 +382,6 @@ public final class Tag {
         public static final String GLOBAL_SPACE = "GLOBAL";
         public static final String LOCAL_SPACE = "LOCAL";
         public static final String GENERIC_SPACE = "GENERIC";
-        // Barrier
-        public static final String ENTRY_FENCE = "EF";
-        public static final String EXIT_FENCE = "XF";
         // Default Tags
         public static final String DEFAULT_SPACE = GENERIC_SPACE;
         public static final String DEFAULT_SCOPE = DEVICE;
