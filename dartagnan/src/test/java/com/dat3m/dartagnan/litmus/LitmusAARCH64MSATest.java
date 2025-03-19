6c2156a79687ce726e32efccb9dad641c2ebc10e
René Pascal Maseli <r.maseli@tu-bs.de>
2025-03-19 11:56:15 +0100

    Litmus test collection

--- dartagnan/src/test/java/com/dat3m/dartagnan/litmus/LitmusAARCH64MSATest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/litmus/LitmusAARCH64MSATest.java
@@ -5,18 +5,29 @@ import com.dat3m.dartagnan.utils.rules.Provider;
 import com.dat3m.dartagnan.utils.rules.Providers;
 import com.dat3m.dartagnan.wmm.Wmm;
 import org.junit.runners.Parameterized;
+import org.sosy_lab.common.configuration.Configuration;
 
 import java.io.IOException;
+
+import static com.dat3m.dartagnan.configuration.OptionNames.INITIALIZE_REGISTERS;
+import static com.dat3m.dartagnan.configuration.OptionNames.USE_INTEGERS;
 
 public class LitmusAARCH64MSATest extends LitmusAARCH64Test {
 
     @Parameterized.Parameters(name = "{index}: {0}, {1}")
     public static Iterable<Object[]> data() throws IOException {
-        return buildLitmusTests("local/mixedsizetests", "ARM8", "-herd");
+        return buildLitmusTests("litmus/AARCH64-MIXED", "ARM8", "-MIXED");
     }
 
     public LitmusAARCH64MSATest(String path, Result expected) {
         super(path, expected);
+    }
+
+    protected Provider<Configuration> getConfigurationProvider() {
+        return Provider.fromSupplier(() -> Configuration.builder()
+                .setOption(INITIALIZE_REGISTERS, "true")
+                .setOption(USE_INTEGERS, "false")
+                .build());
     }
 
     @Override
