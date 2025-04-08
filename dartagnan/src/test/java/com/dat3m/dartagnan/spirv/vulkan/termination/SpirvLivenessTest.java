4dce23719b8e1850fa9f851d18403ad6cd8a75de
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2025-04-09 03:15:20 +0800

    Add MP patters for testing progress hierarchy
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/test/java/com/dat3m/dartagnan/spirv/vulkan/termination/SpirvLivenessTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/spirv/vulkan/termination/SpirvLivenessTest.java
@@ -18,11 +18,14 @@ import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.log.BasicLogManager;
 import org.sosy_lab.java_smt.SolverContextFactory;
 import org.sosy_lab.java_smt.api.SolverContext;
+import com.dat3m.dartagnan.configuration.ProgressModel;
+import com.dat3m.dartagnan.program.event.Tag;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.Map;
 
 import static com.dat3m.dartagnan.configuration.Property.TERMINATION;
 import static com.dat3m.dartagnan.utils.ResourceHelper.getRootPath;
@@ -37,19 +40,42 @@ public class SpirvLivenessTest {
     private final String modelPath = getRootPath("cat/spirv.cat");
     private final String programPath;
     private final int bound;
+    private final ProgressModel.Hierarchy progressModel;
     private final Result expected;
 
-    public SpirvLivenessTest(String file, int bound, Result expected) {
-        this.programPath = getTestResourcePath("spirv/vulkan/basic/" + file);
+    public SpirvLivenessTest(String file, int bound, ProgressModel.Hierarchy progressModel, Result expected) {
+        this.programPath = getTestResourcePath("spirv/vulkan/termination/" + file);
         this.bound = bound;
+        this.progressModel = progressModel;
         this.expected = expected;
     }
 
     @Parameterized.Parameters(name = "{index}: {0}, {1}, {2}")
     public static Iterable<Object[]> data() throws IOException {
+
+        ProgressModel.Hierarchy fairUniform = ProgressModel.uniform(ProgressModel.FAIR);
+        ProgressModel.Hierarchy qfObe = ProgressModel.scoped(ProgressModel.FAIR, Map.of(
+                                                Tag.Vulkan.QUEUE_FAMILY, ProgressModel.OBE));
+        ProgressModel.Hierarchy qfObeSgUnfair = ProgressModel.scoped(ProgressModel.FAIR, Map.of(
+                                                        Tag.Vulkan.QUEUE_FAMILY, ProgressModel.OBE,
+                                                        Tag.Vulkan.SUB_GROUP, ProgressModel.UNFAIR));
+        ProgressModel.Hierarchy qfHsa = ProgressModel.scoped(ProgressModel.FAIR, Map.of(
+                                                Tag.Vulkan.QUEUE_FAMILY, ProgressModel.HSA));
+        ProgressModel.Hierarchy qfHsaSgUnfair = ProgressModel.scoped(ProgressModel.FAIR, Map.of(
+                                                    Tag.Vulkan.QUEUE_FAMILY, ProgressModel.HSA,
+                                                    Tag.Vulkan.SUB_GROUP, ProgressModel.UNFAIR));
+
         return Arrays.asList(new Object[][]{
-                {"non-uniform-barrier-1.spv.dis", 1, PASS},
-                {"non-uniform-barrier-2.spv.dis", 1, FAIL},
+                {"non-uniform-barrier-1.spv.dis", 1, fairUniform, PASS},
+                {"non-uniform-barrier-2.spv.dis", 1, fairUniform, FAIL},
+                {"mp-groupID.spv.dis", 1, fairUniform, PASS},
+                {"mp-groupID.spv.dis", 1, qfObe, FAIL},
+                {"mp-groupID.spv.dis", 1, qfHsa, PASS},
+                {"mp-groupID.spv.dis", 1, qfHsaSgUnfair, PASS},
+                {"mp-atomicAdd-groupID.spv.dis", 1, fairUniform, PASS},
+                {"mp-atomicAdd-groupID.spv.dis", 1, qfObe, PASS},
+                {"mp-atomicAdd-groupID.spv.dis", 1, qfObeSgUnfair, PASS},
+                {"mp-atomicAdd-groupID.spv.dis", 1, qfHsa, FAIL},
         });
     }
 
@@ -77,6 +103,7 @@ public class SpirvLivenessTest {
         VerificationTask.VerificationTaskBuilder builder = VerificationTask.builder()
                 .withConfig(Configuration.builder().build())
                 .withBound(bound)
+                .withProgressModel(progressModel)
                 .withTarget(Arch.VULKAN);
         Program program = new ProgramParser().parse(new File(programPath));
         Wmm mcm = new ParserCat().parse(new File(modelPath));
