ff4220e4679c95424abe8ea8a9673b2f32c7ffac
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2025-04-12 13:50:16 +0800

    Reserve some exit codes for future needs
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/utils/ExitCode.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/utils/ExitCode.java
@@ -2,22 +2,22 @@ package com.dat3m.dartagnan.utils;
 
 public enum ExitCode {
     // Normal termination
-    NORMAL_TERMINATION,
-    // Unexpected result
-    UNKNOWN_ERROR, TIMEOUT_ELAPSED, BOUNDED_RESULT,
-    // Verification error
-	PROGRAM_SPEC_VIOLATION, CAT_SPEC_VIOLATION, TERMINATION_VIOLATION, DATA_RACE_FREEDOM_VIOLATION;
+    NORMAL_TERMINATION, BOUNDED_RESULT,
+    // Verification errors
+	PROGRAM_SPEC_VIOLATION, CAT_SPEC_VIOLATION, TERMINATION_VIOLATION, DATA_RACE_FREEDOM_VIOLATION,
+    // Unexpected results
+    UNKNOWN_ERROR, TIMEOUT_ELAPSED;
 
     public int getExitCode() {
         return switch (this) {
             case NORMAL_TERMINATION -> 0;
-            case UNKNOWN_ERROR -> 1;
-            case TIMEOUT_ELAPSED -> 2;
-            case BOUNDED_RESULT -> 3;
-            case PROGRAM_SPEC_VIOLATION -> 4;
-            case CAT_SPEC_VIOLATION -> 5;
-            case TERMINATION_VIOLATION -> 6;
-            case DATA_RACE_FREEDOM_VIOLATION -> 7;
+            case BOUNDED_RESULT -> 1;
+            case PROGRAM_SPEC_VIOLATION -> 10;
+            case CAT_SPEC_VIOLATION -> 11;
+            case TERMINATION_VIOLATION -> 12;
+            case DATA_RACE_FREEDOM_VIOLATION -> 13;
+            case UNKNOWN_ERROR -> 20;
+            case TIMEOUT_ELAPSED -> 21;
         };
     }
 }
