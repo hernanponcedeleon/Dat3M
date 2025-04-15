871e2916a6d9eb6ba3654ab2243b530d66410cd4
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2025-04-15 16:46:06 +0800

    Few improvements for SVCOMP and witness validation
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/utils/ExitCode.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/utils/ExitCode.java
@@ -5,6 +5,8 @@ public enum ExitCode {
     NORMAL_TERMINATION, BOUNDED_RESULT,
     // Verification errors
     PROGRAM_SPEC_VIOLATION, CAT_SPEC_VIOLATION, TERMINATION_VIOLATION, DATA_RACE_FREEDOM_VIOLATION,
+    // Witness validation errors
+    WITNESS_NOT_VALIDATED, WRONG_WITNESS_FILE,
     // Unexpected results
     UNKNOWN_ERROR, TIMEOUT_ELAPSED;
 
@@ -16,8 +18,10 @@ public enum ExitCode {
             case CAT_SPEC_VIOLATION -> 11;
             case TERMINATION_VIOLATION -> 12;
             case DATA_RACE_FREEDOM_VIOLATION -> 13;
-            case UNKNOWN_ERROR -> 20;
-            case TIMEOUT_ELAPSED -> 21;
+            case WITNESS_NOT_VALIDATED -> 20;
+            case WRONG_WITNESS_FILE -> 21;
+            case UNKNOWN_ERROR -> 30;
+            case TIMEOUT_ELAPSED -> 31;
         };
     }
 }
