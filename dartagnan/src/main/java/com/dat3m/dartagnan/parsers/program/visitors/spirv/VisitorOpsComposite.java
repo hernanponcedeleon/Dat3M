7a2b40f68105f6bf021493f3c53f6f4d8b6e124f
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2025-04-26 03:09:44 +0800

    Add support for OpVectorShuffle
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/spirv/VisitorOpsComposite.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/spirv/VisitorOpsComposite.java
@@ -4,13 +4,15 @@ import com.dat3m.dartagnan.exception.ParsingException;
 import com.dat3m.dartagnan.expression.Expression;
 import com.dat3m.dartagnan.expression.ExpressionFactory;
 import com.dat3m.dartagnan.expression.Type;
-import com.dat3m.dartagnan.expression.type.TypeFactory;
+import com.dat3m.dartagnan.expression.type.*;
+import com.dat3m.dartagnan.expression.aggregates.ConstructExpr;
 import com.dat3m.dartagnan.parsers.SpirvBaseVisitor;
 import com.dat3m.dartagnan.parsers.SpirvParser;
 import com.dat3m.dartagnan.parsers.program.visitors.spirv.builders.ProgramBuilder;
 
 import java.util.List;
 import java.util.Set;
+import java.util.ArrayList;
 
 public class VisitorOpsComposite extends SpirvBaseVisitor<Void> {
 
@@ -64,6 +66,39 @@ public class VisitorOpsComposite extends SpirvBaseVisitor<Void> {
         throw new ParsingException("Type mismatch in composite insert for '%s'", id);
     }
 
+    @Override
+    public Void visitOpVectorShuffle(SpirvParser.OpVectorShuffleContext ctx) {
+        String id = ctx.idResult().getText();
+        String v1Id = ctx.vector1().getText();
+        Expression v1 = builder.getExpression(v1Id);
+        String v2Id = ctx.vector2().getText();
+        Expression v2 = builder.getExpression(v2Id);
+        List<Integer> components = ctx.components().stream()
+            .map(SpirvParser.ComponentsContext::getText)
+            .map(Integer::parseInt)
+            .toList();
+        if(v1.getType() instanceof ArrayType aType1) {
+            if(v2.getType() instanceof ArrayType aType2) {
+                int s1 = aType1.getNumElements();
+                int s2 = aType2.getNumElements();
+                List<Expression> concat = new ArrayList<>();
+                for (Integer index : components) {
+                    if (index >= 0 && index < s1) {
+                        concat.add(v1 instanceof ConstructExpr ? v1.getOperands().get(index) : expressions.makeExtract(v1, index));
+                    } else if (index >= s1 && index < s1 + s2) {
+                        concat.add(v2 instanceof ConstructExpr ? v2.getOperands().get(index - s1) : expressions.makeExtract(v2, index - s1));
+                    } else {
+                        throw new ParsingException("Index %s out of bounds", index);
+                    }
+                }
+                builder.addExpression(id, expressions.makeArray(aType1.getElementType(), concat, true));
+                return null;
+            }
+            throw new ParsingException("Parameter %s of OpVectorShuffle is not a vector ", v2Id);
+        }
+        throw new ParsingException("Parameter %s of OpVectorShuffle is not a vector ", v1Id);
+    }
+
     private Expression getInsertion(Expression compositeExpr, Expression objectExpr, List<Integer> indexes, String id) {
         try {
             return expressions.makeInsert(compositeExpr, objectExpr, indexes);
@@ -75,7 +110,8 @@ public class VisitorOpsComposite extends SpirvBaseVisitor<Void> {
     public Set<String> getSupportedOps() {
         return Set.of(
                 "OpCompositeExtract",
-                "OpCompositeInsert"
+                "OpCompositeInsert",
+                "OpVectorShuffle"
         );
     }
 }
