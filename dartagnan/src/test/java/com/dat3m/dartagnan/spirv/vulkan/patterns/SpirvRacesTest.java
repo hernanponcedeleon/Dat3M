cdbf3cb9e98978ac15be818ac97d91bc7b945b55
natgavrilenko <natgavrilenko@users.noreply.github.com>
2025-06-10 13:36:58 +0200

    Align cat definition with the alloy model (#901)
    * Align cat definition with the alloy model
    * Removed spirv-check model
    * Renamed cat model spirv to vulkan
    * Add names to asmo litmus tests
    * Renamed cat file in the encoding test
    
    ---------
    
    Signed-off-by: Natalia Gavrilenko <natalia.gavrilenko@huawei.com>

--- dartagnan/src/test/java/com/dat3m/dartagnan/spirv/vulkan/patterns/SpirvRacesTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/spirv/vulkan/patterns/SpirvRacesTest.java
@@ -35,7 +35,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Parameterized.class)
 public class SpirvRacesTest {
 
-    private final String modelPath = getRootPath("cat/spirv.cat");
+    private final String modelPath = getRootPath("cat/vulkan.cat");
     private final String programPath;
     private final boolean filter;
     private final Result expected;
