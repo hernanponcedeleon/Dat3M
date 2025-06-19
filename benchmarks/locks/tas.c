77c1c4a0a6d0b39f552866c3027b44cb8bcb2ede
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2025-06-19 15:18:00 +0800

    Add taslock benchmark
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

---
+++ benchmarks/locks/tas.c
@@ -0,0 +1,41 @@
+#include <pthread.h>
+#include "tas.h"
+#include <assert.h>
+
+#ifndef NTHREADS
+#define NTHREADS 3
+#endif
+
+int shared;
+taslock_t lock;
+int sum = 0;
+
+void *thread_n(void *arg)
+{
+    intptr_t index = ((intptr_t) arg);
+
+    taslock_acquire(&lock);
+    shared = index;
+    int r = shared;
+    assert(r == index);
+    sum++;
+    taslock_release(&lock);
+    return NULL;
+}
+
+int main()
+{
+    pthread_t t[NTHREADS];
+
+    taslock_init(&lock);
+
+    for (int i = 0; i < NTHREADS; i++)
+        pthread_create(&t[i], 0, thread_n, (void *)(size_t)i);
+
+    for (int i = 0; i < NTHREADS; i++)
+        pthread_join(t[i], 0);
+
+    assert(sum == NTHREADS);
+
+    return 0;
+}
