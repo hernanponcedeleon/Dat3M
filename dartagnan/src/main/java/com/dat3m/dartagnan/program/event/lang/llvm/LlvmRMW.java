0ed8dae2ccd077a73c5bc4172d388fa0a78a9004
Thomas Haas <tomy.haas@t-online.de>
2025-09-01 13:18:07 +0200

    Made llvmRMW print the operator in lower case

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/event/lang/llvm/LlvmRMW.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/lang/llvm/LlvmRMW.java
@@ -21,7 +21,7 @@ public class LlvmRMW extends RMWOpResultBase {
     @Override
     public String defaultString() {
         return String.format("%s = llvm_rmw_%s(%s, %s, %s)\t### LLVM",
-                resultRegister, operator.getName(), address, operand, mo);
+                resultRegister, operator.getName().toLowerCase(), address, operand, mo);
     }
 
     @Override
