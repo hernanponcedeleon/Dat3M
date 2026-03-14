4b3efa5803ec801348f7fb8036792d771d3c69ae
Hernan Ponce de Leon <hernanl.leon@huawei.com>
2026-03-14 14:15:52 +0800

    Use prefix only for litmus
    Signed-off-by: Hernan Ponce de Leon <hernanl.leon@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/SyntacticContextAnalysis.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/SyntacticContextAnalysis.java
@@ -217,7 +217,8 @@ public class SyntacticContextAnalysis {
 
     public static String getSourceLocationString(Event ev) {
         final SourceLocation loc = ev.getMetadata(SourceLocation.class);
-        final String prefix = ev.getThread().getName();
+        // We generate the thread prefix only for litmus for now
+        final String prefix = loc instanceof SourceLocation.Litmus ? ev.getThread().getName() : "";
         return String.format("@%s%s", prefix, loc != null ? loc : "#unknown");
     }
 
