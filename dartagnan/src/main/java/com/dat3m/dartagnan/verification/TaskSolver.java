b195927de3985035bbe0bc727f01449d93a75615
Thomas Haas <tomy.haas@t-online.de>
2026-04-04 14:49:23 +0200

    Moved Timeout and ModelChecker creation into TaskSolver

--- dartagnan/src/main/java/com/dat3m/dartagnan/verification/TaskSolver.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/verification/TaskSolver.java
@@ -3,9 +3,12 @@ package com.dat3m.dartagnan.verification;
 import com.dat3m.dartagnan.configuration.Method;
 import com.dat3m.dartagnan.encoding.IREvaluator;
 import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.utils.Utils;
 import com.dat3m.dartagnan.verification.model.ExecutionModelManager;
 import com.dat3m.dartagnan.verification.model.ExecutionModelNext;
+import com.dat3m.dartagnan.verification.solving.AssumeSolver;
 import com.dat3m.dartagnan.verification.solving.ModelChecker;
+import com.dat3m.dartagnan.verification.solving.RefinementSolver;
 import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,9 +16,14 @@ import org.sosy_lab.common.ShutdownManager;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
+import org.sosy_lab.common.configuration.TimeSpanOption;
 import org.sosy_lab.java_smt.api.SolverException;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.dat3m.dartagnan.configuration.OptionNames.METHOD;
+import static com.dat3m.dartagnan.configuration.OptionNames.TIMEOUT;
+import static com.dat3m.dartagnan.configuration.Property.DATARACEFREEDOM;
 
 @Options
 public class TaskSolver implements AutoCloseable {
@@ -29,6 +37,16 @@ public class TaskSolver implements AutoCloseable {
             description = "Method to be used.",
             toUppercase = true)
     private Method method = Method.getDefault();
+
+    @Option(
+            name = TIMEOUT,
+            description = "Timeout before interrupting the task solver. Can specify time units ns, ms, s (default), min, and h.")
+    @TimeSpanOption(min = 0, codeUnit = TimeUnit.MILLISECONDS, defaultUserUnit = TimeUnit.SECONDS)
+    private int timeout = 0;
+
+    private boolean hasTimeout() {
+        return timeout > 0;
+    }
 
     // ====================================== State ======================================
 
@@ -64,15 +82,44 @@ public class TaskSolver implements AutoCloseable {
 
     private void initModelChecker() throws InvalidConfigurationException {
         Preconditions.checkState(modelChecker == null, "Model checker already initialized");
-        modelChecker = ModelChecker.create(task, method);
+
+        if (task.getProperty().contains(DATARACEFREEDOM) && method != Method.EAGER) {
+            // For DATARACEFREEDOM, we always use EAGER
+            logger.warn("Method {} is not supported for property {}. Using EAGER instead.", method, DATARACEFREEDOM);
+            method = Method.EAGER;
+        }
+
+        modelChecker = switch (method) {
+            case EAGER -> AssumeSolver.create(task);
+            case LAZY -> RefinementSolver.create(task);
+        };
         if (shutdownManager != null) {
             modelChecker.setShutdownManager(shutdownManager);
         }
     }
 
-    public void run() throws InterruptedException, InvalidConfigurationException, SolverException {
+    public void run() throws SolverException, InterruptedException, InvalidConfigurationException {
         initModelChecker();
+
+        if (!hasTimeout()) {
+            modelChecker.run();
+            return;
+        }
+
+        final java.lang.Thread t = new java.lang.Thread(() -> {
+            try {
+                final long timeoutInMillis = timeout;
+                java.lang.Thread.sleep(timeoutInMillis);
+                final String error = String.format("Timeout of %s exceeded.", Utils.toTimeString(timeoutInMillis));
+                modelChecker.requestShutdown(error);
+            } catch (InterruptedException e) {
+                // Verification ended, nothing to be done.
+            }
+        });
+
+        t.start();
         modelChecker.run();
+        t.interrupt();
     }
 
     public Result getResult() {
@@ -106,7 +153,7 @@ public class TaskSolver implements AutoCloseable {
         }
     }
 
-    private void checkHasRun() {
+    protected void checkHasRun() {
         Preconditions.checkState(modelChecker != null, "Model checker has not run yet.");
     }
 
