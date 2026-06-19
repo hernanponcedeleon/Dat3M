e42c489f2454216e18dd2c1751532492877b7b52
René Pascal Maseli <r.maseli@tu-bs.de>
2026-06-19 15:06:18 +0200

    fixup! Fix ModifierTrait

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/alias/ModifierTrait.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/alias/ModifierTrait.java
@@ -233,7 +233,7 @@ public interface ModifierTrait <Modifier> {
         public Md shrinkToBounds(Md modifier, int objectSize) {
             final int remainingSize = objectSize - modifier.offset;
             for (final int a : modifier.alignment) {
-                if (a > 0 ? a < remainingSize : -a < remainingSize && -a <= modifier.offset) {
+                if (a > 0 ? a < remainingSize : -a < remainingSize || -a <= modifier.offset) {
                     return modifier;
                 }
             }
