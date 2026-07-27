a6c59dbafc22a2d1713e41615fe5abc555618c44
Natalia Gavrilenko <natgavrilenko@gmail.com>
2026-07-27 10:59:25 +0200

    Vulkan: Add new model

--- dartagnan/src/test/java/com/dat3m/dartagnan/spirv/vulkan/basic/SpirvAssertionsTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/spirv/vulkan/basic/SpirvAssertionsTest.java
@@ -27,7 +27,8 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Parameterized.class)
 public class SpirvAssertionsTest {
 
-    private final String modelPath = getRootPath("cat/vulkan.cat");
+    private final String oldModelPath = getRootPath("cat/vulkan.cat");
+    private final String newModelFixed = getRootPath("cat/vulkan-fixed.cat");
     private final String programPath;
     private final int bound;
     private final Result expected;
@@ -126,10 +127,11 @@ public class SpirvAssertionsTest {
 
     @Test
     public void test() throws Exception {
-        assertEquals(expected, TestHelper.createAndRunSolver(mkTask(), Method.EAGER));
+        assertEquals(expected, TestHelper.createAndRunSolver(mkTask(oldModelPath), Method.EAGER));
+        assertEquals(expected, TestHelper.createAndRunSolver(mkTask(newModelFixed), Method.EAGER));
     }
 
-    private VerificationTask mkTask() throws Exception {
+    private VerificationTask mkTask(String modelPath) throws Exception {
         VerificationTask.VerificationTaskBuilder builder = VerificationTask.builder()
                 .withConfig(TestHelper.getBasicConfig())
                 .withBound(bound)
