2792884fd7d879524481c8b2feff71da14ce6e06
René Maseli <r.maseli@tu-bs.de>
2026-09-03 17:29:20 +0200

    Fix another asymmetry in BranchEquivalence

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/BranchEquivalence.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/analysis/BranchEquivalence.java
@@ -229,6 +229,7 @@ public class BranchEquivalence extends AbstractEquivalence<Event> {
                 final Branch creatorBranch = decomposition.event2BranchMap.get(start.getCreator());
                 threadInitialBranch.mustPred.add(creatorBranch);
                 threadInitialBranch.exclusiveBranches.addAll(creatorBranch.exclusiveBranches);
+                creatorBranch.exclusiveBranches.forEach(b -> b.exclusiveBranches.add(threadInitialBranch));
                 creatorBranch.mustSucc.add(threadInitialBranch);
             } else {
                 unconditionalThreadInitialBranches.add(threadInitialBranch);
