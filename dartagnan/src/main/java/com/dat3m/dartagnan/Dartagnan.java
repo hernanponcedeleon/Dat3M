a782f02825ab60b41e3ea8204765970ff2e36405
Hernan Ponce de Leon <zeta96@gmail.com>
2026-09-05 18:20:02 +0200

    Make Dartagnan stateless

--- dartagnan/src/main/java/com/dat3m/dartagnan/Dartagnan.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/Dartagnan.java
@@ -28,6 +28,7 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static com.dat3m.dartagnan.configuration.OptionNames.TARGET;
@@ -40,13 +41,8 @@ public class Dartagnan extends BaseOptions {
 
     private static final Logger logger = LoggerFactory.getLogger(Dartagnan.class);
 
-    private final Pipelines pipelines;
-    private final ProgramParser programParser;
-
-    private Dartagnan(Configuration config) throws InvalidConfigurationException, IOException {
+    private Dartagnan(Configuration config) throws InvalidConfigurationException {
         config.recursiveInject(this);
-        pipelines = Pipelines.load(getCompilationPipelinePath());
-        programParser = new ProgramParser(pipelines);
     }
 
     public static void main(String[] args) throws Exception {
@@ -70,12 +66,14 @@ public class Dartagnan extends BaseOptions {
 
         final Configuration config = loadConfigurationFromArgs(args);
         final Dartagnan o = new Dartagnan(config);
+        final Pipelines pipelines = Pipelines.load(o.getCompilationPipelinePath());
+        final ProgramParser programParser = new ProgramParser(pipelines);
         final Path catFile  = getCatFileFromArgs(args);
-        final List<Path> progFiles = o.getProgramFilesFromArgs(args);
+        final List<Path> progFiles = getProgramFilesFromArgs(args, programParser::isSupportedFile);
         final boolean isBatchMode = progFiles.size() > 1;
         final OutputGenerator outputGenerator = OutputGenerator.create(isBatchMode, config);
 
-        logEnvironmentInfo(o.pipelines.getTools());
+        logEnvironmentInfo(pipelines.getTools());
 
         logger.info("CAT file path: {}", catFile);
 
@@ -85,7 +83,7 @@ public class Dartagnan extends BaseOptions {
             Output output;
             try {
                 // ----------- Generate verification task -----------
-                final Program p = o.programParser.parse(progFile);
+                final Program p = programParser.parse(progFile);
                 if (o.overrideEntryFunction()) {
                     p.setEntrypoint(new Entrypoint.Simple(p.getFunctionByName(o.getEntryFunction()).orElseThrow(
                             () -> new MalformedProgramException(String.format("Program has no function named %s. Select a different entry point.", o.getEntryFunction())))));
@@ -191,11 +189,11 @@ public class Dartagnan extends BaseOptions {
         return catFile;
     }
 
-    private List<Path> getProgramFilesFromArgs(String[] args) throws IOException {
+    private static List<Path> getProgramFilesFromArgs(String[] args, Predicate<Path> isSupportedFile) throws IOException {
         final List<Path> files = Stream.of(args)
                 .map(Path::of)
                 .filter(Files::exists)
-                .flatMap(path -> getProgramFiles(path).stream())
+                .flatMap(path -> getProgramFiles(path, isSupportedFile).stream())
                 .toList();
         if (files.isEmpty()) {
             throw new IOException("Path to input program(s) not given or format not recognized");
@@ -203,10 +201,10 @@ public class Dartagnan extends BaseOptions {
         return files;
     }
 
-    private List<Path> getProgramFiles(Path path) {
+    private static List<Path> getProgramFiles(Path path, Predicate<Path> isSupportedFile) {
         try (Stream<Path> stream = Files.walk(path)) {
             return stream.filter(Files::isRegularFile)
-                    .filter(programParser::isSupportedFile)
+                    .filter(isSupportedFile)
                     .sorted(Comparator.comparing(Path::toString))
                     .toList();
         } catch (IOException e) {
