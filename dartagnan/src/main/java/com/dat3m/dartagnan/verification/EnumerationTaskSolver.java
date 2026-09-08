8ce4d341da42b9074c384941b0fe55ed51fc31d5
Thomas Haas <tomy.haas@t-online.de>
2026-09-08 13:49:39 +0200

    Add EnumerationTask and related classes (enabled by --mode=enumeration).

--- dartagnan/src/main/java/com/dat3m/dartagnan/verification/EnumerationTaskSolver.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/verification/EnumerationTaskSolver.java
@@ -1,9 +1,10 @@
 package com.dat3m.dartagnan.verification;
 
+import com.dat3m.dartagnan.program.Program;
+import com.dat3m.dartagnan.program.event.metadata.SourceLocation;
 import com.dat3m.dartagnan.verification.solving.EnumerationSolver;
-import com.google.common.collect.ImmutableList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.dat3m.dartagnan.verification.solving.ModelChecker;
+import com.google.common.base.Preconditions;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.java_smt.api.SolverException;
@@ -12,8 +13,6 @@ import static com.dat3m.dartagnan.program.Program.SourceLanguage.LITMUS;
 
 @Options
 public final class EnumerationTaskSolver extends TaskSolverBase<EnumerationTaskSolver, EnumerationTask, EnumerationResult> implements AutoCloseable {
-
-    private static final Logger logger = LoggerFactory.getLogger(EnumerationTaskSolver.class);
 
     // =================================== Construction ===================================
 
@@ -36,13 +35,6 @@ public final class EnumerationTaskSolver extends TaskSolverBase<EnumerationTaskS
 
     @Override
     public void run() throws SolverException, InterruptedException, InvalidConfigurationException {
-
-        if (task.getProgram().getSpecification() == null) {
-            logger.warn("Program has no specification; no final states to enumerate");
-            result = new EnumerationResult(task, ResultStatus.PASS, ImmutableList.of(), ImmutableList.of());
-            return;
-        }
-
         try (EnumerationSolver enumerator = EnumerationSolver.create(task)) {
             enumerator.setShutdownManager(shutdownManager);
             startRun();
