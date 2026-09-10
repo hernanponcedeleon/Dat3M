b2cb8080c06688b73f03c1cd767d6ef3f6d5d4cb
Thomas Haas <tomy.haas@t-online.de>
2026-09-10 11:23:33 +0200

    Enumeration of spec-less program yields 0 states now.

--- dartagnan/src/main/java/com/dat3m/dartagnan/verification/EnumerationTaskSolver.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/verification/EnumerationTaskSolver.java
@@ -1,10 +1,9 @@
 package com.dat3m.dartagnan.verification;
 
-import com.dat3m.dartagnan.program.Program;
-import com.dat3m.dartagnan.program.event.metadata.SourceLocation;
 import com.dat3m.dartagnan.verification.solving.EnumerationSolver;
-import com.dat3m.dartagnan.verification.solving.ModelChecker;
-import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.java_smt.api.SolverException;
@@ -13,6 +12,8 @@ import static com.dat3m.dartagnan.program.Program.SourceLanguage.LITMUS;
 
 @Options
 public final class EnumerationTaskSolver extends TaskSolverBase<EnumerationTaskSolver, EnumerationTask, EnumerationResult> implements AutoCloseable {
+
+    private static final Logger logger = LoggerFactory.getLogger(EnumerationTaskSolver.class);
 
     // =================================== Construction ===================================
 
@@ -35,6 +36,13 @@ public final class EnumerationTaskSolver extends TaskSolverBase<EnumerationTaskS
 
     @Override
     public void run() throws SolverException, InterruptedException, InvalidConfigurationException {
+
+        if (task.getProgram().getSpecification() == null) {
+            logger.warn("Program has no specification; no final states to enumerate");
+            result = new EnumerationResult(task, ResultStatus.PASS, ImmutableList.of(), ImmutableList.of());
+            return;
+        }
+
         try (EnumerationSolver enumerator = EnumerationSolver.create(task)) {
             enumerator.setShutdownManager(shutdownManager);
             startRun();
