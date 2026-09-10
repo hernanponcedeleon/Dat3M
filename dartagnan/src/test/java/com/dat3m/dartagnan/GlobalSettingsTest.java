3678547ac3a27d6e9478a4323052d67798f3d873
René Maseli <r.maseli@tu-bs.de>
2026-09-10 21:13:03 +0200

    Move test suites

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/GlobalSettingsTest.java
@@ -0,0 +1,40 @@
+package com.dat3m.dartagnan;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class GlobalSettingsTest {
+
+    private final Path pathOutputDirectory = GlobalSettings.getOutputDirectory();
+
+    @Test
+    public void shouldCreateOutputDirectoryInCreateMode() throws IOException {
+        // given
+        Files.deleteIfExists(pathOutputDirectory);
+
+        // when
+        GlobalSettings.getOrCreateOutputDirectory();
+
+        // then
+        assertTrue(Files.exists(pathOutputDirectory));
+        assertTrue(Files.isDirectory(pathOutputDirectory));
+    }
+
+    @Test
+    public void shouldNotCreateOutputDirectoryInGetMode() throws IOException {
+        // given
+        Files.deleteIfExists(pathOutputDirectory);
+
+        // when
+        GlobalSettings.getOutputDirectory();
+
+        // then
+        assertFalse(Files.exists(pathOutputDirectory));
+    }
+}
---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/utils/printer/PrinterTest.java
@@ -0,0 +1,93 @@
+package com.dat3m.dartagnan.utils.printer;
+
+import com.dat3m.dartagnan.program.Program;
+import com.dat3m.dartagnan.program.processing.LoopUnrolling;
+import com.dat3m.dartagnan.program.processing.compilation.Compilation;
+import com.dat3m.dartagnan.wmm.Wmm;
+import org.junit.Test;
+
+import static com.dat3m.dartagnan.test.ResourceHelper.getRootPath;
+import static com.dat3m.dartagnan.test.ResourceHelper.getTestResourcePath;
+import static com.dat3m.dartagnan.test.TestHelper.parseProgram;
+import static com.dat3m.dartagnan.test.TestHelper.parseWmm;
+import static org.junit.Assert.assertNotNull;
+
+public class PrinterTest {
+
+    // Test to call toString() of most events
+
+    @Test()
+    public void Printll() throws Exception {
+        Program p = parseProgram(getTestResourcePath("locks/linuxrwlock.ll"));
+        assertNotNull(Printer.newInstance().print(p));
+        Compilation.newInstance().run(p);
+        LoopUnrolling.newInstance().run(p);
+        assertNotNull(Printer.newInstance().print(p));
+    }
+
+    @Test()
+    public void PrintX86() throws Exception {
+        Program p = parseProgram(getTestResourcePath("litmus/MP+mfence-rmw+rmw-mfence.litmus"));
+        assertNotNull(Printer.newInstance().print(p));
+        assertNotNull(p.getSpecification().toString());
+    }
+
+    @Test()
+    public void PrintPPC() throws Exception {
+        Program p = parseProgram(getTestResourcePath("litmus/MP+lwsync+data-wsi-rfi-ctrlisync.litmus"));
+        assertNotNull(Printer.newInstance().print(p));
+        assertNotNull(p.getSpecification().toString());
+    }
+
+    @Test()
+    public void PrintAARCH64() throws Exception {
+        Program p = parseProgram(getTestResourcePath("litmus/MP+popl+poap.litmus"));
+        assertNotNull(Printer.newInstance().print(p));
+        assertNotNull(p.getSpecification().toString());
+    }
+
+    @Test()
+    public void PrintLinux() throws Exception {
+        Program p = parseProgram(getTestResourcePath("litmus/C-rcu-link-after.litmus"));
+        assertNotNull(Printer.newInstance().print(p));
+        assertNotNull(p.getSpecification().toString());
+    }
+
+    @Test()
+    public void PrintLinux2() throws Exception {
+        Program p = parseProgram(getRootPath("litmus/LKMM/dart/C-atomic-fetch-simple-01.litmus"));
+        assertNotNull(Printer.newInstance().print(p));
+        assertNotNull(p.getSpecification().toString());
+    }
+
+    @Test()
+    public void PrintLinux3() throws Exception {
+        Program p = parseProgram(getRootPath("litmus/LKMM/manual/atomic/C-atomic-01.litmus"));
+        assertNotNull(Printer.newInstance().print(p));
+        assertNotNull(p.getSpecification().toString());
+    }
+
+    @Test()
+    public void TSOtoString() throws Exception {
+        Wmm cat = parseWmm(getRootPath("cat/tso.cat"));
+        assertNotNull(cat.toString());
+    }
+
+    @Test()
+    public void AARCH64toString() throws Exception {
+        Wmm cat = parseWmm(getRootPath("cat/aarch64.cat"));
+        assertNotNull(cat.toString());
+    }
+
+    @Test()
+    public void PowertoString() throws Exception {
+        Wmm cat = parseWmm(getRootPath("cat/power.cat"));
+        assertNotNull(cat.toString());
+    }
+
+    @Test()
+    public void LinuxtoString() throws Exception {
+        Wmm cat = parseWmm(getRootPath("cat/linux-kernel.cat"));
+        assertNotNull(cat.toString());
+    }
+}
