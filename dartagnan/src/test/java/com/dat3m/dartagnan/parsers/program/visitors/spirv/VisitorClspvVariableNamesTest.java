e63ac1918161ab43049cbe6bc2a7fb96492c6a05
Hernan Ponce de Leon <zeta96@gmail.com>
2026-09-14 16:47:00 +0200

    Use VariableName metadata for MemoryObject

--- dartagnan/src/test/java/com/dat3m/dartagnan/parsers/program/visitors/spirv/VisitorClspvVariableNamesTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/parsers/program/visitors/spirv/VisitorClspvVariableNamesTest.java
@@ -2,16 +2,15 @@ package com.dat3m.dartagnan.parsers.program.visitors.spirv;
 
 import com.dat3m.dartagnan.parsers.program.ProgramParser;
 import com.dat3m.dartagnan.program.Program;
-import com.dat3m.dartagnan.program.memory.MemoryObject;
+import com.dat3m.dartagnan.program.memory.metadata.VariableName;
 import org.junit.Test;
 
 import java.nio.file.Path;
-import java.util.Set;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.dat3m.dartagnan.utils.ResourceHelper.getTestResourcePath;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class VisitorClspvVariableNamesTest {
 
@@ -20,10 +19,12 @@ public class VisitorClspvVariableNamesTest {
         Path path = getTestResourcePath("spirv/vulkan/patterns/mp.spvasm");
         Program program = new ProgramParser().parse(path);
 
-        Set<String> names = program.getMemory().getObjects().stream()
-                .map(MemoryObject::getName)
-                .collect(Collectors.toSet());
-        assertTrue(names.containsAll(Set.of("flag", "data", "r0", "r1")));
-        assertFalse(names.contains("%5"));
+        Map<String, String> variableNames = program.getMemory().getObjects().stream()
+                .filter(m -> m.hasMetadata(VariableName.class))
+                .collect(Collectors.toMap(
+                        m -> m.getName(),
+                        m -> m.getMetadata(VariableName.class).value()
+                ));
+        assertEquals(Map.of("%5", "flag", "%6", "data", "%7", "r0", "%8", "r1"), variableNames);
     }
 }
