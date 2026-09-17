7322892d9a183366caab880806a2e60e524eece3
René Maseli <r.maseli@tu-bs.de>
2026-09-17 10:42:21 +0200

    Raise the timeouts for ArrayValidTest and BranchTest
    (Originally, there was no general timeout on these tests.)

--- dartagnan/src/test/java/com/dat3m/dartagnan/litmus/ArrayValidTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/litmus/ArrayValidTest.java
@@ -23,8 +23,5 @@ public class ArrayValidTest extends AbstractLitmusTest {
     }
 
     @Override
-    protected long getTimeoutSeconds() { return 1; }
-
-    @Override
     protected Task.TaskBuilder getTaskBuilder() { return super.getTaskBuilder().withSolverTimeout(60); }
 }
