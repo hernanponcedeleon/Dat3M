5d532d6b43df907ceff73694e1eb0b33317fe492
René Maseli <r.maseli@tu-bs.de>
2026-09-21 12:42:22 +0200

    fixup! Move testing utilities into package utils.testing

--- dartagnan/src/test/java/com/dat3m/dartagnan/parsers/program/ProgramParserTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/parsers/program/ProgramParserTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import java.nio.file.Path;
 
-import static com.dat3m.dartagnan.utils.ResourceHelper.getTestResourcePath;
+import static com.dat3m.dartagnan.test.ResourceHelper.getTestResourcePath;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
