e1596a5a5b7179b76051f3be23ec370722b1ebbc
René Maseli <r.maseli@tu-bs.de>
2026-09-21 18:17:11 +0200

    Revert "TODO enable both methods"
    This reverts commit 739e64145a26b16f20cf8a9d329351a22f6f50fd.

--- dartagnan/src/test/java/com/dat3m/dartagnan/test/AbstractVerificationTaskSolverTest.java
+++ dartagnan/src/test/java/com/dat3m/dartagnan/test/AbstractVerificationTaskSolverTest.java
@@ -19,6 +19,7 @@ import java.util.EnumSet;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeNotNull;
+import static org.junit.Assume.assumeTrue;
 
 public abstract class AbstractVerificationTaskSolverTest {
 
@@ -26,11 +27,13 @@ public abstract class AbstractVerificationTaskSolverTest {
 
     @Test
     public void testAssume() throws Exception {
+        assumeTrue(isEagerMethodEnabled());
         testSolver(Method.EAGER);
     }
 
     @Test
     public void testRefinement() throws Exception {
+        assumeTrue(isLazyMethodEnabled());
         testSolver(Method.LAZY);
     }
 
